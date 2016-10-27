173d9cd526b26b798ededea861f67cab8ce0e21b
AndrewJacksonThe2nd <championofthecommonman@gmail.com>
2016-10-27 12:43:43 -0700

    Removes old files

--- mods/HUDRESTORATION/lib/Lua/HUDRestoration/CrimeNetManagerNew.lua
+++
@@ -1,8 +0,0 @@
---Controls CRIME.NET ambience noise.
-CrimeNetGui.oinit = CrimeNetGui.init
-
-function CrimeNetGui:init(...)
-	
-		CrimeNetGui:oinit(...)
-	self._crimenet_ambience = managers.menu_component:post_event( "crimenet_ambience" )
-end

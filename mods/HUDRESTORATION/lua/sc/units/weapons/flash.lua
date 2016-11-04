23a81c676d46039a65d9b8269a9f9f7affb144f4
AndrewJacksonThe2nd <championofthecommonman@gmail.com>
2016-11-04 13:29:52 -0700

    whoops

--- mods/HUDRESTORATION/lua/sc/units/weapons/flash.lua
+++ mods/HUDRESTORATION/lua/sc/units/weapons/flash.lua
@@ -4,7 +4,7 @@
 -- v2.0 improved indicator positioning (should now move along with the flash grenade)
 -- v2.1 Added a new icon for the Flash grenade
 -- http://www.unknowncheats.me/forum/payday-2/135236-grenade-indicator-flash-grenades-only-smoke-grenades-excluded.html
-
+if restoration.Options:GetValue("SC/SC") then
 local QuickFlashGrenade_flashbang_grenade_marker_ID
 
 local QuickFlashGrenade__play_sound_and_effects_original = QuickFlashGrenade._play_sound_and_effects
@@ -44,5 +44,7 @@ function QuickFlashGrenade:_play_sound_and_effects()
             QuickFlashGrenade_flashbang_grenade_marker_ID = nil
         end
     end
+
+end
 
 end

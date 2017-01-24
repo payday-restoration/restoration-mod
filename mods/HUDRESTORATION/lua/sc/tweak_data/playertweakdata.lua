3a97d49b65c72e9ed766659cf17d67acc7098af4
SC <gametwisterdotnet@yahoo.com>
2017-01-24 01:04:53 -0800

    stuff

--- mods/HUDRESTORATION/lua/sc/tweak_data/playertweakdata.lua
+++ mods/HUDRESTORATION/lua/sc/tweak_data/playertweakdata.lua
@@ -203,8 +203,8 @@ function PlayerTweakData:init()
 	self.damage.MIN_DAMAGE_INTERVAL = 0.3
 	self.damage.respawn_time_penalty = 30
 	self.damage.base_respawn_time_penalty = 5
-	self.damage.automatic_assault_ai_trade_time = 999999999999999999
-	self.damage.automatic_assault_ai_trade_time_max = 999999999999999999
+	self.damage.automatic_assault_ai_trade_time = 120
+	self.damage.automatic_assault_ai_trade_time_max = 180
 	self.fall_health_damage = 3
 	self.fall_damage_alert_size = 250
 	self.SUSPICION_OFFSET_LERP = 0.75

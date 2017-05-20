be963ad4e1937a556219c9d615037691c96d167b
SC <gametwisterdotnet@yahoo.com>
2017-05-20 01:55:20 -0700

    removing hooks
    cause they're in vanilla now

--- mods/HUDRESTORATION/lua/sc/managers/teamai.lua
+++
@@ -1,7 +0,0 @@
-if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue("SC/SC") then
-	if managers.job and (managers.job:current_job_id() == "short2_stage2b") then
-		CriminalsManager.MAX_NR_TEAM_AI = 2
-	else
-		CriminalsManager.MAX_NR_TEAM_AI = 3
-	end
-end

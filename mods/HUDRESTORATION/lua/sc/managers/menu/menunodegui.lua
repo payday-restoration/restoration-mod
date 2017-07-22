2970a1054409a1872d8f443a9e194aaf71eaf92b
GHBDougley <douglaskmarx@gmail.com>
2017-07-21 18:30:10 -0600

    Updated phrasing

--- mods/HUDRESTORATION/lua/sc/managers/menu/menunodegui.lua
+++ mods/HUDRESTORATION/lua/sc/managers/menu/menunodegui.lua
@@ -27,7 +27,7 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 				Global._friendsonly_warning_shown = true
 				QuickMenu:new(
 					"SC's Mod",
-					"Warning! You are using Seamlink's overhaul (and possibly some other mods) which is known to have some incompatibilities with SC's Mod! If you are experiencing issues, please disable Seamlink's overhaul or SC's Mod before reporting anything to Seamlink or myself.",
+					"Warning! You are using Seamlink's overhaul (and possibly some other mods) which is known to have some incompatibilities with SC's Mod! If you are experiencing issues, please disable Seamlink's overhaul or SC's Mod before reporting anything to Seamlink or Restoration.",
 					{
 						{
 							text = "ok",
@@ -47,7 +47,7 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 				Global._friendsonly_warning_shown = true
 				QuickMenu:new(
 					"SC's Mod",
-					"Warning! You are using Spawn Faster or Harder Difficulty (and possibly some other mods) which is known to have some incompatibilities with SC's Mod! If you are experiencing issues, please disable Spawn Faster/Harder Difficulty or SC's Mod before reporting anything to BRAND0 or myself.",
+					"Warning! You are using Spawn Faster or Harder Difficulty (and possibly some other mods) which is known to have some incompatibilities with SC's Mod! If you are experiencing issues, please disable Spawn Faster/Harder Difficulty or SC's Mod before reporting anything to BRAND0 or Restoration.",
 					{
 						{
 							text = "ok",
@@ -70,7 +70,7 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 				Global._friendsonly_warning_shown = true
 				QuickMenu:new(
 					"SC's Mod",
-					"Your saved SC's Skill Trees have been loaded and your vanilla skills SAVED.",
+					"SC's Mod is currently ON.\n \nYour saved SC's Skill Trees have been LOADED and your vanilla skills SAVED.",
 					{
 						{
 							text = "ok",
@@ -89,7 +89,7 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 				Global._friendsonly_warning_shown = true
 				QuickMenu:new(
 					"SC's Mod",
-					"WARNING! No SC's skill tree save file was detected, a new one will be made. Your VANILLA skills have been SAVED and will be re-enabled when you turn off the mod.",
+					"WARNING!\n \nNo SC's skill tree save file was detected, a new one will be made.\n \nYour VANILLA skills have been SAVED and will be re-enabled when you turn off the re-balance.",
 					{
 						{
 							text = "ok",
@@ -110,7 +110,7 @@ else
 				Global._friendsonly_warning_shown = true
 				QuickMenu:new(
 					"SC's Mod",
-					"Your saved Vanilla Skill Trees have been loaded and your SC skills SAVED.",
+					"SC's Mod is currently OFF.\n \nYour saved Vanilla Skill Trees have been LOADED and your SC skills SAVED.",
 					{
 						{
 							text = "ok",
@@ -129,7 +129,7 @@ else
 				Global._friendsonly_warning_shown = true
 				QuickMenu:new(
 					"SC's Mod",
-					"WARNING! No vanilla skill tree save file was detected, a new one will be made. Your SC skills have been SAVED and will be re-enabled when you turn on the mod again.",
+					"WARNING!\n \nNo vanilla skill tree save file was detected, a new one will be made.\n \nYour SC skills have been SAVED and will be re-enabled when you turn on the re-balance again.",
 					{
 						{
 							text = "ok",

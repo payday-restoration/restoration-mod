10b95b450fcb975ea6ca9cdad13140e491bdb0ac
SC <gametwisterdotnet@yahoo.com>
2017-08-09 02:38:35 -0700

    Fixed Better Bots crash
    ta-dah

--- mods/RestorationMod/lua/sc/units/player_team/logics/teamailogicassault.lua
+++ mods/RestorationMod/lua/sc/units/player_team/logics/teamailogicassault.lua
@@ -1,21 +1,25 @@
 if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue("SC/SC") then
 
-function TeamAILogicAssault.mark_enemy(data, criminal, to_mark, play_sound, play_action)
-	if play_sound then
-		criminal:sound():say(to_mark:base():char_tweak().bot_priority_shout, true)
+	if SystemFS:exists("mods/Better Bots/mod.txt") then
+		return
 	end
-	if play_action and not criminal:movement():chk_action_forbidden("action") then
-		local new_action = {
-			type = "act",
-			variant = "arrest",
-			body_part = 3,
-			align_sync = true
-		}
-		if criminal:brain():action_request(new_action) then
-			data.internal_data.gesture_arrest = true
+
+	function TeamAILogicAssault.mark_enemy(data, criminal, to_mark, play_sound, play_action)
+		if play_sound then
+			criminal:sound():say(to_mark:base():char_tweak().bot_priority_shout, true)
 		end
+		if play_action and not criminal:movement():chk_action_forbidden("action") then
+			local new_action = {
+				type = "act",
+				variant = "arrest",
+				body_part = 3,
+				align_sync = true
+			}
+			if criminal:brain():action_request(new_action) then
+				data.internal_data.gesture_arrest = true
+			end
+		end
+		to_mark:contour():add("mark_enemy", true)
 	end
-	to_mark:contour():add("mark_enemy", true)
-end
 
 end
--- mods/RestorationMod/lua/sc/units/player_team/teamaimovement.lua
+++ mods/RestorationMod/lua/sc/units/player_team/teamaimovement.lua
@@ -1,22 +1,26 @@
 if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue("SC/SC") then
 
-TeamAIMovement.update = TeamAIMovement.super.update
-
-function TeamAIMovement:on_SPOOCed( enemy_unit )
-	local difficulty = Global.game_settings and Global.game_settings.difficulty or "normal"
-	local difficulty_index = tweak_data:difficulty_to_index(difficulty)
-	if difficulty_index < 9 then
-		if self._unit:character_damage()._god_mode then
-			return
-		end
-
-		--self._unit:brain():set_logic( "surrender" )
-		--self._unit:network():send( "arrested" )
-		--self._unit:character_damage():on_arrested()
-		self._unit:character_damage():on_incapacitated()
-	else
-		return "countered"
+	if SystemFS:exists("mods/Better Bots/mod.txt") then
+		return
 	end
-end
+
+	TeamAIMovement.update = TeamAIMovement.super.update
+
+	function TeamAIMovement:on_SPOOCed( enemy_unit )
+		local difficulty = Global.game_settings and Global.game_settings.difficulty or "normal"
+		local difficulty_index = tweak_data:difficulty_to_index(difficulty)
+		if difficulty_index < 9 then
+			if self._unit:character_damage()._god_mode then
+				return
+			end
+			--Maybe we'll be more like PDTH again some day
+			--self._unit:brain():set_logic( "surrender" )
+			--self._unit:network():send( "arrested" )
+			--self._unit:character_damage():on_arrested()
+			self._unit:character_damage():on_incapacitated()
+		else
+			return "countered"
+		end
+	end
 
 end

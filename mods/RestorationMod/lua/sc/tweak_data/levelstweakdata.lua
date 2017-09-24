900c9fd1b93b929f1b72963bff4d0890ac9f018f
SC <gametwisterdotnet@yahoo.com>
2017-09-23 18:42:32 -0700

    Fixes (again)
    It's always something.
    -Fixed Reaper Medics having body armor (????????)
    -Remove some packages that I had loaded for some reason. Should help with load times maybe?

--- mods/RestorationMod/lua/sc/tweak_data/levelstweakdata.lua
+++ mods/RestorationMod/lua/sc/tweak_data/levelstweakdata.lua
@@ -22,12 +22,6 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 		if Global.load_level == true and not PackageManager:loaded("packages/job_nail") then
 			PackageManager:load("packages/job_nail")
 		end
-		if Global.load_level == true and not PackageManager:loaded("packages/lvl_help") then
-			PackageManager:load("packages/lvl_help")
-			end
-		if Global.load_level == true and not PackageManager:loaded("packages/lvl_mad") then
-			PackageManager:load("packages/lvl_mad")
-		end
 		self.short2_stage1.package = {"packages/job_short2_stage1", "levels/narratives/dentist/mia/stage1/world_sounds"}
 		self.friend.package = {"packages/lvl_friend", "levels/narratives/h_alex_must_die/stage_1/world_sounds"}
 		--Stuff for Summers below--

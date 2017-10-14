1e9acbb830584aed3bcc3dc238a2c8e0b01f2151
iamgoofball <iamgoofball@gmail.com>
2017-10-14 12:55:04 -0700

    more fixes

--- mods/RestorationMod/lua/sc/loc/loc.lua
+++ mods/RestorationMod/lua/sc/loc/loc.lua
@@ -111,8 +111,8 @@ Hooks:Add("LocalizationManagerPostInit", "SC_Localization", function(loc)
 		
 		--More fitting descriptions of difficulties--
 		["menu_risk_elite"] = "DEATH WISH. FOR YOU, ACTION IS THE JUICE.",
-		--["menu_risk_sm_wish"] = "OVERDOSE",
-		["menu_difficulty_sm_wish"] = "END OF THE LINE. EVERY CHALLENGE IN THE BOOK IS LAID AGAINST YOU.",
+		["menu_risk_sm_wish"] = "Like PAYDAY With Guns",
+		["menu_difficulty_sm_wish"] = "It's like Dark Souls, but with heisting. Approach like Bloodborne.",
 
 		["bm_melee_katana_info"] = "While playing as Jiro, killing a Cloaker with a charged attack triggers a special kill animation.",
 		

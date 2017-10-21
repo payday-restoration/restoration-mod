9b1e60e906637a49bc1e21ee1652fee92b616e7e
GHBDougley <douglaskmarx@gmail.com>
2017-10-20 20:26:42 -0600

    fixed a thing

--- mods/RestorationMod/lua/sc/loc/loc.lua
+++ mods/RestorationMod/lua/sc/loc/loc.lua
@@ -111,7 +111,7 @@ Hooks:Add("LocalizationManagerPostInit", "SC_Localization", function(loc)
 		
 		--More fitting descriptions of difficulties--
 		["menu_risk_elite"] = "DEATH WISH. FOR YOU, ACTION IS THE JUICE.",
-		["menu_risk_sm_wish"] = "Crackdown: Commissioner Garrett has deployed his cure.",
+		["menu_risk_sm_wish"] = "CRACKDOWN. OMNIA & D.H.S. HAVE MADE YOU THEIR #1 TARGET.",
 		["menu_difficulty_sm_wish"] = "Crackdown",
 
 		["bm_melee_katana_info"] = "While playing as Jiro, killing a Cloaker with a charged attack triggers a special kill animation.",

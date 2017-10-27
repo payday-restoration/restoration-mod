ee9f97d623d0daef4532a16fe071bb531678d000
SC <gametwisterdotnet@yahoo.com>
2017-10-27 16:00:23 -0700

    added save stuff as an option
    subject to change

--- mods/RestorationMod/lua/sc/managers/savefilemanager.lua
+++ mods/RestorationMod/lua/sc/managers/savefilemanager.lua
@@ -1,7 +1,4 @@
---don't edit or you die
-
-
-if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue("SC/SC") then
+if restoration and restoration.Options:GetValue("SC/Save") then
 	SavefileManager.PROGRESS_SLOT = SystemInfo:platform() == Idstring("WIN32") and 77
 	SavefileManager.BACKUP_SLOT = SystemInfo:platform() == Idstring("WIN32") and 77
 end

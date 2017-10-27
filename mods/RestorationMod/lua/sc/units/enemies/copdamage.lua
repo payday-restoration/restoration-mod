a394154e4cd62cc99f36581ba66664d70909ab47
SC <gametwisterdotnet@yahoo.com>
2017-10-27 15:39:59 -0700

    martini pls

--- mods/RestorationMod/lua/sc/units/enemies/copdamage.lua
+++ mods/RestorationMod/lua/sc/units/enemies/copdamage.lua
@@ -159,37 +159,32 @@ if SC and SC._data.sc_player_weapon_toggle or restoration and restoration.Option
 		if self._dead or self._invulnerable then
 			return
 		end
-
 		if self:is_friendly_fire(attack_data.attacker_unit) then
 			return "friendly_fire"
 		end
-
 		local is_civilian = CopDamage.is_civilian(self._unit:base()._tweak_table)
-
 		if not is_civilian then
 			managers.player:send_message(Message.OnEnemyShot, nil, attack_data.attacker_unit, self._unit, attack_data and attack_data.variant or "bullet")
 		end
-
-		if self._has_plate and attack_data.col_ray.body and attack_data.col_ray.body:name() == self._ids_plate_name and not attack_data.armor_piercing then
+		if self._has_plate and attack_data.col_ray.body and attack_data.col_ray.body:name() == self._ids_plate_name and not attack_data.armor_piercing and not attack_data.weapon_unit:base().thrower_unit and attack_data.weapon_unit and attack_data.weapon_unit:base().is_category and not attack_data.weapon_unit:base():is_category("bow", "crossbow", "saw") then
 			local armor_pierce_roll = math.rand(1)
 			local armor_pierce_value = 0
-
 			if attack_data.attacker_unit == managers.player:player_unit() and not attack_data.weapon_unit:base().thrower_unit then
 				armor_pierce_value = armor_pierce_value + attack_data.weapon_unit:base():armor_piercing_chance()
 				armor_pierce_value = armor_pierce_value + managers.player:upgrade_value("player", "armor_piercing_chance", 0)
 				armor_pierce_value = armor_pierce_value + managers.player:upgrade_value("weapon", "armor_piercing_chance", 0)
 				armor_pierce_value = armor_pierce_value + managers.player:upgrade_value("weapon", "armor_piercing_chance_2", 0)
-
 				if attack_data.weapon_unit:base():got_silencer() then
 					armor_pierce_value = armor_pierce_value + managers.player:upgrade_value("weapon", "armor_piercing_chance_silencer", 0)
 				end
-
 				if attack_data.weapon_unit:base():is_category("saw") then
 					armor_pierce_value = armor_pierce_value + managers.player:upgrade_value("saw", "armor_piercing_chance", 0)
 				end
+				if attack_data.attacker_unit == managers.player:player_unit() and attack_data.weapon_unit:base().thrower_unit then
+					armor_pierce_value = 1
+				end
 			elseif attack_data.attacker_unit:base() and attack_data.attacker_unit:base().sentry_gun then
 				local owner = attack_data.attacker_unit:base():get_owner()
-
 				if alive(owner) then
 					if owner == managers.player:player_unit() then
 						armor_pierce_value = armor_pierce_value + managers.player:upgrade_value("sentry_gun", "armor_piercing_chance", 0)
@@ -200,89 +195,78 @@ if SC and SC._data.sc_player_weapon_toggle or restoration and restoration.Option
 					end
 				end
 			end
-
-			if armor_pierce_value <= armor_pierce_roll then
+			if armor_pierce_roll >= armor_pierce_value then
 				return
 			end
 		end
-
-		local result = nil
+		local result
 		local body_index = self._unit:get_body_index(attack_data.col_ray.body:name())
 		local head = self._head_body_name and attack_data.col_ray.body and attack_data.col_ray.body:name() == self._ids_head_body_name
 		local damage = attack_data.damage
-
 		if self._unit:base():char_tweak().DAMAGE_CLAMP_BULLET then
 			damage = math.min(damage, self._unit:base():char_tweak().DAMAGE_CLAMP_BULLET)
 		end
-
+		damage = damage * (self._char_tweak.damage.bullet_damage_mul or 1)
+		local roll = math.rand(1, 100)
+		local dodge_chance = self._char_tweak.damage.bullet_dodge_chance or 0
+		if attack_data.attacker_unit == managers.player:player_unit() and attack_data.weapon_unit:base().thrower_unit then
+			dodge_chance = 0
+		end
+		if roll <= dodge_chance and self._char_tweak.damage.bullet_dodge_chance then
+			self._unit:sound():play("pickup_fak_skill", nil, nil)
+			return
+		end
 		damage = damage * (self._marked_dmg_mul or 1)
-
 		if self._marked_dmg_mul and self._marked_dmg_dist_mul then
 			local dst = mvector3.distance(attack_data.origin, self._unit:position())
 			local spott_dst = tweak_data.upgrades.values.player.marked_inc_dmg_distance[self._marked_dmg_dist_mul]
-
-			if spott_dst[1] < dst then
+			if dst > spott_dst[1] then
 				damage = damage * spott_dst[2]
 			end
 		end
-
-		if self._unit:movement():cool() then
-			damage = self._HEALTH_INIT
-		end
-
 		local headshot = false
 		local headshot_multiplier = 1
-
 		if attack_data.attacker_unit == managers.player:player_unit() then
 			local critical_hit, crit_damage = self:roll_critical_hit(attack_data)
-
 			if critical_hit then
 				managers.hud:on_crit_confirmed()
-
 				damage = crit_damage
 				attack_data.critical_hit = true
 			else
 				managers.hud:on_hit_confirmed()
 			end
-
 			headshot_multiplier = managers.player:upgrade_value("weapon", "passive_headshot_damage_multiplier", 1)
-
 			if tweak_data.character[self._unit:base()._tweak_table].priority_shout then
 				damage = damage * managers.player:upgrade_value("weapon", "special_damage_taken_multiplier", 1)
 			end
-
 			if head then
 				managers.player:on_headshot_dealt()
-
 				headshot = true
 			end
 		end
-
 		if not self._char_tweak.ignore_headshot and not self._damage_reduction_multiplier and head then
-			damage = self._char_tweak.headshot_dmg_mul and damage * self._char_tweak.headshot_dmg_mul * headshot_multiplier or self._health * 10
-		end
-
-		if attack_data.weapon_unit:base().get_add_head_shot_mul then
-			local add_head_shot_mul = attack_data.weapon_unit:base():get_add_head_shot_mul()
-
-			if not head and add_head_shot_mul and self._char_tweak and self._char_tweak.access ~= "tank" then
-				local tweak_headshot_mul = math.max(0, self._char_tweak.headshot_dmg_mul - 1)
-				local mul = tweak_headshot_mul * add_head_shot_mul + 1
-				damage = damage * mul
+			if self._char_tweak.headshot_dmg_mul then
+				damage = damage * self._char_tweak.headshot_dmg_mul * headshot_multiplier
+			else
+				damage = self._health * 10
 			end
 		end
-
+		if attack_data.weapon_unit and attack_data.weapon_unit:base().is_category and attack_data.weapon_unit:base():is_category("smg", "lmg", "assault_rifle", "minigun") and managers.player:has_category_upgrade("weapon", "automatic_head_shot_add") then
+			attack_data.add_head_shot_mul = managers.player:upgrade_value("weapon", "automatic_head_shot_add", nil)
+		end
+		if not head and attack_data.add_head_shot_mul and self._char_tweak and not self._char_tweak.must_headshot then
+			local mul = self._char_tweak.headshot_dmg_mul * attack_data.add_head_shot_mul + 1
+			damage = damage * mul
+		end
 		damage = self:_apply_damage_reduction(damage)
 		attack_data.raw_damage = damage
 		local damage_percent = math.ceil(math.clamp(damage / self._HEALTH_INIT_PRECENT, 1, self._HEALTH_GRANULARITY))
 		damage = damage_percent * self._HEALTH_INIT_PRECENT
 		damage, damage_percent = self:_apply_min_health_limit(damage, damage_percent)
-
 		if self._immortal then
 			damage = math.min(damage, self._health - 1)
 		end
-
-		if self._health <= damage then
+		if damage >= self._health then
 			if self:check_medic_heal() then
 				result = {
 					type = "healed",
@@ -291,8 +275,7 @@ if SC and SC._data.sc_player_weapon_toggle or restoration and restoration.Option
 			else
 				if head then
 					managers.player:on_lethal_headshot_dealt(attack_data.attacker_unit, attack_data)
-
-					if math.random(10) < damage then
+					if damage > math.random(10) then
 						self:_spawn_head_gadget({
 							position = attack_data.col_ray.body:position(),
 							rotation = attack_data.col_ray.body:rotation(),
@@ -300,13 +283,11 @@ if SC and SC._data.sc_player_weapon_toggle or restoration and restoration.Option
 						})
 					end
 				end
-
 				attack_data.damage = self._health
 				result = {
 					type = "death",
 					variant = attack_data.variant
 				}
-
 				self:die(attack_data)
 				self:chk_killshot(attack_data.attacker_unit, "bullet", headshot)
 			end
@@ -317,13 +298,10 @@ if SC and SC._data.sc_player_weapon_toggle or restoration and restoration.Option
 				type = result_type,
 				variant = attack_data.variant
 			}
-
 			self:_apply_damage_to_health(damage)
 		end
-
 		attack_data.result = result
 		attack_data.pos = attack_data.col_ray.position
-
 		if result.type == "death" then
 			local data = {
 				name = self._unit:base()._tweak_table,
@@ -332,27 +310,20 @@ if SC and SC._data.sc_player_weapon_toggle or restoration and restoration.Option
 				weapon_unit = attack_data.weapon_unit,
 				variant = attack_data.variant
 			}
-
 			if managers.groupai:state():all_criminals()[attack_data.attacker_unit:key()] then
 				managers.statistics:killed_by_anyone(data)
 			end
-
 			if attack_data.attacker_unit == managers.player:player_unit() then
 				local special_comment = self:_check_special_death_conditions(attack_data.variant, attack_data.col_ray.body, attack_data.attacker_unit, attack_data.weapon_unit)
-
 				self:_comment_death(attack_data.attacker_unit, self._unit, special_comment)
 				self:_show_death_hint(self._unit:base()._tweak_table)
-
 				local attacker_state = managers.player:current_state()
 				data.attacker_state = attacker_state
-
 				managers.statistics:killed(data)
 				self:_check_damage_achievements(attack_data, head)
-
 				if not is_civilian and managers.player:has_category_upgrade("temporary", "overkill_damage_multiplier") and not attack_data.weapon_unit:base().thrower_unit and attack_data.weapon_unit:base():is_category("shotgun", "saw") then
 					managers.player:activate_temporary_upgrade("temporary", "overkill_damage_multiplier")
 				end
-
 				if is_civilian then
 					managers.money:civilian_killed()
 				end
@@ -361,23 +332,18 @@ if SC and SC._data.sc_player_weapon_toggle or restoration and restoration.Option
 			elseif attack_data.attacker_unit:base().sentry_gun then
 				if Network:is_server() then
 					local server_info = attack_data.weapon_unit:base():server_information()
-
 					if server_info and server_info.owner_peer_id ~= managers.network:session():local_peer():id() then
 						local owner_peer = managers.network:session():peer(server_info.owner_peer_id)
-
 						if owner_peer then
 							owner_peer:send_queued_sync("sync_player_kill_statistic", data.name, data.head_shot and true or false, data.weapon_unit, data.variant, data.stats_name)
 						end
 					else
 						data.attacker_state = managers.player:current_state()
-
 						managers.statistics:killed(data)
 					end
 				end
-
 				local sentry_attack_data = deep_clone(attack_data)
 				sentry_attack_data.attacker_unit = attack_data.attacker_unit:base():get_owner()
-
 				if sentry_attack_data.attacker_unit == managers.player:player_unit() then
 					self:_check_damage_achievements(sentry_attack_data, head)
 				else
@@ -385,34 +351,28 @@ if SC and SC._data.sc_player_weapon_toggle or restoration and restoration.Option
 				end
 			end
 		end
-
 		local hit_offset_height = math.clamp(attack_data.col_ray.position.z - self._unit:movement():m_pos().z, 0, 300)
 		local attacker = attack_data.attacker_unit
-
 		if attacker:id() == -1 then
 			attacker = self._unit
 		end
-
 		local weapon_unit = attack_data.weapon_unit
-
 		if alive(weapon_unit) and weapon_unit:base() and weapon_unit:base().add_damage_result then
 			weapon_unit:base():add_damage_result(self._unit, result.type == "death", attacker, damage_percent)
 		end
-
-		local variant = nil
-
+		local variant
 		if result.type == "knock_down" then
 			variant = 1
 		elseif result.type == "stagger" then
 			variant = 2
 			self._has_been_staggered = true
+		elseif result.type == "healed" then
+			variant = 3
 		else
-			variant = result.type == "healed" and 3 or 0
+			variant = 0
 		end
-
 		self:_send_bullet_attack_result(attack_data, attacker, damage_percent, body_index, hit_offset_height, variant)
 		self:_on_damage_received(attack_data)
-
 		return result
 	end
 

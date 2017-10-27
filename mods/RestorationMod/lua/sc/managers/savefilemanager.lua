50725ece807fe3c2b1e0b1593156bc0bcfe3608d
GHBDougley <douglaskmarx@gmail.com>
2017-10-27 16:41:43 -0600

    savefile 77

--- mods/RestorationMod/lua/sc/managers/savefilemanager.lua
+++ mods/RestorationMod/lua/sc/managers/savefilemanager.lua
@@ -1,12 +1,7 @@
---for the love of god dont edit this
+--don't edit or you die
 
---[[
-
-Disabled these for now cause I'm fucking terrified of this and I'd rather not mess with it for now until we have a game plan. 
-At least we know HOW to do this now. -SC
 
 if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue("SC/SC") then
 	SavefileManager.PROGRESS_SLOT = SystemInfo:platform() == Idstring("WIN32") and 77
 	SavefileManager.BACKUP_SLOT = SystemInfo:platform() == Idstring("WIN32") and 77
 end
-]]--

eb8c7c42063c8f86d0ddff07dc4bcf154126ef31
SC <gametwisterdotnet@yahoo.com>
2017-12-20 01:05:47 -0800

    20122017
    -Reverted the stuff for CopActionShoot. A lot of testing was done and the only result I got was a slightly worse version of what we had.

--- mods/RestorationMod/lua/sc/units/enemies/actions/upper_body/copactionshoot.lua
+++ mods/RestorationMod/lua/sc/units/enemies/actions/upper_body/copactionshoot.lua
@@ -32,18 +32,15 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 		local _time = math.floor(TimerManager:game():time())
 		self._throw_projectile_time = self._throw_projectile_time or 0
 		if self._unit:base()._tweak_table == "boom" and self._throw_projectile_time < _time then
-			if self._shooting_player or self._shooting_husk_player then
+			if self._shooting_player then
 				self._throw_projectile_time = _time + math.round_with_precision(10, 2)
 				shoot_from_pos = shoot_from_pos + Vector3(50, 50, 0)
 				target_pos, target_vec, target_dis, autotarget = _f_CopActionShoot__get_target_pos(self, shoot_from_pos, ...)
 				local roll = math.rand(1, 100)
-				local chance_gas = 100
+				local chance_gas = 25
 				if roll <= chance_gas then
 					self._unit:base():play_voiceline(_G.restoration.BufferedSounds.grenadier.use_gas, true)
-					local detonate_pos = target_pos + Vector3(0, 0, -120)
-					if Network:is_server() then
-						managers.groupai:state():detonate_cs_grenade(detonate_pos, nil, 7.5)
-					end
+					deploy_gas(shoot_from_pos, target_vec)
 				else
 					local dildo = _G.restoration.BufferedSounds.grenadier.spot_heister
 					local voiceline_to_use = dildo[math.random(#dildo)]
@@ -63,17 +60,14 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 				target_pos, target_vec, target_dis, autotarget = _f_CopActionShoot__get_target_pos(self, shoot_from_pos, ...)
 			end
 		elseif self._unit:base()._tweak_table == "rboom" and self._throw_projectile_time < _time then
-			if self._shooting_player or self._shooting_husk_player then
+			if self._shooting_player then
 				self._throw_projectile_time = _time + math.round_with_precision(10, 2)
 				shoot_from_pos = shoot_from_pos + Vector3(50, 50, 0)
 				target_pos, target_vec, target_dis, autotarget = _f_CopActionShoot__get_target_pos(self, shoot_from_pos, ...)
 				local roll = math.rand(1, 100)
 				local chance_gas = 25
 				if roll <= chance_gas then
-					local detonate_pos = target_pos + Vector3(0, 0, -120)
-					if Network:is_server() then
-						managers.groupai:state():detonate_cs_grenade(detonate_pos, nil, 7.5)
-					end		
+					deploy_gas(shoot_from_pos, target_vec)
 				end
 			else
 				target_pos, target_vec, target_dis, autotarget = _f_CopActionShoot__get_target_pos(self, shoot_from_pos, ...)
@@ -93,7 +87,15 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 		end
 		return target_pos, target_vec, target_dis, autotarget
 	end
-	
+
+	function deploy_gas(shoot_from_pos, target_vec)
+		local Net = _G.LuaNetworking
+		local z_fix = {-0.05, -0.02, -0.05, -0.02, -0.07, -0.07, -0.1}
+		target_vec = target_vec + Vector3(0, 0, z_fix[math.random(7)])
+		local detonate_pos = managers.player:player_unit():position()
+		managers.groupai:state():detonate_cs_grenade(detonate_pos, nil, 7.5)
+	end
+
 	function deploy_flash(shoot_from_pos, target_vec)
 		local Net = _G.LuaNetworking
 		local z_fix = {-0.05, -0.02, -0.05, -0.02, -0.07, -0.07, -0.1}

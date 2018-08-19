373b71cc14e7eb3df4888d500fd3d283b378eacd
Daiquiri <douglaskmarx@gmail.com>
2018-08-18 18:14:36 -0600

    alex must die TEST

---
+++ mods/RestorationMod/Corepre.lua
@@ -0,0 +1,5 @@
+RestorationMapFramework = RestorationMapFramework or class(MapFramework)
+RestorationMapFramework._directory = ModPath .. "map_replacements"
+RestorationMapFramework.type_name = "restoration"
+
+RestorationMapFramework:new()

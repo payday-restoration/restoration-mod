db03f7ef6846d022b3aa13a624f18a54821b911e
SC <gametwisterdotnet@yahoo.com>
2019-03-07 23:39:16 -0800

    07032019
    [*]Revamped custom spawngroup enabler code based off Frank's code. Should allow custom spawngroups but not allow dumb stuff like all enemies being able to use vents to happen.

--- lua/sc/managers/mission/elementspawnenemygroup.lua
+++ lua/sc/managers/mission/elementspawnenemygroup.lua
@@ -1,13 +1,27 @@
 if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue("SC/SC") then
+	groupsOLD = {
+		"tac_shield_wall_charge",
+		"FBI_spoocs",
+		"tac_tazer_charge",
+		"tac_tazer_flanking",
+		"tac_shield_wall",
+		"tac_swat_rifle_flank",
+		"tac_shield_wall_ranged",
+		"tac_bull_rush"
+	}
 
-	function ElementSpawnEnemyGroup:spawn_groups()
-		local opt = {}
-		for cat_name, team in pairs(tweak_data.group_ai.enemy_spawn_groups) do
-			if cat_name ~= "single_spooc" or cat_name ~= "Phalanx" then
-				table.insert(opt, cat_name)
+	local old_finalize_values = ElementSpawnEnemyGroup._finalize_values
+	function ElementSpawnEnemyGroup:_finalize_values()
+		old_finalize_values(self)
+
+		-- If we have an ordinary spawn with exactly the old group elements, add all defined groups.
+		local groups = self._values.preferred_spawn_groups
+		if groups and #groups == #groupsOLD and table.contains_all(groups, groupsOLD) then
+			for name,_ in pairs(tweak_data.group_ai.enemy_spawn_groups) do
+				if not table.contains(groups, name) then
+					table.insert(groups, name)
+				end
 			end
 		end
-		return opt
 	end
-
 end

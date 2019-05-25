bce9d7b9f1ef405545587057c629702a760a35aa
Rhynne <douglaskmarx@gmail.com>
2019-05-25 16:57:24 -0600

    setting lock

--- Corepre.lua
+++ Corepre.lua
@@ -1,7 +1,8 @@
 --Map Replacements (WIP)
-
+if restoration and restoration.Options:GetValue("SC/SC") then
 RestorationMapFramework = RestorationMapFramework or class(MapFramework)
 RestorationMapFramework._directory = ModPath .. "map_replacements"
 RestorationMapFramework.type_name = "restoration"
 
 RestorationMapFramework:new()
+end

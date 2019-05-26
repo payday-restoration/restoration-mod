089b9688781d4f3513c810e2799f86361656ba08
Rhynne <douglaskmarx@gmail.com>
2019-05-25 19:00:40 -0600

    fix for maps

--- Corepre.lua
+++ Corepre.lua
@@ -1,8 +1,9 @@
 --Map Replacements (WIP)
-if restoration and restoration.Options:GetValue("SC/SC") then
 RestorationMapFramework = RestorationMapFramework or class(MapFramework)
+
 RestorationMapFramework._directory = ModPath .. "map_replacements"
 RestorationMapFramework.type_name = "restoration"
 
+if restoration and restoration.Options:GetValue("SC/SC") then
 RestorationMapFramework:new()
 end

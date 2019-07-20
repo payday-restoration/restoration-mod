0c510b56ac622abece4bb2a8a730f2c2e999bb31
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2019-07-20 21:11:37 +0100

    Attempt at fixing #68
    Currently dev and gold users have the same matchmatching key

--- lua/sc/network/matchmaking/networkmatchmakingsteam.lua
+++ lua/sc/network/matchmaking/networkmatchmakingsteam.lua
@@ -1,5 +1,10 @@
 if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue("SC/SC") then
 
-	NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = "restoration_9.0"
-
+	if SystemFS:exists("mods/restoration-mod-dev/mod.txt") then
+		NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = "restoration_dev"
+		log(NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY)
+	elseif SystemFS:exists("mods/restoration-mod-gold/mod.txt") then
+		NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = "restoration_gold"
+		log(NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY)
+	end
 end

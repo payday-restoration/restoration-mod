0d09c06ebcb6973f56933310f16d9dcfbe8e21fd
steam-test1 <steam-test1@users.noreply.github.com>
2019-07-21 17:00:52 +0100

    SuperBLT Assets Part 1
    Converted akan and zombie assets to SuperBLT assets system
    The release version of SuperBLT no longer crashes on sta\rtup
    Needs multiplayer testing

--- Corepre.lua
+++ Corepre.lua
@@ -5,10 +5,15 @@ RestorationMapFramework._directory = ModPath .. "map_replacements"
 RestorationMapFramework.type_name = "restoration"
 
 if restoration and restoration.Options:GetValue("SC/SC") then
-RestorationMapFramework:new()
-if not PackageManager:loaded("packages/scassets") then
-	PackageManager:load("packages/scassets")
-end
+	RestorationMapFramework:new()
+	if not PackageManager:loaded("packages/scassets") then
+		PackageManager:load("packages/scassets")
+	end
+	local restoration_mod_instance = ModInstance
+	log("Loading Russian Cops")
+	restoration_mod_instance:GetSuperMod():GetAssetLoader():LoadAssetGroup("akan")
+	log("Loading Zombie Cops")
+	restoration_mod_instance:GetSuperMod():GetAssetLoader():LoadAssetGroup("zombie")
 end
 -- Always load
 if not PackageManager:loaded("packages/addhudmisc") then

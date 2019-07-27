1b0fa1bbf93f18bc280518f90824d2989553a6d7
Skwuruhl <smmarion11@gmail.com>
2019-07-27 01:02:05 -0700

    spread pattern fix
    tweak_data.weapon.spread.dispersion is defaulted to 1.0 which distributes shots evenly by radius. However by area shots will be weighted towards the middle of your spread, if you want spread to distribute evenly by area change this value to 0.5. Just note this will have the opposite effect and shots will be weighted to the outside of your spread radius.
    
    Personally I think 1.0 is the better value as the radius of spread is what makes your shot hit or miss but it is visually weighted to the center.

--- lua/sc/units/weapons/newflamethrowerbase.lua
+++ lua/sc/units/weapons/newflamethrowerbase.lua
@@ -96,12 +96,13 @@ function NewFlamethrowerBase:_fire_raycast(user_unit, from_pos, direction, dmg_m
 	local up = direction:cross(right):normalized()
 	mvector3.set(mvec_direction, direction)
 	for i = 1, shoot_through_data and 1 or self._rays do
+		local r = math.pow(math.random(), tweak_data.weapon.spread.dispersion)
 		local theta = math.random() * 360
-		local ax = math.sin(theta) * (math.random() * spread_x) * (spread_mul or 1)
-		local ay = math.cos(theta) * (math.random() * spread_y) * (spread_mul or 1)
+		local ax = math.tan(r * spread_x * (spread_mul or 1)) * math.cos(theta)
+		local ay = math.tan(r * spread_y * (spread_mul or 1)) * math.sin(theta) * -1
 		mvector3.set(mvec_spread_direction, mvec_direction)
-		mvector3.add(mvec_spread_direction, right * math.rad(ax))
-		mvector3.add(mvec_spread_direction, up * math.rad(ay))
+		mvector3.add(mvec_spread_direction, right * ax)
+		mvector3.add(mvec_spread_direction, up * ay)
 		mvector3.set(mvec_to, mvec_spread_direction)
 		mvector3.multiply(mvec_to, 20000)
 		mvector3.add(mvec_to, from_pos)

c66ff6ef70c6933741bd783e2642a2a056c08841
steam-test1 <steam-test1@users.noreply.github.com>
2019-08-17 07:53:26 +0100

    Added missing value
    Increased the load wait time
    This should give extra time for clients to load custom packages

--- lua/sc/network/base/basenetworksession.lua
+++ lua/sc/network/base/basenetworksession.lua
@@ -1,4 +1,5 @@
 if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue("SC/SC") then
+    BaseNetworkSession.CONNECTION_TIMEOUT = 20
     BaseNetworkSession.LOADING_CONNECTION_TIMEOUT = SystemInfo:platform() == Idstring("WIN32") and 40
-    BaseNetworkSession._LOAD_WAIT_TIME = 6
+    BaseNetworkSession._LOAD_WAIT_TIME = 10
 end

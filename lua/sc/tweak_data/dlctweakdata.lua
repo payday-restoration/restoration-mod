7f35684c2beb9d7ea8bc46aef24ffdde2acbb777
Nelson01023 <nanny01023@gmail.com>
2019-10-30 15:25:15 -0400

    reverted dlctweakdata change
    should deal with the locked item problem, hopefully.
    
    didn't touch dlcmanager cause i dont think its the main source

--- lua/sc/tweak_data/dlctweakdata.lua
+++ lua/sc/tweak_data/dlctweakdata.lua
@@ -1,13 +1,20 @@
+old_DLCTweakData_init = DLCTweakData.init
+function DLCTweakData:init(...)
+	old_DLCTweakData_init(self, ...)
+	
 	if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue("SC/SC") then
-
-	old_DLCTweakData_init = DLCTweakData.init
-	function DLCTweakData:init(tweak_data)
-		old_DLCTweakData_init(self, tweak_data)
-		self.rest_new = {}
-		self.rest_new.free = true
-		self.rest_new.content = {}
-		self.rest_new.content.loot_global_value = "rest"
-		self.rest_new.content.loot_drops = {
+		self.rest = {
+			content = {},
+			free = true
+		}
+		self.rest.content.loot_drops = {}
+		self.rest.content.upgrades = {}	
+		self.wetwork_masks = {
+			content = {},
+			free = true
+		}
+		self.wetwork_masks.content.loot_global_value = "rest"
+		self.wetwork_masks.content.loot_drops = {
 			{
 				type_items = "masks",
 				item_entry = "shatter_true",
@@ -174,12 +181,14 @@
 				amount = 1
 			}			
 		}
-
-		self.sc_new = {}
-		self.sc_new.free = true
-		self.sc_new.content = {}
-		self.sc_new.content.loot_global_value = "sc"
-		self.sc_new.content.loot_drops = {
+	end
+	
+	if SC and SC._data.sc_player_weapon_toggle or restoration and restoration.Options:GetValue("SC/SCWeapon") then
+		self.sc = {}
+		self.sc.free = true
+		self.sc.content = {}
+		self.sc.content.loot_global_value = "sc"
+		self.sc.content.loot_drops = {
 			{
 				type_items = "weapon_mods",
 				item_entry = "wpn_fps_upg_ammo_half_that",

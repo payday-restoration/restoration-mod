bc0726cd32b6fb9a293a6a6cd87f2e6bb093730f
SC <gametwisterdotnet@yahoo.com>
2019-11-08 19:29:57 -0800

    11082019
    [*]Temporarily disabled faction replacer mutators.

--- lua/sc/managers/mutatorsmanager.lua
+++ lua/sc/managers/mutatorsmanager.lua
@@ -30,8 +30,8 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 			MutatorMoreDonutsPlus:new(self),
 			MutatorJungleInferno:new(self),
 			MutatorNoTitans:new(self),
-			MutatorFactionsReplacer:new(self),
-			MutatorZombieOutbreak:new(self)
+			--MutatorFactionsReplacer:new(self),
+			--MutatorZombieOutbreak:new(self)
 		}
 		self._active_mutators = {}
 		local activate = Global.mutators and Global.mutators.active_on_load

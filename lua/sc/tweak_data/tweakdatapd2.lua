744272db3e6f18b2433e411ac3a3bac87e59b4e1
Nelson01023 <nanny01023@gmail.com>
2019-11-10 13:46:40 -0500

    better way of doin it (thx zdann)

--- lua/sc/tweak_data/tweakdatapd2.lua
+++ lua/sc/tweak_data/tweakdatapd2.lua
@@ -2,60 +2,7 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 
 -- Custom suit poses
 Hooks:PostHook(TweakData, "_setup_scene_poses", "assfucker9000", function(self)
-	self.scene_poses.player_style = {
-		generic = {
-			"standard_suit",
-			"standard_suit"
-		},
-		sneak_suit = {
-			"sneak",
-			"sneak"
-		},
-		scrub = {
-			"doctor",
-			"doctor"
-		},
-		raincoat = {
-			"rain",
-			"rain"
-		},
-		murky_suit = {
-			"military",
-			"military"
-		},
-		tux = {
-			"tuxedo",
-			"tuxedo"
-		},
-		winter_suit = {
-			"winter",
-			"winter"
-		},
-		jumpsuit = {
-			"jump",
-			"jump"
-		},
-		clown = {
-			"clown",
-			"clown"
-		},
-		peacoat = {
-			"peacoat",
-			"peacoat"
-		},
-		miami = {
-			"italian",
-			"italian"
-		},
-		loud_suit = {
-			"military",
-			"military"
-		},		
-		jail = {
-			"standard_suit",
-			"standard_suit"
-		}
-	}
+	self.scene_poses.player_style.loud_suit = {"military", "military"}
 end)
 
 end

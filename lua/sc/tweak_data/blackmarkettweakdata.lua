ffaa30f8575e389c1f12e5b0af6d2f382f472899
EvilBobarino <40873080+EvilBobarino@users.noreply.github.com>
2019-11-18 11:25:59 +0000

    loud suit wrists

--- lua/sc/tweak_data/blackmarkettweakdata.lua
+++ lua/sc/tweak_data/blackmarkettweakdata.lua
@@ -581,43 +581,44 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 		set_characters_data("loud_suit", characters_male, {
 			body_replacement = body_replacement_armor,
 			third_body_replacement = body_replacement_armor,
-			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit_2",
+			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit",
 			third_unit = "units/pd2_mod_suits/characters/res_acc_loud_suit/res_acc_loud_suit"
 		})
 		set_characters_data("loud_suit", characters_black_gloves, {
 			body_replacement = body_replacement_armor,
 			third_body_replacement = body_replacement_armor,
-			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit_2",
+			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit",
 			third_unit = "units/pd2_mod_suits/characters/res_acc_loud_suit/res_acc_loud_suit"
 		})
 		set_characters_data("loud_suit", characters_tony, {
 			body_replacement = body_replacement_armor,
 			third_body_replacement = body_replacement_armor,
-			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit_2",
+			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit",
 			third_unit = "units/pd2_mod_suits/characters/res_acc_loud_suit/res_acc_loud_suit"
 		})
 		set_characters_data("loud_suit", characters_bodhi, {
 			body_replacement = body_replacement_armor,
 			third_body_replacement = body_replacement_armor,
-			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit_2",
+			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit",
 			third_unit = "units/pd2_mod_suits/characters/res_acc_loud_suit/res_acc_loud_suit"
 		})
 		set_characters_data("loud_suit", characters_jimmy, {
 			body_replacement = body_replacement_armor,
 			third_body_replacement = body_replacement_armor,
+			disable_wrists = true,
 			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit_2",
 			third_unit = "units/pd2_mod_suits/characters/res_acc_loud_suit/res_acc_loud_suit"
 		})
 		set_characters_data("loud_suit", characters_chains, {
 			body_replacement = body_replacement_armor,
 			third_body_replacement = body_replacement_armor,
-			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit_2",
+			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit",
 			third_unit = "units/pd2_mod_suits/characters/res_acc_loud_suit/res_acc_loud_suit"
 		})
 		set_characters_data("loud_suit", characters_sangres, {
 			body_replacement = body_replacement_armor,
 			third_body_replacement = body_replacement_armor,
-			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit_2",
+			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit",
 			third_unit = "units/pd2_mod_suits/characters/res_acc_loud_suit/res_acc_loud_suit"
 		})
 		set_characters_data("loud_suit", characters_jacket, {
@@ -629,36 +630,38 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 		set_characters_data("loud_suit", characters_male_big, {
 			body_replacement = body_replacement_armor,
 			third_body_replacement = body_replacement_standard,
-			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit_2",
+			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit",
 			third_unit = "units/pd2_mod_suits/characters/res_acc_loud_suit_male_big/res_acc_loud_suit_male_big"
 		})
 		set_characters_data("loud_suit", characters_worst_addition, {
 			body_replacement = body_replacement_armor,
 			third_body_replacement = body_replacement_standard,
-			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit_2",
+			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit",
 			third_unit = "units/pd2_mod_suits/characters/res_acc_loud_suit_male_big/res_acc_loud_suit_male_big"
 		})
 		set_characters_data("loud_suit", characters_female, {
 			body_replacement = body_replacement_armor,
 			third_body_replacement = body_replacement_standard,
-			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit_2",
+			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit",
 			third_unit = "units/pd2_mod_suits/characters/res_acc_loud_suit_female_thin/res_acc_loud_suit_female_thin"
 		})
 		set_characters_data("loud_suit", characters_sydney, {
 			body_replacement = body_replacement_armor,
 			third_body_replacement = body_replacement_standard,
-			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit_2",
+			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit",
 			third_unit = "units/pd2_mod_suits/characters/res_acc_loud_suit_female_thin/res_acc_loud_suit_female_thin"
 		})
 		set_characters_data("loud_suit", characters_joy, {
 			body_replacement = body_replacement_armor,
 			third_body_replacement = body_replacement_standard,
+			disable_wrists = true,
 			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit_2",
 			third_unit = "units/pd2_mod_suits/characters/res_acc_loud_suit_female_thin/res_acc_loud_suit_female_thin"
 		})
 		set_characters_data("loud_suit", characters_female_big, {
 			body_replacement = body_replacement_armor,
 			third_body_replacement = body_replacement_standard,
+			disable_wrists = true,
 			unit = "units/pd2_mod_suits/characters/res_acc_fps_loud_suit/res_acc_fps_loud_suit_2",
 			third_unit = "units/pd2_mod_suits/characters/res_acc_loud_suit_female_big/res_acc_loud_suit_female_big"
 		})	

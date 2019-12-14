37dcadaf076e73f1ccee72d04173f0e6337f90df
EvilBobarino <40873080+EvilBobarino@users.noreply.github.com>
2019-12-14 17:55:09 +0000

    putting these back

--- lua/sc/tweak_data/levelstweakdata.lua
+++ lua/sc/tweak_data/levelstweakdata.lua
@@ -153,6 +153,8 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 		self.kosugi.package = {"packages/kosugi", "packages/addbdu"}
 		self.kosugi.player_style = "sneak_suit"		
 		
+		self.hox_1.player_style = "suit_sunny"
+		self.hox_2.player_style = "suit_sunny"
 		self.hox_3.player_style = "suit_sunny"
 		
 		self.rat.player_style = "suit_sunny"

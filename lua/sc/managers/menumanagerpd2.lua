343b6f87b20cbc6330943d3900be2c84e2ed1d31
steam-test1 <steam-test1@users.noreply.github.com>
2019-12-14 01:14:54 +0000

    Menu Tweaks
    Added Crew Management to the lobby menu (Host only)
    Added Side Jobs link to clients in lobby and crimespree lobby

--- lua/sc/managers/menumanagerpd2.lua
+++ lua/sc/managers/menumanagerpd2.lua
@@ -8,6 +8,14 @@ function MenuCallbackHandler:on_visit_res_guide()
 	end
 end
 
+function MenuCallbackHandler:open_crew_management()
+	managers.menu:open_node("crew_management")
+end
+
+function MenuCallbackHandler:open_side_jobs()
+	managers.menu:open_node("side_jobs")
+end
+
 function MenuCallbackHandler:SCEnabled()
 	if restoration and restoration.Options:GetValue("SC/SC") then
 		return true

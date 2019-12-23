b5e7a7cdc85ab874d9b319213ad0a5afecf22791
Nelson01023 <nanny01023@gmail.com>
2019-12-22 20:45:23 -0500

    reinbeck tweaks
    - made reload exit times much faster cause there's a split second of no shooty
    - made pump speed faster resolve no shooting for split second
    - game sucks

--- lua/sc/tweak_data/weapontweakdata.lua
+++ lua/sc/tweak_data/weapontweakdata.lua
@@ -11988,8 +11988,6 @@ if SC and SC._data.sc_player_weapon_toggle or restoration and restoration.Option
 		self.beck.rays = 9
 		self.beck.timers = {
 			shotgun_reload_enter = 0.4,
-			shotgun_reload_exit_empty = 1.3,
-			shotgun_reload_exit_not_empty = 1,
 			shotgun_reload_shell = 0.6666667,
 			shotgun_reload_first_shell_offset = 0.33666667,
 			unequip = 0.5,
@@ -12006,8 +12004,8 @@ if SC and SC._data.sc_player_weapon_toggle or restoration and restoration.Option
 		self.beck.kick.standing = self.new_m4.kick.standing
 		self.beck.kick.crouching = self.new_m4.kick.standing
 		self.beck.kick.steelsight = self.new_m4.kick.standing
-		self.beck.single.fire_rate = 0.8
-		self.beck.fire_mode_data.fire_rate = 0.8
+		self.beck.single.fire_rate = 0.65
+		self.beck.fire_mode_data.fire_rate = 0.65
 		self.beck.AMMO_MAX = 40 
 		self.beck.damage_near = 600
 		self.beck.damage_far = 1200

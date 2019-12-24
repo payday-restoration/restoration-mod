3206497ca043357f7aa4a0cea6098053e9177ae2
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2019-12-24 21:31:48 +0000

    Fixed grenade crates been purchased in pro jobs

--- lua/sc/managers/menumanager.lua
+++ lua/sc/managers/menumanager.lua
@@ -33,5 +33,35 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 			MenuCallbackHandler:start_job(job_data)
 		end
 	end
+	
+	function MenuPrePlanningInitiator:set_locks_to_param(params, key, index)
+		local data = tweak_data:get_raw_value("preplanning", key, index) or {}
+		local enabled = params.enabled ~= false
+		params.tooltip = params.tooltip or {}
+		params.tooltip.errors = params.tooltip.errors or {}
+	
+		if data.dlc_lock and not Global.game_settings and Global.game_settings.one_down then
+			local dlc_unlocked = managers.dlc:is_dlc_unlocked(data.dlc_lock)
+	
+			if not dlc_unlocked then
+				local error_text_id = tweak_data:get_raw_value("lootdrop", "global_values", data.dlc_lock, "unlock_id")
+	
+				table.insert(params.tooltip.errors, managers.localization:to_upper_text(error_text_id))
+	
+				enabled = false
+			end
+		elseif data.upgrade_lock then
+			local upgrade_unlocked = managers.player:has_category_upgrade(data.upgrade_lock.category, data.upgrade_lock.upgrade)
+	
+			if not upgrade_unlocked then
+				table.insert(params.tooltip.errors, managers.localization:to_upper_text("menu_asset_lock_" .. data.upgrade_lock.upgrade))
+	
+				enabled = false
+			end
+		end
+	
+		params.enabled = enabled
+		params.ignore_disabled = true
+	end
 
 end

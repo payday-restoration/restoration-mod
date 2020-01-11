f1f295b2c967fd07c91ef2a503a4e6183281a652
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-01-11 19:32:54 -0300

    Prevent enemy marking from interrupting bots
    Namely, if they're shooting or reloading.
    
    Leaving the "arrest" animation for now instead of replacing it with "cmd_point", as this still uses vanilla code to find enemies to mark, and that doesn't care about actual eye LoS, just that enemy is verified.

--- lua/sc/units/player_team/logics/teamailogicassault.lua
+++ lua/sc/units/player_team/logics/teamailogicassault.lua
@@ -1,5 +1,4 @@
 if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue("SC/SC") then
-
 	if SystemFS:exists("mods/Better Bots/mod.txt") then
 		return
 	end
@@ -8,17 +7,24 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 		if play_sound then
 			criminal:sound():say(to_mark:base():char_tweak().bot_priority_shout, true)
 		end
-		if play_action and not criminal:movement():chk_action_forbidden("action") then
-			local new_action = {
-				type = "act",
-				variant = "arrest",
-				body_part = 3,
-				align_sync = true
-			}
-			if criminal:brain():action_request(new_action) then
-				data.internal_data.gesture_arrest = true
+
+		if play_action then
+			local can_play_action = not criminal:movement():chk_action_forbidden("action") and not criminal:anim_data().reload and not data.internal_data.firing and not data.internal_data.shooting
+
+			if can_play_action then
+				local new_action = {
+					type = "act",
+					variant = "arrest",
+					body_part = 3,
+					align_sync = true
+				}
+
+				if criminal:brain():action_request(new_action) then
+					data.internal_data.gesture_arrest = true
+				end
 			end
 		end
+
 		to_mark:contour():add("mark_enemy", true)
 	end
 

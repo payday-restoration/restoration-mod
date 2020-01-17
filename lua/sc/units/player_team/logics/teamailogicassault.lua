c685e3d7337a550ab105bed4433da75e1ab6a334
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-01-16 23:48:28 -0300

    Improved vanilla bot enemy marking a bit + turret marking
    Regarding the improvements, now it checks if the unit causes a reaction (and a combat one) before anything else, then checks if it can use contours. Then, proceeds as usual, but also checks if the unit isn't already marked (using red/pink contours, so that Medic healing contours won't invalidate this) before actually deciding if it's a valid unit to mark based on closest distance.

--- lua/sc/units/player_team/logics/teamailogicassault.lua
+++ lua/sc/units/player_team/logics/teamailogicassault.lua
@@ -3,9 +3,53 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 		return
 	end
 
+	function TeamAILogicAssault.find_enemy_to_mark(enemies)
+		local best_nmy, best_nmy_wgt = nil
+
+		for key, attention_info in pairs(enemies) do
+			if attention_info.reaction and AIAttentionObject.REACT_COMBAT <= attention_info.reaction and attention_info.unit:contour() and attention_info.identified then
+				if attention_info.verified or attention_info.nearly_visible then
+					if attention_info.is_deployable or attention_info.is_person and attention_info.char_tweak and attention_info.char_tweak.priority_shout then
+						if not attention_info.char_tweak or (not attention_info.char_tweak.priority_shout_max_dis or attention_info.dis < attention_info.char_tweak.priority_shout_max_dis) then
+							local att_contour = attention_info.unit:contour()
+							local mark = false
+
+							if not att_contour._contour_list then
+								mark = true
+							else
+								if attention_info.is_deployable then
+									if not att_contour:has_id("mark_unit_dangerous") and not att_contour:has_id("mark_unit_dangerous_damage_bonus") and not att_contour:has_id("mark_unit_dangerous_damage_bonus_distance") then
+										mark = true
+									end
+								else
+									if not att_contour:has_id("mark_enemy") and not att_contour:has_id("mark_enemy_damage_bonus") and not att_contour:has_id("mark_enemy_damage_bonus_distance") then
+										mark = true
+									end
+								end
+							end
+
+							if mark then
+								if not best_nmy_wgt or attention_info.verified_dis < best_nmy_wgt then
+									best_nmy_wgt = attention_info.verified_dis
+									best_nmy = attention_info.unit
+								end
+							end
+						end
+					end
+				end
+			end
+		end
+
+		return best_nmy
+	end
+
 	function TeamAILogicAssault.mark_enemy(data, criminal, to_mark, play_sound, play_action)
 		if play_sound then
-			criminal:sound():say(to_mark:base():char_tweak().bot_priority_shout, true)
+			if to_mark:base().sentry_gun then
+				criminal:sound():say("f44x_any", true)
+			else
+				criminal:sound():say(to_mark:base():char_tweak().bot_priority_shout, true)
+			end
 		end
 
 		if play_action then
@@ -25,7 +69,11 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 			end
 		end
 
-		to_mark:contour():add("mark_enemy", true)
+		if to_mark:base().sentry_gun then
+			to_mark:contour():add("mark_unit_dangerous", true)
+		else
+			to_mark:contour():add("mark_enemy", true)
+		end
 	end
 
 end

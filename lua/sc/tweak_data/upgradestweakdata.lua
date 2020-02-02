95359aee2435fa43446b6f7e0f472e078d5fc6ca
Ravicale <rjwiedenroth@crimson.ua.edu>
2020-02-01 20:48:04 -0600

    Undid Hacker Changes not ready for daylight.

--- lua/sc/tweak_data/upgradestweakdata.lua
+++ lua/sc/tweak_data/upgradestweakdata.lua
@@ -1635,7 +1635,7 @@ function UpgradesTweakData:_init_pd2_values()
 	--Hacker
 	self.values.player.pocket_ecm_jammer_base = {
 		{
-			cooldown_drain = 2,
+			cooldown_drain = 4,
 			duration = 12
 		}
 	}	

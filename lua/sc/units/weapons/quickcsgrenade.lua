af4493e01b7a7dd4dde4043148b6028dab588df1
SC <gametwisterdotnet@yahoo.com>
2020-02-03 14:32:07 -0800

    02032020
    [*]Made the 'unknown' heist tag more consistent with other heist text.
    [*]Lowered the threshold for an alarm in stealth, this means you can afford fewer mistakes.
    [*]Disabled bots being able to dodge, this proved to be counter-productive as it did nothing for them besides looking cool and making them waste time dodging instead of shooting.
    [*]Fixed bot grace values being inconsistent with the player.
    [*]Restored tick rates on gas grenades to once again be consistent with player grace values, adjusted damage to health and stamina to roughly match DPS from gas.

--- lua/sc/units/weapons/quickcsgrenade.lua
+++ lua/sc/units/weapons/quickcsgrenade.lua
@@ -7,7 +7,7 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 		self._tweak_data = tweak_data.projectiles[grenade_entry]
 		self._radius = self._tweak_data.radius or 300
 		self._radius_blurzone_multiplier = self._tweak_data.radius_blurzone_multiplier or 1.3
-		self._damage_per_tick = 0.45
+		self._damage_per_tick = 0.3
 		self._stamina_per_tick = 0.0
 		if difficulty_index <= 2 then
 			self._damage_tick_period = 0.5
@@ -19,15 +19,15 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 			self._damage_tick_period = 0.35
 		elseif difficulty_index == 6 then
 			self._damage_tick_period = 0.3
-			self._damage_per_tick = 0.6
+			self._damage_per_tick = 0.45
 		elseif difficulty_index == 7 then
 			self._damage_tick_period = 0.3
-			self._stamina_per_tick = 1.5
-			self._damage_per_tick = 0.75			
+			self._stamina_per_tick = 1.25
+			self._damage_per_tick = 0.6			
 		else
-			self._damage_tick_period = 0.3
-			self._stamina_per_tick = 3
-			self._damage_per_tick = 0.9
+			self._damage_tick_period = 0.25
+			self._stamina_per_tick = 2.5
+			self._damage_per_tick = 0.75
 		end
 	end
 	

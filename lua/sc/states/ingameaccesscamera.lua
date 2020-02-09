8bc966907fcf582019fb02cdaf52a4857cb02492
Rhynne <douglaskmarx@gmail.com>
2020-02-09 06:42:41 -0600

    lod crash fix

--- lua/sc/states/ingameaccesscamera.lua
+++
@@ -1,7 +0,0 @@
-local at_enter_original = IngameAccessCamera.at_enter
-
-function IngameAccessCamera:at_enter(old_state, ...)
-    at_enter_original(self, old_state, ...)
-    
-	managers.enemy:set_gfx_lod_enabled(true)
-end

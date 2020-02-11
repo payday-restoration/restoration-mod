d4361afaa7a389a137927473805266389f236df8
Ravicale <rjwiedenroth@crimson.ua.edu>
2020-02-10 23:37:32 -0600

    Numbers

--- lua/sc/tweak_data/upgradestweakdata.lua
+++ lua/sc/tweak_data/upgradestweakdata.lua
@@ -1465,13 +1465,13 @@ function UpgradesTweakData:_init_pd2_values()
 
 	self.values.player.damage_to_armor = {
 		{
-			{1.8, 3},
-			{2.0, 3},
-			{2.2, 3},
+			{2.1, 3},
 			{2.4, 3},
-			{2.6, 3},
-			{2.8, 3},
-			{3.0, 3}
+			{2.7, 3},
+			{3.0, 3},
+			{3.2, 3},
+			{3.4, 3},
+			{3.6, 3}
 		}
 	}
 	

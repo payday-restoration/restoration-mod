0d16e28cb861a579d96fdb3ba5365bed7814da9e
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-02-16 03:27:33 -0300

    Sync uncloak sequence for Titan Spoocs/Autumn
    Plus a small change to change the slot of surrendered enemies.

---
+++ lua/sc/units/enemies/huskcopbrain.lua
@@ -0,0 +1,33 @@
+HuskCopBrain._NET_EVENTS = {
+	uncloak = 3,
+	weapon_laser_off = 2,
+	weapon_laser_on = 1
+}
+
+function HuskCopBrain:sync_surrender(surrendered)
+	if surrendered then
+		self._unit:base():set_slot(self._unit, 22)
+	else
+		self._unit:base():set_slot(self._unit, 12)
+	end
+
+	self._surrendered = surrendered
+end
+
+function HuskCopBrain:sync_net_event(event_id)
+	if event_id == self._NET_EVENTS.uncloak then
+		if self._unit:damage() and self._unit:damage():has_sequence("decloak") then
+			self._unit:damage():run_sequence_simple("decloak")
+
+			local weapon_unit = self._unit:inventory():equipped_unit()
+
+			if weapon_unit and weapon_unit:damage() and weapon_unit:damage():has_sequence("decloak") then
+				weapon_unit:damage():run_sequence_simple("decloak")
+			end
+		end
+	elseif event_id == self._NET_EVENTS.weapon_laser_on then
+		self._add_laser_t = HuskCopBrain._ENABLE_LASER_TIME
+	elseif event_id == self._NET_EVENTS.weapon_laser_off then
+		self:disable_weapon_laser()
+	end
+end

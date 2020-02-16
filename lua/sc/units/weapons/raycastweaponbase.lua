a90d5f5203d9e20729b82d96ed1a4762fb3561a3
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-02-16 07:30:14 -0300

    Better no duplicated bullets fix
    Works against anything, not just characters, so that player husks don't destroy things they're not supposed to.
    
    This also comes along with a friendly fire check for body extensions, like Dozer armor, a Shield's shield lights (or other potential body extensions on them), anything like that.

--- lua/sc/units/weapons/raycastweaponbase.lua
+++ lua/sc/units/weapons/raycastweaponbase.lua
@@ -311,5 +311,125 @@ if SC and SC._data.sc_player_weapon_toggle or restoration and restoration.Option
 
 		return result
 	end
-	
+
+	function InstantBulletBase:on_collision(col_ray, weapon_unit, user_unit, damage, blank, no_sound, already_ricocheted)
+		if Network:is_client() and not blank and user_unit ~= managers.player:player_unit() then
+			blank = true
+		end
+
+		local hit_unit = col_ray.unit
+		local is_shield = hit_unit:in_slot(managers.slot:get_mask("enemy_shield_check")) and alive(hit_unit:parent())
+
+		if alive(weapon_unit) and is_shield and weapon_unit:base()._shield_knock then
+			local enemy_unit = hit_unit:parent()
+
+			if enemy_unit:character_damage() and enemy_unit:character_damage().dead and not enemy_unit:character_damage():dead() then
+				if enemy_unit:base():char_tweak() then
+					if enemy_unit:base():char_tweak().damage.shield_knocked and not enemy_unit:character_damage():is_immune_to_shield_knockback() then
+						local MIN_KNOCK_BACK = 200
+						local KNOCK_BACK_CHANCE = 0.8
+						local dmg_ratio = math.min(damage, MIN_KNOCK_BACK)
+						dmg_ratio = dmg_ratio / MIN_KNOCK_BACK + 1
+
+						local rand = math.random() * dmg_ratio
+
+						if KNOCK_BACK_CHANCE < rand then
+							local damage_info = {
+								damage = 0,
+								type = "shield_knock",
+								variant = "melee",
+								col_ray = col_ray,
+								result = {
+									variant = "melee",
+									type = "shield_knock"
+								}
+							}
+
+							enemy_unit:character_damage():_call_listeners(damage_info)
+						end
+					end
+				end
+			end
+		end
+
+		local play_impact_flesh = not hit_unit:character_damage() or not hit_unit:character_damage()._no_blood
+
+		if hit_unit:damage() and managers.network:session() and col_ray.body:extension() and col_ray.body:extension().damage then
+			local damage_body_extension = true
+			local character_unit = nil
+
+			if hit_unit:character_damage() then
+				character_unit = hit_unit
+			elseif is_shield and hit_unit:parent():character_damage() then
+				character_unit = hit_unit:parent()
+			end
+
+			if character_unit and character_unit:character_damage().is_friendly_fire and character_unit:character_damage():is_friendly_fire(user_unit) then
+				damage_body_extension = false
+			end
+
+			--do a friendly fire check if the unit hit is a character or a character's shield before damaging the body extension that was hit
+			if damage_body_extension then
+				local sync_damage = not blank and hit_unit:id() ~= -1
+				local network_damage = math.ceil(damage * 163.84)
+				damage = network_damage / 163.84
+
+				if sync_damage then
+					local normal_vec_yaw, normal_vec_pitch = self._get_vector_sync_yaw_pitch(col_ray.normal, 128, 64)
+					local dir_vec_yaw, dir_vec_pitch = self._get_vector_sync_yaw_pitch(col_ray.ray, 128, 64)
+
+					managers.network:session():send_to_peers_synched("sync_body_damage_bullet", col_ray.unit:id() ~= -1 and col_ray.body or nil, user_unit:id() ~= -1 and user_unit or nil, normal_vec_yaw, normal_vec_pitch, col_ray.position, dir_vec_yaw, dir_vec_pitch, math.min(16384, network_damage))
+				end
+
+				local local_damage = not blank or hit_unit:id() == -1
+
+				if local_damage then
+					col_ray.body:extension().damage:damage_bullet(user_unit, col_ray.normal, col_ray.position, col_ray.ray, 1)
+					col_ray.body:extension().damage:damage_damage(user_unit, col_ray.normal, col_ray.position, col_ray.ray, damage)
+
+					if alive(weapon_unit) and weapon_unit:base().categories and weapon_unit:base():categories() then
+						for _, category in ipairs(weapon_unit:base():categories()) do
+							col_ray.body:extension().damage:damage_bullet_type(category, user_unit, col_ray.normal, col_ray.position, col_ray.ray, 1)
+						end
+					end
+				end
+			end
+		end
+
+		local result = nil
+
+		if alive(weapon_unit) and hit_unit:character_damage() and hit_unit:character_damage().damage_bullet then
+			local is_alive = not hit_unit:character_damage():dead()
+
+			if not blank then
+				local knock_down = weapon_unit:base()._knock_down and weapon_unit:base()._knock_down > 0 and math.random() < weapon_unit:base()._knock_down
+
+				result = self:give_impact_damage(col_ray, weapon_unit, user_unit, damage, weapon_unit:base()._use_armor_piercing, false, knock_down, weapon_unit:base()._stagger, weapon_unit:base()._variant)
+			end
+
+			local is_dead = hit_unit:character_damage():dead()
+
+			if not is_dead then
+				if not result or result == "friendly_fire" then
+					play_impact_flesh = false
+				end
+			end
+
+			local push_multiplier = self:_get_character_push_multiplier(weapon_unit, is_alive and is_dead)
+
+			managers.game_play_central:physics_push(col_ray, push_multiplier)
+		else
+			managers.game_play_central:physics_push(col_ray)
+		end
+
+		if play_impact_flesh then
+			managers.game_play_central:play_impact_flesh({
+				col_ray = col_ray,
+				no_sound = no_sound
+			})
+			self:play_impact_sound_and_effects(weapon_unit, col_ray, no_sound)
+		end
+
+		return result
+	end
 end

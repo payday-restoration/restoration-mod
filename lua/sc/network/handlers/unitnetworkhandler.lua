62a553405cf6db06836388cca4aca8f01e9080a6
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-03-08 21:31:04 -0300

    Don't block idle for clients for spooc actions

--- lua/sc/network/handlers/unitnetworkhandler.lua
+++ lua/sc/network/handlers/unitnetworkhandler.lua
@@ -51,6 +51,41 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 		end
 	end
 
+	function UnitNetworkHandler:action_spooc_start(unit, target_u_pos, flying_strike, action_id)
+		if not self._verify_character(unit) or not self._verify_gamestate(self._gamestate_filter.any_ingame) then
+			return
+		end
+
+		local action_desc = {
+			block_type = "walk",
+			type = "spooc",
+			path_index = 1,
+			body_part = 1,
+			nav_path = {
+				unit:position()
+			},
+			target_u_pos = target_u_pos,
+			flying_strike = flying_strike,
+			action_id = action_id,
+			blocks = {
+				act = -1,
+				turn = -1,
+				walk = -1
+			}
+		}
+
+		if flying_strike then
+			action_desc.blocks.idle = -1
+			action_desc.blocks.light_hurt = -1
+			action_desc.blocks.heavy_hurt = -1
+			action_desc.blocks.fire_hurt = -1
+			action_desc.blocks.hurt = -1
+			action_desc.blocks.expl_hurt = -1
+			action_desc.blocks.taser_tased = -1
+		end
+
+		unit:movement():action_request(action_desc)
+	end
 
 	function UnitNetworkHandler:action_aim_state(unit, state)
 		if not self._verify_gamestate(self._gamestate_filter.any_ingame) or not self._verify_character(unit) then

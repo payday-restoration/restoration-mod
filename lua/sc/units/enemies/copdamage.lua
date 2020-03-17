860d970252d9ae9897a5a872f90add28cf87afaf
Ravicale <rjwiedenroth@crimson.ua.edu>
2020-03-16 19:50:54 -0500

    fixed a dumb

--- lua/sc/units/enemies/copdamage.lua
+++ lua/sc/units/enemies/copdamage.lua
@@ -2026,11 +2026,9 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 
 		local player_unit = managers.player:player_unit()
 		if player_unit and self._unit:movement() and self._unit:movement():m_rot() then
-			if player_unit:movement() and enemy_rotation then
-				local fwd_vec = mvector3.dot(self._unit:movement():m_rot():y(), player_unit:movement():m_head_rot():y())
-				if fwd_vec > 0.2 then
-					critical_value = critical_value + managers.player:upgrade_value("player", "backstab_crits", 0)
-				end
+			local fwd_vec = mvector3.dot(self._unit:movement():m_rot():y(), player_unit:movement():m_head_rot():y())
+			if fwd_vec > 0.2 then
+				critical_value = critical_value + managers.player:upgrade_value("player", "backstab_crits", 0)
 			end
 		end
 

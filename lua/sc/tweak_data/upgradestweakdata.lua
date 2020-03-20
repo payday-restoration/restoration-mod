9179965789d875070523224b078774fa69a2dbb8
Ravicale <rjwiedenroth@crimson.ua.edu>
2020-03-19 21:27:38 -0500

    Grinder stuff

--- lua/sc/tweak_data/upgradestweakdata.lua
+++ lua/sc/tweak_data/upgradestweakdata.lua
@@ -1374,8 +1374,8 @@ function UpgradesTweakData:_init_pd2_values()
 		works_with_armor_kit = true,
 		tick_time = 1,
 		total_ticks = 6,
-		max_stacks = 6,
-		stacking_cooldown = 0.4,
+		max_stacks = 5,
+		stacking_cooldown = 0.5,
 		add_stack_sources = {
 			bullet = true,
 			explosion = true,

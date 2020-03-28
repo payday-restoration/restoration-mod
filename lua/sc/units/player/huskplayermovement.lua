f56fd39722fa183a7013170b3c448f60b41dcbd8
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-03-28 06:54:48 -0300

    Few improvements + extra debug testing toggle

--- lua/sc/units/player/huskplayermovement.lua
+++ lua/sc/units/player/huskplayermovement.lua
@@ -7,6 +7,7 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 
 	local init_original = HuskPlayerMovement.init
 	function HuskPlayerMovement:init(unit)
+		self._draw_player_newest_pos = nil
 		self._draw_player_detect_pos = nil
 		self._stand_detection_offset_z = mvec3_z(tweak_data.player.stances.default.standard.head.translation)
 		init_original(self, unit)
@@ -27,13 +28,24 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 		sync_action_walk_nav_point_original(self, pos, speed, action, params)
 	end
 
-	function HuskPlayerMovement:_update_m_detect_pos(new_pos)
-		mvec3_set(self._m_detect_pos, new_pos)
+	function HuskPlayerMovement:_update_m_detect_pos(new_pos, new_pose_code)
 		mvec3_set(self._m_newest_pos, new_pos)
+		mvec3_set(self._m_detect_pos, new_pos)
 
-		local offset_z = self._pose_code == 2 and self._crouch_detection_offset_z or self._stand_detection_offset_z
+		local offset_z = nil
+
+		if new_pose_code then
+			offset_z = new_pose_code == 2 and self._crouch_detection_offset_z or self._stand_detection_offset_z
+		else
+			offset_z = self._pose_code == 2 and self._crouch_detection_offset_z or self._stand_detection_offset_z
+		end
 
 		mvec3_set_z(self._m_detect_pos, mvec3_z(self._m_detect_pos) + offset_z)
+
+		if self._draw_player_newest_pos then
+			local m_brush = Draw:brush(Color.blue:with_alpha(0.5), 0.1)
+			m_brush:sphere(self._m_newest_pos, 15)
+		end
 
 		if self._draw_player_detect_pos then
 			local head_brush = Draw:brush(Color.yellow:with_alpha(0.5), 0.1)
@@ -43,6 +55,6 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 
 	function HuskPlayerMovement:sync_action_change_pose(pose_code, pos)
 		self._desired_pose_code = pose_code
-		self:_update_m_detect_pos(pos)
+		self:_update_m_detect_pos(pos, pose_code)
 	end
 end

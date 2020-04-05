80eac85407529718ffec9a01f7709f8bb239c3ba
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-04-05 03:34:11 -0300

    Small HuskCopDamage change

--- lua/sc/units/enemies/huskcopdamage.lua
+++ lua/sc/units/enemies/huskcopdamage.lua
@@ -1,6 +1,5 @@
 if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue("SC/SC") then
 	local old_death = HuskCopDamage.die
-
 	function HuskCopDamage:die(variant)
 		old_death(self, variant)
 
@@ -11,11 +10,11 @@ if SC and SC._data.sc_ai_toggle or restoration and restoration.Options:GetValue(
 		if self._unit:contour() then
 			self._unit:contour():remove("omnia_heal", false)
 			self._unit:contour():remove("medic_show", false)
-		end 	
+		end
 
 		if self._unit:base():has_tag("tank_titan") or self._unit:base():has_tag("shield_titan") or self._unit:base():has_tag("captain") or self._unit:base():has_tag("lpf") then
-			self._unit:sound():play(self._unit:base():char_tweak().die_sound_event_2, nil, true)
-		end	
+			self._unit:sound():play(self._unit:base():char_tweak().die_sound_event_2, nil, nil)
+		end
 	end
 
 	function HuskCopDamage:heal_unit(...)

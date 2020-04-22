c86682ee381a34c9e4e7dbe77f33ccf09e51eabb
steam-test1 <steam-test1@users.noreply.github.com>
2020-04-22 16:43:26 +0100

    Added turning on/off overhaul dialog
    Included functions for the original idea

--- classes/callbacks.lua
+++ classes/callbacks.lua
@@ -3,10 +3,15 @@ RestorationCoreCallbacks._value_changed_funcs = {}
 
 --Save Force Restart
 function RestorationCoreCallbacks:Restart(key, value)
-	DelayedCalls:Add("SCRestartGame", 1, function()
-		log("Game closing in 1 second[s]!")
-		Application:close()
-	end)
+	if restoration.Options:GetValue("SC/SC") == false then
+		managers.menu:show_overhaul_dialog()
+	elseif restoration.Options:GetValue("SC/SC") == true then
+		managers.menu:enable_overhaul_dialog()
+	end
+	-- DelayedCalls:Add("SCRestartGame", 1, function()
+	-- 	log("Game closing in 1 second[s]!")
+	-- 	Application:close()
+	-- end)
 end
 
 --Any function inside the table will be called, this way we can keep things like hud updated.
--- lua/sc/managers/menumanagerpd2.lua
+++ lua/sc/managers/menumanagerpd2.lua
@@ -29,3 +29,102 @@ function MenuCallbackHandler:SCEnabled()
 		return false
 	end
 end
+
+function MenuManager:keep_overhaul_on()
+	local option = true
+	restoration.Options:SetValue("SC/SC", option)
+	managers.menu:post_event("menu_enter")
+end
+
+function MenuManager:keep_overhaul_off()
+	local option = false
+	restoration.Options:SetValue("SC/SC", option)
+end
+
+function MenuManager:close_game()
+	managers.menu:post_event("menu_enter")
+	Application:close()
+end
+
+function MenuManager:show_overhaul_dialog(func)
+	local count = 60
+	local dialog_data = {
+		title = managers.localization:text("Warning_overhaul_title"),
+		text = managers.localization:text("dialog_show_overhaul_dialog", {
+			TIME = count
+		}),
+		id = "accept_changes"
+	}
+	-- local cancel_button = {
+	-- 	text = managers.localization:text("dialog_cancel"),
+	-- 	callback_func = callback(self, self, "keep_overhaul_on"),
+	-- 	cancel_button = true
+	-- }
+	local ok_button = {
+		text = managers.localization:text("dialog_ok"),
+		callback_func = callback(self, self, "close_game")
+	}
+	dialog_data.button_list = {
+		ok_button
+	}
+	dialog_data.counter = {
+		1,
+		function ()
+			count = count - 1
+
+			if count < 0 then
+				self:close_game()
+				managers.system_menu:close(dialog_data.id)
+			else
+				local dlg = managers.system_menu:get_dialog(dialog_data.id)
+
+				if dlg then
+					dlg:set_text(managers.localization:text("dialog_show_overhaul_dialog", {
+						TIME = count
+					}), true)
+				end
+			end
+		end
+	}
+
+	managers.system_menu:show(dialog_data)
+end
+
+function MenuManager:enable_overhaul_dialog(func)
+	local count = 60
+	local dialog_data = {
+		title = managers.localization:text("Warning_overhaul_title"),
+		text = managers.localization:text("dialog_enable_overhaul_dialog", {
+			TIME = count
+		}),
+		id = "accept_changes"
+	}
+	local ok_button = {
+		text = managers.localization:text("dialog_ok"),
+		callback_func = callback(self, self, "close_game")
+	}
+	dialog_data.button_list = {
+		ok_button
+	}
+	dialog_data.counter = {
+		1,
+		function ()
+			count = count - 1
+
+			if count < 0 then
+				self:close_game()
+				managers.system_menu:close(dialog_data.id)
+			else
+				local dlg = managers.system_menu:get_dialog(dialog_data.id)
+
+				if dlg then
+					dlg:set_text(managers.localization:text("dialog_enable_overhaul_dialog", {
+						TIME = count
+					}), true)
+				end
+			end
+		end
+	}
+
+	managers.system_menu:show(dialog_data)
+end

c035a948fcf8243ef780b57765acc58a95296285
Ravicale <rjwiedenroth@crimson.ua.edu>
2020-05-06 15:25:56 -0400

    Fixed ammo custody stuff.

--- lua/sc/managers/playermanager.lua
+++ lua/sc/managers/playermanager.lua
@@ -819,7 +819,7 @@ function PlayerManager:_internal_load()
 	else
 		for id, weapon in pairs(player:inventory():available_selections()) do
 			if alive(weapon.unit) then
-				weapon.unit:base():remove_ammo(1 - tweak_data.player.damage.custody_ammo_kept)
+				weapon.unit:base():remove_ammo(tweak_data.player.damage.custody_ammo_kept)
 				managers.hud:set_ammo_amount(id, weapon.unit:base():ammo_info())
 			end
 		end

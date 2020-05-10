618d0ef87938c4468ee22a0322a69910a6b53f25
Neslon-Poggers <nanny01023@gmail.com>
2020-05-09 21:25:02 -0400

    ( thx offy ) unbuged

--- lua/sc/managers/menu/menunodegui.lua
+++ lua/sc/managers/menu/menunodegui.lua
@@ -10,24 +10,26 @@ local warned_mods = {
     ["deathvox"] = "Crackdown"
 }
 
-Hooks:PostHook(MenuNodeGui,"_setup_item_rows","res_mod_conflict_notif",function(self,node,...)
+Hooks:PostHook(MenuNodeGui,"_setup_item_rows","resmod_incompat_warn",function(self,node,...)
     local title = "RESTORATION MOD INCOMPATIBLIITY WARNING"
     local desc = "Caution! You have the following mods installed, which may conflict with Restoration Mod:\n"
-    
+    local has_any
     for key,mod_name in pairs(warned_mods) do 
         if rawget(_G,key) then 
+            has_any = true
             desc = desc .. "\n" .. mod_name
         end
     end
-    
-    QuickMenu:new(
-        title,
-        desc,
-        {
+    if has_any then 
+        QuickMenu:new(
+            title,
+            desc,
             {
-                text = "Okay",
-                is_cancel_button = true
+                {
+                    text = "OK",
+                    is_cancel_button = true
+                }
             }
-        }
-    ,true)
+        ,true)
+    end
 end)

ccdd3c32ce7920ddafcb6645eadf683786bf6c1d
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-05-22 22:58:18 -0300

    Client-sided stealth detection testing 3

--- lua/sc/units/player/playermovement.lua
+++ lua/sc/units/player/playermovement.lua
@@ -87,3 +87,86 @@ function PlayerMovement:_upd_underdog_skill(t)
 
 	data.chk_t = t + (activated and data.chk_interval_active or data.chk_interval_inactive)
 end
+
+function PlayerMovement:on_suspicion(observer_unit, status, local_client_detection)
+	if Network:is_server() or local_client_detection then
+		self._suspicion_debug = self._suspicion_debug or {}
+		self._suspicion_debug[observer_unit:key()] = {
+			unit = observer_unit,
+			name = observer_unit:name(),
+			status = status
+		}
+		local visible_status = nil
+
+		if managers.groupai:state():whisper_mode() and not managers.groupai:state():stealth_hud_disabled() then
+			visible_status = status
+		else
+			visible_status = false
+		end
+
+		self._suspicion = self._suspicion or {}
+
+		if visible_status == false or visible_status == true then
+			self._suspicion[observer_unit:key()] = nil
+
+			if not next(self._suspicion) then
+				self._suspicion = nil
+			end
+
+			if visible_status and observer_unit:movement() and not observer_unit:movement():cool() and TimerManager:game():time() - observer_unit:movement():not_cool_t() > 1 then
+				self._suspicion_ratio = false
+
+				self:_feed_suspicion_to_hud()
+
+				return
+			end
+		elseif type(visible_status) == "number" and (not observer_unit:movement() or observer_unit:movement():cool()) then
+			self._suspicion[observer_unit:key()] = visible_status
+		else
+			return
+		end
+
+		self:_calc_suspicion_ratio_and_sync(observer_unit, visible_status, local_client_detection)
+	else
+		self._suspicion_ratio = status
+	end
+
+	self:_feed_suspicion_to_hud()
+end
+
+function PlayerMovement:_calc_suspicion_ratio_and_sync(observer_unit, status, local_client_detection)
+	local suspicion_sync = nil
+
+	if self._suspicion and status ~= true then
+		local max_suspicion = nil
+
+		for u_key, val in pairs(self._suspicion) do
+			if not max_suspicion or max_suspicion < val then
+				max_suspicion = val
+			end
+		end
+
+		if max_suspicion then
+			self._suspicion_ratio = max_suspicion
+			suspicion_sync = math.ceil(self._suspicion_ratio * 254)
+		else
+			self._suspicion_ratio = false
+			suspicion_sync = false
+		end
+	elseif type(status) == "boolean" then
+		self._suspicion_ratio = status
+		suspicion_sync = status and 255 or 0
+	else
+		self._suspicion_ratio = false
+		suspicion_sync = 0
+	end
+
+	if not local_client_detection and suspicion_sync ~= self._synced_suspicion then
+		self._synced_suspicion = suspicion_sync
+		local peer = managers.network:session():peer_by_unit(self._unit)
+
+		if peer then
+			managers.network:session():send_to_peers_synched("suspicion", peer:id(), suspicion_sync)
+		end
+	end
+end

c13c55e5435d6cbb9ba3b101733044ea8740157f
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-05-31 19:16:32 -0300

    Ignore AI task limit during stealth
    For consistency and super smooth detection.

--- lua/sc/managers/enemymanager.lua
+++ lua/sc/managers/enemymanager.lua
@@ -5,6 +5,53 @@ local t_rem = table.remove
 local t_ins = table.insert
 local tmp_vec1 = Vector3()
 
+function EnemyManager:_update_queued_tasks(t, dt)
+	if managers.groupai:state():whisper_mode() then
+		for i_task, task_data in ipairs(self._queued_tasks) do
+			if not task_data.t or task_data.t < t then
+				self:_execute_queued_task(i_task)
+			end
+		end
+	else
+		local i_asap_task, asp_task_t = nil
+		self._queue_buffer = self._queue_buffer + dt
+		local tick_rate = tweak_data.group_ai.ai_tick_rate
+
+		if tick_rate <= self._queue_buffer then
+			for i_task, task_data in ipairs(self._queued_tasks) do
+				if not task_data.t or task_data.t < t then
+					self:_execute_queued_task(i_task)
+
+					self._queue_buffer = self._queue_buffer - tick_rate
+
+					if self._queue_buffer <= 0 then
+						break
+					end
+				elseif task_data.asap and (not asp_task_t or task_data.t < asp_task_t) then
+					i_asap_task = i_task
+					asp_task_t = task_data.t
+				end
+			end
+		end
+
+		if #self._queued_tasks == 0 then
+			self._queue_buffer = 0
+		end
+
+		if i_asap_task and not self._queued_task_executed then
+			self:_execute_queued_task(i_asap_task)
+		end
+	end
+
+	local all_clbks = self._delayed_clbks
+
+	if all_clbks[1] and all_clbks[1][2] < t then
+		local clbk = table.remove(all_clbks, 1)[3]
+
+		clbk()
+	end
+end
+
 function EnemyManager:get_nearby_medic(unit)
 	if self:is_civilian(unit) then
 		return nil

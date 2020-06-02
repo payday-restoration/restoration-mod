d672a4506d1720033c8b23c7275c6b25ef5dee38
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-06-02 00:30:41 -0300

    Added asap task executing for stealth
    Just to be consistent, as I removed that when I made the previous change. This means that if no tasks were executed during a frame, one asap task will be executed regardless of its execution time (this is how it normally works without the changes I made).
    
    I also got a report regarding framerate issues during stealth, possibly due to these changes. I personally have no issues but that doesn't mean other people don't, but two tests are not enough. And while maps that are badly optimized don't count (so don't bother reporting those), I'm sure that smooth and 100% consistent detection while in stealth is preferable (well, as long as you don't go below 10-20 fps or something like that, please report it if you do).

--- lua/sc/managers/enemymanager.lua
+++ lua/sc/managers/enemymanager.lua
@@ -6,14 +6,20 @@ local t_ins = table.insert
 local tmp_vec1 = Vector3()
 
 function EnemyManager:_update_queued_tasks(t, dt)
+	local i_asap_task, asap_task_t = nil
+
 	if managers.groupai:state():whisper_mode() then
 		for i_task, task_data in ipairs(self._queued_tasks) do
 			if not task_data.t or task_data.t < t then
 				self:_execute_queued_task(i_task)
+			elseif task_data.asap then
+				if not asap_task_t or task_data.t < asap_task_t then
+					i_asap_task = i_task
+					asap_task_t = task_data.t
+				end
 			end
 		end
 	else
-		local i_asap_task, asp_task_t = nil
 		self._queue_buffer = self._queue_buffer + dt
 		local tick_rate = tweak_data.group_ai.ai_tick_rate
 
@@ -27,9 +33,11 @@ function EnemyManager:_update_queued_tasks(t, dt)
 					if self._queue_buffer <= 0 then
 						break
 					end
-				elseif task_data.asap and (not asp_task_t or task_data.t < asp_task_t) then
-					i_asap_task = i_task
-					asp_task_t = task_data.t
+				elseif task_data.asap then
+					if not asap_task_t or task_data.t < asap_task_t then
+						i_asap_task = i_task
+						asap_task_t = task_data.t
+					end
 				end
 			end
 		end
@@ -37,16 +45,16 @@ function EnemyManager:_update_queued_tasks(t, dt)
 		if #self._queued_tasks == 0 then
 			self._queue_buffer = 0
 		end
+	end
 
-		if i_asap_task and not self._queued_task_executed then
-			self:_execute_queued_task(i_asap_task)
-		end
+	if i_asap_task and not self._queued_task_executed then
+		self:_execute_queued_task(i_asap_task)
 	end
 
 	local all_clbks = self._delayed_clbks
 
 	if all_clbks[1] and all_clbks[1][2] < t then
-		local clbk = table.remove(all_clbks, 1)[3]
+		local clbk = t_rem(all_clbks, 1)[3]
 
 		clbk()
 	end

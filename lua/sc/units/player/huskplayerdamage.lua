4fe6aa5d9a647facf2e9bb8a69754cc7b6331a8d
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-06-02 20:21:48 -0300

    Enable player FF on CS and pro jobs
    Pro jobs get friendly fire with 25% of the damage on OVK or below, else 50%.

--- lua/sc/units/player/huskplayerdamage.lua
+++ lua/sc/units/player/huskplayerdamage.lua
@@ -1,13 +1,67 @@
+function HuskPlayerDamage:damage_bullet(attack_data)
+	if Global.one_down or Global.inc_mission or managers.mutators:is_mutator_active(MutatorFriendlyFire) then
+		self:_send_damage_to_owner(attack_data)
+	end
+end
+
+function HuskPlayerDamage:damage_melee(attack_data)
+	if Global.one_down or Global.inc_mission or managers.mutators:is_mutator_active(MutatorFriendlyFire) then
+		self:_send_damage_to_owner(attack_data)
+	end
+end
+
+function HuskPlayerDamage:damage_fire(attack_data)
+	if Global.one_down or Global.inc_mission or managers.mutators:is_mutator_active(MutatorFriendlyFire) then
+		local apply_damage_reduction = true
+		local attacker_unit = attack_data.attacker_unit
+
+		if attacker_unit and alive(attacker_unit) then
+			if attacker_unit:base() and attacker_unit:base().thrower_unit then
+				attacker_unit = attacker_unit:base():thrower_unit()
+				attack_data.attacker_unit = attacker_unit
+			end
+
+			if attacker_unit:base().is_husk_player then
+				apply_damage_reduction = false
+			end
+		end
+
+		--if somehow something that deals fire damage that isn't a player hits a player husk, apply the reduction as usual
+		if apply_damage_reduction then
+			attack_data.damage = attack_data.damage * 0.2
+		end
+
+		self:_send_damage_to_owner(attack_data)
+	end
+end
+
 function HuskPlayerDamage:_send_damage_to_owner(attack_data)
 	local peer_id = managers.criminals:character_peer_id_by_unit(self._unit)
-    local damage = managers.mutators:modify_value("HuskPlayerDamage:FriendlyFireDamage", attack_data.damage)
-    damage = managers.modifiers:modify_value("HuskPlayerDamage:FriendlyFireDamageCS", attack_data.damage)
+	local damage = attack_data.damage
+
+	if managers.mutators:is_mutator_active(MutatorFriendlyFire) then
+		damage = managers.mutators:modify_value("HuskPlayerDamage:FriendlyFireDamage", damage)
+	elseif Global.one_down then
+		local difficulty = Global.game_settings and Global.game_settings.difficulty or "normal"
+		local difficulty_index = tweak_data:difficulty_to_index(difficulty)
+
+		if difficulty_index <= 5 then
+			damage = damage * 0.25
+		else
+			damage = damage * 0.5
+		end
+	elseif Global.inc_mission then
+		damage = managers.modifiers:modify_value("HuskPlayerDamage:FriendlyFireDamageCS", damage)
+	end
 
 	managers.network:session():send_to_peers("sync_friendly_fire_damage", peer_id, attack_data.attacker_unit, damage, attack_data.variant)
 
-	if attack_data.attacker_unit == managers.player:player_unit() then
+	--disabling since it's not accurate, as the player can dodge/block it, maybe make a custom friendly fire indicator to use instead?
+	--[[if attack_data.attacker_unit == managers.player:player_unit() then
 		managers.hud:on_hit_confirmed()
-	end
+	end]]
 
-	managers.job:set_memory("trophy_flawless", true, false)
+	if managers.mutators:is_mutator_active(MutatorFriendlyFire) then
+		managers.job:set_memory("trophy_flawless", true, false)
+	end
 end

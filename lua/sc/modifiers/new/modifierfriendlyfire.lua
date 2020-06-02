6fd009f81428538dd43bffc4dcf899b2107c4701
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-06-02 20:33:33 -0300

    Update modifierfriendlyfire.lua

--- lua/sc/modifiers/new/modifierfriendlyfire.lua
+++ lua/sc/modifiers/new/modifierfriendlyfire.lua
@@ -3,13 +3,13 @@ ModifierFriendlyFire._type = "ModifierFriendlyFire"
 ModifierFriendlyFire.name_id = "none"
 ModifierFriendlyFire.desc_id = "menu_cs_modifier_friendlyfire"
 
---pretty much a copy paste from mutator tbh
 function ModifierFriendlyFire:modify_value(id, value)
-	local dmg_mult = 0.5
 	if id == "PlayerDamage:FriendlyFire" then
 		return false
 	elseif id == "HuskPlayerDamage:FriendlyFireDamageCS" then
-		return value * dmg_mult * 0.25
+		local dmg_mult = 1 --replace with scaling multiplier or whatever
+
+		return value * dmg_mult
 	elseif id == "ProjectileBase:create_sweep_data:slot_mask" then
 		return value + 3
 	end

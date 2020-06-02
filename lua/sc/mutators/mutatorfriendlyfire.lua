4ed93c9e73289394d3ffa27ec7f32055d1cf8e94
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-06-02 20:29:06 -0300

    Prevent FF from allowing anything to hit husks
    Player husks that is.

--- lua/sc/mutators/mutatorfriendlyfire.lua
+++ lua/sc/mutators/mutatorfriendlyfire.lua
@@ -1,6 +1,23 @@
 MutatorFriendlyFire.reductions = {money = 0, exp = 0}
 MutatorFriendlyFire.disables_achievements = false
 
+function MutatorFriendlyFire:setup(mutator_manager)
+	MutatorFriendlyFire.super.setup(mutator_manager)
+
+	--this results in anything that uses this slotmask to hit player husks, which is NOT good
+	--managers.slot._masks.bullet_impact_targets = managers.slot._masks.bullet_impact_targets_ff
+end
+
+function MutatorFriendlyFire:modify_value(id, value)
+	if id == "PlayerDamage:FriendlyFire" then
+		return false
+	elseif id == "HuskPlayerDamage:FriendlyFireDamage" then
+		return value * self:get_friendly_fire_damage_multiplier()
+	elseif id == "ProjectileBase:create_sweep_data:slot_mask" then
+		return value + 3
+	end
+end
+
 --No Damage grace--
 
 --Why--
@@ -22,4 +39,4 @@ MutatorFatRoll.icon_coords = {
 function MutatorFatRoll:setup(data)
 	tweak_data.player.damage.MIN_DAMAGE_INTERVAL = 0
 	tweak_data.character.presets.gang_member_damage.MIN_DAMAGE_INTERVAL = 0
-end		
+end

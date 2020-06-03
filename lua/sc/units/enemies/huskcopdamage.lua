3130e69a62d0c90bb7aa35bad6088c10d8870416
Ellieeeee <32231454+Ellieeeee@users.noreply.github.com>
2020-06-02 23:55:25 -0500

    Update huskcopdamage.lua

--- lua/sc/units/enemies/huskcopdamage.lua
+++ lua/sc/units/enemies/huskcopdamage.lua
@@ -26,6 +26,15 @@ function HuskCopDamage:die(variant)
 			end
 		end
 	end
+
+	--client sided boomboom
+	if self._unit:base()._tweak_table == "boom" then
+		local boom_boom = false
+		boom_boom = managers.modifiers:modify_value("CopDamage:CanBoomBoom", boom_boom)
+		if boom_boom then
+			MutatorExplodingEnemies._detonate(MutatorExplodingEnemies, self, attack_data, true, 20, 500)
+		end
+	end
 end
 
 function HuskCopDamage:heal_unit(...)

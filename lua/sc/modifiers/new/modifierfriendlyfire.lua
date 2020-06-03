8e89fb4c31292e9fb535005827fbe1b03daa8dc2
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-06-02 21:22:51 -0300

    Updated modifierfriendlyfire again

--- lua/sc/modifiers/new/modifierfriendlyfire.lua
+++ lua/sc/modifiers/new/modifierfriendlyfire.lua
@@ -4,13 +4,17 @@ ModifierFriendlyFire.name_id = "none"
 ModifierFriendlyFire.desc_id = "menu_cs_modifier_friendlyfire"
 
 function ModifierFriendlyFire:modify_value(id, value)
-	if id == "PlayerDamage:FriendlyFire" then
+	if id == "HuskPlayerDamage:FriendlyFireDamageCSEnabled" then
+		return true
+	elseif id == "PlayerDamage:FriendlyFire" then
 		return false
 	elseif id == "HuskPlayerDamage:FriendlyFireDamageCS" then
 		local dmg_mult = 1 --replace with scaling multiplier or whatever
 
 		return value * dmg_mult
-	elseif id == "ProjectileBase:create_sweep_data:slot_mask" then
+	elseif id == "ProjectileBase:create_sweep_data:slot_mask" or id == "PlayerStandard:init:melee_slot_mask" or id == "RaycastWeaponBase:setup:weapon_slot_mask" then
 		return value + 3
 	end
+
+	return value
 end

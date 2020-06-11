dee97e0a2052fcbf35314dabdffd36beb2dad05f
Ravicale <epicnetmail@gmail.com>
2020-06-10 21:07:08 -0400

    Removed excess debug stuff.

--- lua/sc/units/weapons/frag.lua
+++ lua/sc/units/weapons/frag.lua
@@ -1,28 +1,3 @@
-function FragGrenade:_setup_from_tweak_data()
-	local grenade_entry = self._tweak_projectile_entry or "frag"
-	log(grenade_entry)
-	local tweak_entry = tweak_data.projectiles[grenade_entry]
-	self._init_timer = tweak_entry.init_timer or 2.5
-	self._mass_look_up_modifier = tweak_entry.mass_look_up_modifier
-	self._range = tweak_entry.range
-	self._effect_name = tweak_entry.effect_name or "effects/payday2/particles/explosions/grenade_explosion"
-	self._curve_pow = tweak_entry.curve_pow or 3
-	self._damage = tweak_entry.damage
-	self._player_damage = tweak_entry.player_damage
-	self._alert_radius = tweak_entry.alert_radius
-	local sound_event = tweak_entry.sound_event or "grenade_explode"
-	self._custom_params = {
-		camera_shake_max_mul = 4,
-		sound_muffle_effect = true,
-		effect = self._effect_name,
-		sound_event = sound_event,
-		feedback_range = self._range * 2
-	}
-
-	return tweak_entry
-end
-
-
 function FragGrenade:bullet_hit()
 --yay
 end

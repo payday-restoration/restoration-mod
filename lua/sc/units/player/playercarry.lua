d75e7020653f7ae0f96a1a9d9e8b39f97898b65b
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-06-17 23:34:00 -0300

    Carrying a bag now modifies detection as intended
    Only applies to stealth.
    
    Super old issue. Initially this only applied when entering the carry state. Already crouching when picking up a bag, or crouching after picking up one, would change your attention presets back to normal, making you get detected as if you didn't have the bag.
    
    The issue kept being reported and was never fixed, though instead, at one point, the relevant code was removed as it caused consistency issues with detection, leading to frustration for unaware players.
    
    So yeah, this is how it's intended to work. You'll be fine as long as you stay down when moving bags.

--- lua/sc/units/player/playercarry.lua
+++ lua/sc/units/player/playercarry.lua
@@ -25,3 +25,18 @@ function PlayerCarry:_get_max_walk_speed(...)
 		
 	return PlayerCarry.super._get_max_walk_speed(self, ...) * multiplier
 end
+
+function PlayerCarry:_upd_attention()
+	if managers.groupai:state():whisper_mode() and not self._state_data.ducking then
+		local preset = {
+			"pl_friend_combatant_cbt",
+			"pl_friend_non_combatant_cbt",
+			"pl_foe_combatant_cbt_stand",
+			"pl_foe_non_combatant_cbt_stand"
+		}
+
+		self._ext_movement:set_attention_settings(preset)
+	else
+		PlayerCarry.super._upd_attention(self)
+	end
+end

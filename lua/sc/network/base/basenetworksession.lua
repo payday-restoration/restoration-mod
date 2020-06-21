3e6ab4f8845e5ae03972b0d22a095bd528168cc0
Rhynne <douglaskmarx@gmail.com>
2020-06-21 15:35:16 -0500

    Update basenetworksession.lua

--- lua/sc/network/base/basenetworksession.lua
+++ lua/sc/network/base/basenetworksession.lua
@@ -1,6 +1,7 @@
-BaseNetworkSession.CONNECTION_TIMEOUT = 20
-BaseNetworkSession.LOADING_CONNECTION_TIMEOUT = SystemInfo:platform() == Idstring("WIN32") and 40
-BaseNetworkSession._LOAD_WAIT_TIME = 10
+--why is there more than fucking one of these timeouts?  fuck off
+BaseNetworkSession.CONNECTION_TIMEOUT = 9001
+BaseNetworkSession.LOADING_CONNECTION_TIMEOUT = SystemInfo:platform() == Idstring("WIN32") and 9001
+BaseNetworkSession._LOAD_WAIT_TIME = 9001
 
 Hooks:PostHook(BaseNetworkSession,"on_peer_sync_complete","resmod_send_sync_env_data",function(self,peer,peer_id)
 	if not self._local_peer then

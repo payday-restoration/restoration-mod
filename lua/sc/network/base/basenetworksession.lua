589b2b31bd5e53b6a050ad8c7f00b804ee473a79
Ravicale <epicnetmail@gmail.com>
2020-06-22 02:33:42 -0400

    Revert basenetworksession changes.
    Mega buge.

--- lua/sc/network/base/basenetworksession.lua
+++ lua/sc/network/base/basenetworksession.lua
@@ -1,7 +1,6 @@
---why is there more than fucking one of these timeouts?  fuck off
-BaseNetworkSession.CONNECTION_TIMEOUT = 9001
-BaseNetworkSession.LOADING_CONNECTION_TIMEOUT = SystemInfo:platform() == Idstring("WIN32") and 9001
-BaseNetworkSession._LOAD_WAIT_TIME = 9001
+BaseNetworkSession.CONNECTION_TIMEOUT = 20
+BaseNetworkSession.LOADING_CONNECTION_TIMEOUT = SystemInfo:platform() == Idstring("WIN32") and 40
+BaseNetworkSession._LOAD_WAIT_TIME = 10
 
 Hooks:PostHook(BaseNetworkSession,"on_peer_sync_complete","resmod_send_sync_env_data",function(self,peer,peer_id)
 	if not self._local_peer then

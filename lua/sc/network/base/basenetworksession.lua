7aea7e7765c5e3c1eaa0b5f6db3643d24bae6c43
Rhynne <douglaskmarx@gmail.com>
2020-06-22 01:47:43 -0500

    Gun Metal Grey (Poopy Version)

--- lua/sc/network/base/basenetworksession.lua
+++ lua/sc/network/base/basenetworksession.lua
@@ -1,7 +1,7 @@
-BaseNetworkSession.CONNECTION_TIMEOUT = 20
-BaseNetworkSession.LOADING_CONNECTION_TIMEOUT = SystemInfo:platform() == Idstring("WIN32") and 40
-BaseNetworkSession._LOAD_WAIT_TIME = 10
-
+BaseNetworkSession.CONNECTION_TIMEOUT = 60
+BaseNetworkSession.LOADING_CONNECTION_TIMEOUT = SystemInfo:platform() == Idstring("WIN32") and 60
+BaseNetworkSession._LOAD_WAIT_TIME = 60
+--above, keep the values at 60 or around 60.  that's one minute... if for some reason this causes issues, drop it to ~45s
 Hooks:PostHook(BaseNetworkSession,"on_peer_sync_complete","resmod_send_sync_env_data",function(self,peer,peer_id)
 	if not self._local_peer then
 		return

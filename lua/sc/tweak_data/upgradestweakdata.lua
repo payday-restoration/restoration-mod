23b051d4cc4b1b87d52623d457a4d25482462ab6
Ravicale <epicnetmail@gmail.com>
2020-06-24 11:20:10 -0400

    Minor localization + balance.
    Grinder stack duration reduced.
    	It was really trivial to stay at max stacks.
    Quick Fix buff reverted (back to 50% DR).
    Haunt per down panic chance reduced to 7% from 8%.
    	It was still a bit much with the new down system.
    Damage Absorption is no longer used as a keyword.
    	Even though it is a *vanilla mechanic*, it's poorly explained enough that I keep seeing questions about it.
    Removed ambiguity from biker description.

--- lua/sc/tweak_data/upgradestweakdata.lua
+++ lua/sc/tweak_data/upgradestweakdata.lua
@@ -476,7 +476,7 @@ Hooks:PostHook(UpgradesTweakData, "_init_pd2_values", "ResSkillsInit", function(
 				--Basic
 					self.values.first_aid_kit.deploy_time_multiplier = {0.5} --Also applies to DBs.
 				--Ace
-					self.values.temporary.first_aid_damage_reduction = {{0.4, 5}}
+					self.values.temporary.first_aid_damage_reduction = {{0.5, 5}}
 
 			--Painkillers--
 				self.values.temporary.passive_revive_damage_reduction = {
@@ -1171,7 +1171,7 @@ Hooks:PostHook(UpgradesTweakData, "_init_pd2_values", "ResSkillsInit", function(
 
 			--Haunt
 				--Basic
-					self.values.player.killshot_spooky_panic_chance = {0.08}
+					self.values.player.killshot_spooky_panic_chance = {0.07}
 				--Ace
 					self.values.player.killshot_extra_spooky_panic_chance = {0.2}
 				
@@ -1371,7 +1371,7 @@ Hooks:PostHook(UpgradesTweakData, "_init_pd2_values", "ResSkillsInit", function(
 		armors_allowed = {"level_5"},
 		works_with_armor_kit = true,
 		tick_time = 1,
-		total_ticks = 6,
+		total_ticks = 4,
 		max_stacks = 5,
 		stacking_cooldown = 0.5,
 		add_stack_sources = {
@@ -1393,7 +1393,7 @@ Hooks:PostHook(UpgradesTweakData, "_init_pd2_values", "ResSkillsInit", function(
 		0.3,
 		0.4
 	}	
-	self.values.player.damage_to_hot_extra_ticks = {3}
+	self.values.player.damage_to_hot_extra_ticks = {2}
 	self.values.player.hot_speed_bonus = {0.04}
 	
 	self.values.player.perk_armor_loss_multiplier = {

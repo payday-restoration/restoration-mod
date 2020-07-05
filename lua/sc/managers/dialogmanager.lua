2084cdcf01f6ee9192cd9f9a66f2b9dc3c70bc46
EvilBobarino <40873080+EvilBobarino@users.noreply.github.com>
2020-07-05 15:11:10 +0100

    a bunch of ponrs
    alaskan deal
    hotline miami
    four stores
    jewelery store
    diamond store
    ukranian job
    mallcrasher

--- lua/sc/managers/dialogmanager.lua
+++ lua/sc/managers/dialogmanager.lua
@@ -76,6 +76,125 @@ function DialogManager:queue_dialog(id, ...)
 			end		
 		end	
 	
+		--Alaskan Deal--				
+		if id == "Play_cpt_wwh_16" then
+			if difficulty_index <= 2 then
+				escape_time = 1050
+			elseif difficulty_index == 3 then
+				escape_time = 1020
+			elseif difficulty_index == 4 then
+				escape_time = 990
+			elseif difficulty_index == 5 then
+				escape_time = 960	
+			elseif difficulty_index == 6 or difficulty_index == 7 then
+				escape_time = 930					
+			else
+				escape_time = 900
+			end		
+		end	
+	
+		--Hotline Miami Day 1--				
+		if id == "Play_pln_hm1_52" then
+			if difficulty_index <= 2 then
+				escape_time = 1050
+			elseif difficulty_index == 3 then
+				escape_time = 1020
+			elseif difficulty_index == 4 then
+				escape_time = 990
+			elseif difficulty_index == 5 then
+				escape_time = 960	
+			elseif difficulty_index == 6 or difficulty_index == 7 then
+				escape_time = 930					
+			else
+				escape_time = 900
+			end		
+		end	
+	
+		--Hotline Miami Day 2--				
+		if id == "Play_pln_hm2_15" then
+			if difficulty_index <= 2 then
+				escape_time = 450 
+			elseif difficulty_index == 3 then
+				escape_time = 420
+			elseif difficulty_index == 4 then
+				escape_time = 390
+			elseif difficulty_index == 5 then
+				escape_time = 360	
+			elseif difficulty_index == 6 or difficulty_index == 7 then
+				escape_time = 330						
+			else
+				escape_time = 300
+			end
+		end	
+	
+		--Four Stores--				
+		if id == "pln_fost_vh_01" then
+			if difficulty_index <= 2 then
+				escape_time = 450 
+			elseif difficulty_index == 3 then
+				escape_time = 420
+			elseif difficulty_index == 4 then
+				escape_time = 390
+			elseif difficulty_index == 5 then
+				escape_time = 360	
+			elseif difficulty_index == 6 or difficulty_index == 7 then
+				escape_time = 330						
+			else
+				escape_time = 300
+			end
+		end	
+	
+		--Jewelery Store & Ukrainian Job--				
+		if id == "Play_pln_ukranian_stage1_28" then
+			if difficulty_index <= 2 then
+				escape_time = 690
+			elseif difficulty_index == 3 then
+				escape_time = 660
+			elseif difficulty_index == 4 then
+				escape_time = 630
+			elseif difficulty_index == 5 then
+				escape_time = 600	
+			elseif difficulty_index == 6 or difficulty_index == 7 then
+				escape_time = 570						
+			else
+				escape_time = 540
+			end	
+		end
+	
+		--Diamond Store--				
+		if id == "Play_pln_fj1_07" then
+			if difficulty_index <= 2 then
+				escape_time = 690
+			elseif difficulty_index == 3 then
+				escape_time = 660
+			elseif difficulty_index == 4 then
+				escape_time = 630
+			elseif difficulty_index == 5 then
+				escape_time = 600	
+			elseif difficulty_index == 6 or difficulty_index == 7 then
+				escape_time = 570						
+			else
+				escape_time = 540
+			end	
+		end	
+	
+		--Mallcrasher--				
+		if id == "pln_mallcrash_stage1_14" then
+			if difficulty_index <= 2 then
+				escape_time = 690
+			elseif difficulty_index == 3 then
+				escape_time = 660
+			elseif difficulty_index == 4 then
+				escape_time = 630
+			elseif difficulty_index == 5 then
+				escape_time = 600	
+			elseif difficulty_index == 6 or difficulty_index == 7 then
+				escape_time = 570						
+			else
+				escape_time = 540
+			end	
+		end	
+	
 		--Train Heist--				
 		if id == "Play_pln_tr1b_02" then
 			if difficulty_index <= 2 then

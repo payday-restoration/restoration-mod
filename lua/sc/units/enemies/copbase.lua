7682745a2af0273bb63bb74e82568c65ab5f10e5
Neslon-Poggers <nanny01023@gmail.com>
2020-07-14 13:44:47 -0400

    slight murkywater asset updates
    - fixed smoothing on murky fbi swat 1's head, redid his lod models completely so that it looks mostly okay. fixed bad textures on his lod head. redid weighting on pouches.
    
    - fixed smoothing on murky fbi swat 2's head and fixed his lod body model. slightly moved his chest shotgunner pouch up

--- lua/sc/units/enemies/copbase.lua
+++ lua/sc/units/enemies/copbase.lua
@@ -14,12 +14,29 @@ function CopBase:random_mat_seq_initialization()
     or unit_name == Idstring("units/payday2/characters/ene_cop_2/ene_cop_2")
     or unit_name == Idstring("units/payday2/characters/ene_cop_3/ene_cop_3")
     or unit_name == Idstring("units/payday2/characters/ene_cop_4/ene_cop_4")
+	
+    local murkies = unit_name == Idstring("units/pd2_mod_sharks/characters/ene_fbi_swat_1/ene_fbi_swat_1") 
     
     if self._unit:damage() and self._unit:damage():has_sequence("pick_mats_for_cops") and cops then
           -- log("doin youre mom!")    
         self._unit:damage():run_sequence_simple("pick_mats_for_cops")
+    elseif self._unit:damage() and self._unit:damage():has_sequence("set_style_murky") and murkies then
+		  -- log("t")
+        self._unit:damage():run_sequence_simple("set_style_murky")
     end
 end	
+
+local material_config_paths = {
+  "units/pd2_mod_sharks/characters/ene_fbi_swat_1/ene_fbi_swat_1_disktrasa"
+}
+
+for i, material_config_path in pairs(material_config_paths) do
+  local normal_ids = Idstring(material_config_path)
+  local contour_ids = Idstring(material_config_path .. "_contour")
+
+  CopBase._material_translation_map[tostring(normal_ids:key())] = contour_ids
+  CopBase._material_translation_map[tostring(contour_ids:key())] = normal_ids 
+end
 
 function CopBase:_chk_spawn_gear()
 	if self._tweak_table == "spooc" then

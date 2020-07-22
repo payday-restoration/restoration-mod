95031eabf6f3e1e9d57458e198995bf7180a5991
Neslon-Poggers <nanny01023@gmail.com>
2020-07-21 22:29:33 -0400

    Ronald T Bugerson
    i fuck ur mother bady
    .
    .
    .
    .
    .
    .
    .
    .
    .
    .
    
    implemented jarey's zeal medic

--- lua/sc/tweak_data/charactertweakdata.lua
+++ lua/sc/tweak_data/charactertweakdata.lua
@@ -14827,6 +14827,7 @@ function CharacterTweakData:character_map()
 				"ene_zeal_city_1",
 				"ene_zeal_city_2",
 				"ene_zeal_city_3",
+				"ene_zeal_medic",				
 				"ene_zeal_sniper",
 				"ene_zeal_swat_heavy",
 				"ene_zeal_heavy_shield",

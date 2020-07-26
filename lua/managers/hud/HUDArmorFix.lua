2e1f34b14a2e9c1c16f89582e3b4b92e33d96105
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2020-07-26 18:15:14 +0100

    Attempt at fixing armor issues

--- lua/managers/hud/HUDArmorFix.lua
+++ lua/managers/hud/HUDArmorFix.lua
@@ -1,9 +1,19 @@
 if not restoration.Options:GetValue("HUD/Extra/ArmorFix") then return end
 
 RestorationCoreHooks:Post(HUDTeammate, "init", function(self)
-    local radial_health_panel = self._player_panel:child("radial_health_panel")
-    local damage_indicator = radial_health_panel:child("damage_indicator")
-    damage_indicator:configure({
-		texture = "guis/textures/restoration/hud_radial_rim"
-    })
+  local radial_health_panel = self._player_panel:child("radial_health_panel")
+  local radial_health = radial_health_panel:child("damage_indicator")
+  local damage_indicator = radial_health_panel:child("damage_indicator")
+  radial_health:configure({
+    texture = "guis/textures/restoration/hud_radialbg"
+  })
+  damage_indicator:configure({
+    texture = "guis/textures/restoration/hud_radial_rim",
+    texture_rect = {
+    128,
+    0,
+    -128,
+    128
+    }
+  })
 end)

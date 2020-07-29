431334c2e3a628e14af1fc2221974e09c724ed34
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2020-07-29 20:37:41 +0100

    Attempt at fixing crash

--- lua/sc/setups/Setup.lua
+++ lua/sc/setups/Setup.lua
@@ -1,4 +1,4 @@
 Hooks:PostHook(Setup, "init_managers", "pdth_hud_define_challenges", function(ply)
-    tweak_data.challenges_res = ChallengesTweakDataRes:new()
+    tweak_data.challenges_res = ChallengesTweakData:new()
     managers.challenges_res = ChallengesManagerRes:new()
 end)

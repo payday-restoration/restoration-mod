10adcebb89ebaa6204abc0993a9b39f71eefb35b
Ravicale <epicnetmail@gmail.com>
2020-08-16 15:24:51 -0500

    Cluster grenade children hop up higher.

--- lua/sc/units/weapons/frag.lua
+++ lua/sc/units/weapons/frag.lua
@@ -6,7 +6,7 @@ Hooks:PostHook(FragGrenade, "_detonate" , "ResClusterGrenade" , function(self, t
 	local grenade_tweak = tweak_data.projectiles[self._tweak_projectile_entry]
 	if grenade_tweak and grenade_tweak.cluster then
 		for i=1, grenade_tweak.cluster_count or 1 do
-			ProjectileBase.throw_projectile(grenade_tweak.cluster, self._unit:position(), Vector3(math.random(-1, 1), math.random(-1, 1), 0.3), nil, self:thrower_unit() or self._unit, true)
+			ProjectileBase.throw_projectile(grenade_tweak.cluster, self._unit:position(), Vector3(math.random(-1, 1), math.random(-1, 1), 0.5), nil, self:thrower_unit() or self._unit, true)
 		end
 	end
 end)

7134db300104abbae4c25fc8e87986de256fcdac
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2020-09-23 21:53:15 +0100

    Fixes
    Disabled armor flash fix
    Disabled coins reward for challenges

--- classes/ChallengesManager.lua
+++ classes/ChallengesManager.lua
@@ -281,12 +281,12 @@ function ChallengesManagerRes:_completed_challenge( name )
 			managers.hud:present_mid_text( { title = title, text = text, time = 4, icon = nil, event = "stinger_objectivecomplete", type = "challenge" } )
 		end
 
-		local cc_reward = self._challenges_map_res[ name ].cc
-		if cc_reward then
-			managers.custom_safehouse:add_coins( cc_reward, true )
-		else
-			return
-		end
+		-- local cc_reward = self._challenges_map_res[ name ].cc
+		-- if cc_reward then
+		-- 	managers.custom_safehouse:add_coins( cc_reward, true )
+		-- else
+		-- 	return
+		-- end
 		-- managers.experience:add_points( self._challenges_map_res[ name ].xp, true )
 	end
 
--- lua/managers/hud/HUDArmorFix.lua
+++ lua/managers/hud/HUDArmorFix.lua
@@ -1,9 +1,17 @@
-if not restoration.Options:GetValue("HUD/Extra/ArmorFix") then return end
+--[[
+
+if not restoration.Options:GetValue("HUD/Extra/ArmorFix") and not restoration:all_enabled("HUD/MainHUD", "HUD/Teammate") then return end
 
 RestorationCoreHooks:Post(HUDTeammate, "init", function(self)
   local radial_health_panel = self._player_panel:child("radial_health_panel")
+  local radial_health = radial_health_panel:child("damage_indicator")
   local damage_indicator = radial_health_panel:child("damage_indicator")
+  radial_health:configure({
+    texture = "guis/textures/restoration/hud_radialbg_base_fix"
+  })
   damage_indicator:configure({
-    texture = "guis/textures/restoration/hud_radial_rim"
+    texture = "guis/textures/restoration/hud_radial_rim_base_fix"
   })
 end)
+
+]]

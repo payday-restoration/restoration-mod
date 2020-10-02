a452e95fa68e98fb22bf193a9c5d324405c33c01
SC <gametwisterdotnet@yahoo.com>
2020-10-01 20:02:15 -0700

    Even more tips
    [*]Adding even more tips. Almost there!

--- lua/sc/tweak_data/tipstweakdata.lua
+++ lua/sc/tweak_data/tipstweakdata.lua
@@ -282,7 +282,397 @@ function TipsTweakData:init()
 			image = "grenadier_load",
 			consoles = true,
 			category = "new_units_res"
-		},			
+		},	
+		{
+			cat_index = 1,
+			image = "general_difficulty",
+			consoles = true,
+			category = "captains_res"
+		},	
+		{
+			cat_index = 2,
+			image = "general_difficulty",
+			consoles = true,
+			category = "captains_res"
+		},	
+		{
+			cat_index = 3,
+			image = "weapon_explosives",
+			consoles = true,
+			category = "captains_res"
+		},	
+		{
+			cat_index = 4,
+			image = "weapon_explosives",
+			consoles = true,
+			category = "captains_res"
+		},	
+		{
+			cat_index = 5,
+			image = "tactics_cover",
+			consoles = true,
+			category = "captains_res"
+		},	
+		{
+			cat_index = 6,
+			image = "general_difficulty",
+			consoles = true,
+			category = "captains_res"
+		},	
+		{
+			cat_index = 7,
+			image = "general_difficulty",
+			consoles = true,
+			category = "captains_res"
+		},	
+		{
+			cat_index = 8,
+			image = "general_equipment",
+			consoles = true,
+			category = "captains_res"
+		},	
+		{
+			cat_index = 9,
+			image = "weapon_melee",
+			consoles = true,
+			category = "captains_res"
+		},
+		{
+			cat_index = 10,
+			image = "enemy_winters",
+			consoles = true,
+			category = "captains_res"
+		},	
+		{
+			cat_index = 11,
+			image = "enemy_winters",
+			consoles = true,
+			category = "captains_res"
+		},	
+		{
+			cat_index = 12,
+			image = "enemy_winters",
+			consoles = true,
+			category = "captains_res"
+		},	
+		{
+			cat_index = 1,
+			image = "general_cameras",
+			consoles = true,
+			category = "stealth_res"
+		},	
+		{
+			cat_index = 2,
+			image = "enemy_guards",
+			consoles = true,
+			category = "stealth_res"
+		},	
+		{
+			cat_index = 3,
+			image = "enemy_guards",
+			consoles = true,
+			category = "stealth_res"
+		},	
+		{
+			cat_index = 4,
+			image = "enemy_guards",
+			consoles = true,
+			category = "stealth_res"
+		},	
+		{
+			cat_index = 5,
+			image = "weapon_silencer",
+			consoles = true,
+			category = "stealth_res"
+		},
+		{
+			cat_index = 6,
+			image = "general_civilians",
+			consoles = true,
+			category = "stealth_res"
+		},	
+		{
+			cat_index = 7,
+			image = "general_technician",
+			consoles = true,
+			category = "stealth_res"
+		},	
+		{
+			cat_index = 8,
+			image = "tactics_stealth",
+			consoles = true,
+			category = "stealth_res"
+		},	
+		{
+			cat_index = 9,
+			image = "tactics_ecm",
+			consoles = true,
+			category = "stealth_res"
+		},	
+		{
+			cat_index = 10,
+			image = "weapon_silencer",
+			consoles = true,
+			category = "stealth_res"
+		},	
+		{
+			cat_index = 11,
+			image = "general_hostages",
+			consoles = true,
+			category = "stealth_res"
+		},		
+		{
+			cat_index = 12,
+			image = "crimenet_stealth",
+			consoles = true,
+			category = "stealth_res"
+		},	
+		{
+			cat_index = 13,
+			image = "enemy_guards",
+			consoles = true,
+			category = "stealth_res"
+		},
+		{
+			cat_index = 14,
+			image = "crimenet_stealth",
+			consoles = true,
+			category = "stealth_res"
+		},		
+		{
+			cat_index = 15,
+			image = "tactics_stealth",
+			consoles = true,
+			category = "stealth_res"
+		},		
+		{
+			cat_index = 16,
+			image = "general_bodybag",
+			consoles = true,
+			category = "stealth_res"
+		},	
+		{
+			cat_index = 17,
+			image = "general_cabletie",
+			consoles = true,
+			category = "stealth_res"
+		},	
+		{
+			cat_index = 18,
+			image = "enemy_guards",
+			consoles = true,
+			category = "stealth_res"
+		},	
+		{
+			cat_index = 19,
+			image = "tactics_stealth",
+			consoles = true,
+			category = "stealth_res"
+		},
+		{
+			cat_index = 20,
+			image = "crimenet_stealth",
+			consoles = true,
+			category = "stealth_res"
+		},	
+		{
+			cat_index = 21,
+			image = "tactics_stealth",
+			consoles = true,
+			category = "stealth_res"
+		},		
+		{
+			cat_index = 1,
+			image = "general_enforcer",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 2,
+			image = "weapon_primary_secondary",
+			consoles = true,
+			category = "equip_skills_res"
+		},
+		{
+			cat_index = 3,
+			image = "tactics_shooting",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 4,
+			image = "general_ghost",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 5,
+			image = "general_technician",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 6,
+			image = "general_technician",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 7,
+			image = "general_perks",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 8,
+			image = "general_perks",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 9,
+			image = "heister_wick",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 10,
+			image = "heister_dallas",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 11,
+			image = "heister_bonnie",
+			consoles = true,
+			category = "equip_skills_res"
+		},		
+		{
+			cat_index = 12,
+			image = "heister_jimmy",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 13,
+			image = "general_perks",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 14,
+			image = "heister_clover",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 15,
+			image = "heister_scarface",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 16,
+			image = "general_perks",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 17,
+			image = "general_enforcer",
+			consoles = true,
+			category = "equip_skills_res"
+		},
+		{
+			cat_index = 18,
+			image = "general_fugitive",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 19,
+			image = "general_ghost",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 20,
+			image = "general_ghost",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 21,
+			image = "weapon_melee",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 22,
+			image = "weapon_melee",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 23,
+			image = "tactics_headshot",
+			consoles = true,
+			category = "equip_skills_res"
+		},
+		{
+			cat_index = 24,
+			image = "weapon_melee",
+			consoles = true,
+			category = "equip_skills_res"
+		},
+		{
+			cat_index = 25,
+			image = "enemy_flashbang",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 26,
+			image = "tactics_shooting",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 27,
+			image = "weapon_melee",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 28,
+			image = "general_equipment",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 29,
+			image = "general_equipment",
+			consoles = true,
+			category = "equip_skills_res"
+		},
+		{
+			cat_index = 30,
+			image = "tactics_revive",
+			consoles = true,
+			category = "equip_skills_res"
+		},	
+		{
+			cat_index = 31,
+			image = "general_perks",
+			consoles = true,
+			category = "equip_skills_res"
+		},		
+		{
+			cat_index = 1,
+			image = "crimenet_heists",
+			consoles = true,
+			category = "misc_res"
+		}		
 	}
 	self.category_totals = {}
 

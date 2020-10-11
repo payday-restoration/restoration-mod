fc67c5167d2881ebdc9f4266c6f02819a95c9954
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2020-10-11 21:29:29 +0100

    Fixed halloween color grading not working

--- lua/sc/managers/menu/menuscenemanager.lua
+++ lua/sc/managers/menu/menuscenemanager.lua
@@ -1,15 +1,11 @@
 Month = os.date("%m")
 Day = os.date("%d")	
 
---[[
-
 Hooks:PostHook(MenuSceneManager, "_set_up_environments", "ResHalloweenColorGrade", function(self)
 	if Month == "10" and restoration.Options:GetValue("OTHER/Holiday") then
 		self._environments.standard.color_grading = "color_halloween"
 	end
 end)
-
-]]--
 
 function MenuSceneManager:_setup_bg()
 	local yaw = 180

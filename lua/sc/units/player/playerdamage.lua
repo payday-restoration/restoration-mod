b6cd334660472ed99eb062ad5dfd5504b6c3599c
Ravicale <epicnetmail@gmail.com>
2020-10-18 16:09:45 -0500

    Fixed potential crash.

--- lua/sc/units/player/playerdamage.lua
+++ lua/sc/units/player/playerdamage.lua
@@ -780,7 +780,7 @@ function PlayerDamage:damage_bullet(attack_data, ...)
 	self:_call_listeners(damage_info)
 	
 	--Attacks with slow debuffs.
-	if alive(attack_data.attacker_unit) and tweak_data.character[attack_data.attacker_unit:base()._tweak_table].slowing_bullets and alive(self._unit) and not self._unit:movement():current_state().driving then
+	if alive(attack_data.attacker_unit) and tweak_data.character[attack_data.attacker_unit:base()._tweak_table] and tweak_data.character[attack_data.attacker_unit:base()._tweak_table].slowing_bullets and alive(self._unit) and not self._unit:movement():current_state().driving then
 		local slow_data = tweak_data.character[attack_data.attacker_unit:base()._tweak_table].slowing_bullets
 		if slow_data.taunt then
 			attack_data.attacker_unit:sound():say("post_tasing_taunt")

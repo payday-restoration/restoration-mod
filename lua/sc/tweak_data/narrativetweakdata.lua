c0eb2557d1b092a8e88e04db8c34dc1a8d00fff8
bad@names <averygoodname77@gmail.com>
2020-10-28 19:37:00 -0400

    restarting the fire

--- lua/sc/tweak_data/narrativetweakdata.lua
+++ lua/sc/tweak_data/narrativetweakdata.lua
@@ -20,5 +20,32 @@ Hooks:PostHook( NarrativeTweakData, "init", "SC_Narratives", function(self)
 		700000,
 		700000,
 		700000
-		}			
+		}	
+		
+	self.stages = {
+		firestarter_1_res = {
+			type = "d",
+			type_id = "heist_type_hijack",
+			level_id = "firestarter_1_res"
+		},
+		firestarter_2_res = {
+			type = "d",
+			type_id = "heist_type_stealth",
+			level_id = "firestarter_2_res"
+		},
+		firestarter_3_res = {
+			type = "d",
+			level_id = "firestarter_3_res",
+			type_id = "heist_type_knockover",
+			mission = "default",
+			mission_filter = {
+				5
+			}
+		}
+	}
+	self.jobs.firestarter.chain = {
+		self.stages.firestarter_1_res,
+		self.stages.firestarter_2_res,
+		self.stages.firestarter_3_res
+		}
 end)

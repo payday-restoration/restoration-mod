70c64a33f652ea44d217a1f3f301409c1c64f9c3
Ravicale <epicnetmail@gmail.com>
2020-12-31 12:42:29 -0600

    Removed unused Raid Lockpicking code.

--- lua/sc/units/player/playerstandard.lua
+++ lua/sc/units/player/playerstandard.lua
@@ -1438,38 +1438,6 @@ function PlayerStandard:_get_unit_intimidation_action(intimidate_enemies, intimi
 	return self:_get_intimidation_action(prime_target, char_table, intimidation_amount, primary_only, detect_only, secondary)
 end
 
-function PlayerStandard:interrupt_all_actions()
-	local t = TimerManager:game():time()
-
-	self:_interupt_action_reload(t)
-	self:_interupt_action_steelsight(t)
-	self:_interupt_action_running(t)
-	self:_interupt_action_charging_weapon(t)
-	self:_interupt_action_interact(t)
-	self:_interupt_action_ladder(t)
-	self:_interupt_action_melee(t)
-	self:_interupt_action_throw_grenade(t)
-	self:_interupt_action_throw_projectile(t)
-	self:_interupt_action_use_item(t)
-	self:_interupt_action_cash_inspect(t)
-end
-
---RAID minigames.  Disabled for the time being.
---[[Hooks:PostHook(PlayerStandard, "_start_action_interact", "RaidMinigameCheckInteraction", function(self, t, input, timer, object)
-	if alive(object) then
-		local name = object:interaction().tweak_data
-		local tweak = tweak_data.interaction[name]
-		if name ~= "open_door_with_keys" then -- Crashes & is not really lockpicking in the same sense.
-			if tweak and (tweak.is_lockpicking or tweak.special_interaction == "raid") then
-				game_state_machine:change_state_by_name("ingame_special_interaction", {object = object, type = tweak.special_interaction or "raid"})
-				self._interact_expire_t = nil
-				managers.hud:hide_interaction_bar()
-				object:interaction():_post_event(self._unit, "sound_interupt")
-			end
-		end
-	end
-end)]]--
-
 --Replace coroutine with a playermanager function. The coroutine had issues with randomly not being called- or not having values get reset, and overall being jank???
 function PlayerStandard:_find_pickups(t)
 	local pickups = World:find_units_quick("sphere", self._unit:movement():m_pos(), self._pickup_area, self._slotmask_pickups)

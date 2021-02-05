0e5c2214370f00b2d0329bf83a6b921e73a0467c
Ravicale <epicnetmail@gmail.com>
2021-02-05 12:09:08 -0600

    Some deployable code cleanup.
    Removed unused ammobagbase hook.
    Fixed up the doctorbagbase hook.

--- lua/sc/units/equipment/doctorbagbase.lua
+++ lua/sc/units/equipment/doctorbagbase.lua
@@ -1,34 +1,23 @@
-function DoctorBagBase:_set_visual_stage()
-	local percentage = self._amount / self._max_amount
+function DoctorBagBase:take(unit)
+	if self._empty then
+		return
+	end
+	
+	--Remove first aid damage reduction.
 
-	if self._unit:damage() then
-		local state = "state_" .. math.ceil(percentage * 3)
+	local taken = self:_take(unit)
 
-		if self._unit:damage():has_sequence(state) then
-			self._unit:damage():run_sequence_simple(state)
-		end
+	if taken > 0 then
+		unit:sound():play("pickup_ammo")
+		managers.network:session():send_to_peers_synched("sync_doctor_bag_taken", self._unit, taken)
+		managers.mission:call_global_event("player_refill_doctorbag")
 	end
 
-	function DoctorBagBase:take(unit)
-		if self._empty then
-			return
-		end
-		
-		local taken = self:_take(unit)
-
-		if taken > 0 then
-			unit:sound():play("pickup_ammo")
-			managers.network:session():send_to_peers_synched("sync_doctor_bag_taken", self._unit, taken)
-			managers.mission:call_global_event("player_refill_doctorbag")
-		end
-
-		if self._amount <= 0 then
-			self:_set_empty()
-		else
-			self:_set_visual_stage()
-		end
-
-		return taken > 0
+	if self._amount <= 0 then
+		self:_set_empty()
+	else
+		self:_set_visual_stage()
 	end
 
+	return taken > 0
 end

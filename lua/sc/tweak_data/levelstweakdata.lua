4a013e93d73e0cdce5f5b3e3b641d22a6d2dccf0
Neslon-Poggers <nanny01023@gmail.com>
2021-02-15 15:06:13 -0500

    fuck off
    unloaded rvd package on ggc

--- lua/sc/tweak_data/levelstweakdata.lua
+++ lua/sc/tweak_data/levelstweakdata.lua
@@ -214,7 +214,7 @@ Hooks:PostHook( LevelsTweakData, "init", "SC_levels", function(self)
 	self.friend.player_style = "suit_sunny"
 	
 	self.kenaz.ai_group_type = lapd
-	self.kenaz.package = {"packages/kenaz", "packages/narr_rvd", "packages/lapdassets", "packages/rex_gold"}	
+	self.kenaz.package = {"packages/kenaz", "packages/lapdassets", "packages/rex_gold"}	
 	self.kenaz.player_style = "continental"				
 	
 	self.rvd1.ai_group_type = lapd

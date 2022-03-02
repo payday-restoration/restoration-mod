a93b9b77c2f9512a3facc93ff7f70244d0ea634c
Reeeno <EvilBobarino@gmail.com>
2022-03-02 05:06:42 +0000

    a

--- lua/sc/units/enemies/copdamage.lua
+++ lua/sc/units/enemies/copdamage.lua
@@ -3092,22 +3092,6 @@ function CopDamage:_on_damage_received(damage_info)
 		end
 	end
 	
-	
-	
-	local attacker_unit = damage_info and damage_info.attacker_unit
-	if Network:is_server() and damage_info.result.type == "death" and alive(attacker_unit) and attacker_unit:base() then -- if this gets run on clients, don't do anything
-		local weapon_unit = attacker_unit.inventory and attacker_unit:inventory() and attacker_unit:inventory():equipped_unit()
-		--[[if weapon_unit and weapon_unit:base():is_category("saw") then
-			managers.groupai:state():_voice_saw
-		elseif attacker_unit:base().sentry_gun then
-			managers.groupai:state():_voice_sentry
-		else]]--
-		if attacker_unit:base().is_tripmine then -- might not work since the tripmine might've been destroyed by now
-			managers.groupai:state():_voice_trip_mine
-
-		end
-	end
-	
 end
 
 function CopDamage:damage_mission(attack_data)

15e2757d6c9b4926365f00a2f70671c20fec5f80
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2022-03-10 16:46:52 +0000

    Actually correct wwise callback for the whizby
    This doesn't change anything, it's just how it should've been set up. I wasn't familiar with how to set up a wwise callback.

--- lua/sc/units/player/playersound.lua
+++ lua/sc/units/player/playersound.lua
@@ -1,10 +1,10 @@
 function PlayerSound:play_whizby(params)
 	local sound_source = SoundDevice:create_source("whizby")
 	sound_source:set_position(params.position) -- actually accurate position
-	sound_source:post_event("bullet_whizby_medium", callback(self, self, "clbk_whizby_finished", sound_source), nil, "end_of_event")
+	sound_source:post_event("bullet_whizby_medium", callback(self, self, "clbk_whizby_finished"), nil, "end_of_event")
 end
 
-function PlayerSound:clbk_whizby_finished(sound_source)
+function PlayerSound:clbk_whizby_finished(instance, sound_source)
 	sound_source:stop() -- probably not needed, but just in case
 	sound_source:delete()
 end

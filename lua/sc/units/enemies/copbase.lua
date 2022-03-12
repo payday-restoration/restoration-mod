f21beaf780a1ad77b1edbd6c12b5a37210282464
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2022-03-12 18:59:31 +0000

    Add a missing comma

--- lua/sc/units/enemies/copbase.lua
+++ lua/sc/units/enemies/copbase.lua
@@ -224,7 +224,7 @@ local weapons_map = {
 	[Idstring("units/pd2_dlc_hvh/characters/ene_swat_hvh_1/ene_swat_hvh_1"):key()] = "m4_blue",
 
 	--Yellow Heavy SWAT Weapon Changes (test)
-	[Idstring("units/pd2_dlc_mad/characters/ene_akan_cs_heavy_ak47_ass/ene_akan_cs_heavy_ak47_ass"):key()] = "ak102"
+	[Idstring("units/pd2_dlc_mad/characters/ene_akan_cs_heavy_ak47_ass/ene_akan_cs_heavy_ak47_ass"):key()] = "ak102",
 
 	--Biker Weapon Changes--
 	[Idstring("units/payday2/characters/ene_biker_1/ene_biker_1"):key()] = "mac11",

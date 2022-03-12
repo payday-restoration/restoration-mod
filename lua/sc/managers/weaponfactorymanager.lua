d6abecd3be7e24221e9fffb321dd0c783b1e16f9
Lolnoob1 <12839830+Lolnoobwut1@users.noreply.github.com>
2022-03-12 14:48:37 -0800

    RPK Potassium Mag Animations (and other stuff)
    - Added functionality to apply animation weights per attachment. Currently only used by the RPK's Potassium Magazine, but will be expanded in the future for vertical grips, drum magazines, etc.
    - Added new reload animations for the RPK's Potassium Magazine.
    - Reorganized some XML tweaks to prevent file bloat.
    - Removed redundant code.

--- lua/sc/managers/weaponfactorymanager.lua
+++ lua/sc/managers/weaponfactorymanager.lua
@@ -1,17 +1,15 @@
-function WeaponFactoryManager:get_part_desc_by_part_id_from_weapon(part_id, factory_id, blueprint)
+--Gets any animation weights from attachments (vertical grip, drum mag, etc)
+function WeaponFactoryManager:get_animation_weights_from_weapon(factory_id, blueprint)
 	local factory = tweak_data.weapon.factory
-	local override = self:_get_override_parts(factory_id, blueprint)
-	local part = self:_part_data(part_id, factory_id, override)
-	local desc_id = part.desc_id or tweak_data.blackmarket.weapon_mods[part_id].desc_id
-	local params = {
-	BTN_GADGET = managers.localization:btn_macro("weapon_gadget", true),
-	BTN_BIPOD = managers.localization:btn_macro("deploy_bipod", true)
-	}
+	local t = {}
 
-	if managers.menu:is_pc_controller() and managers.localization:exists(desc_id .. "_pc")then
-			return managers.localization:text(desc_id .. "_pc", params)
-		else
-			return managers.localization:text(desc_id, params)
+	for _, id in ipairs(self:get_assembled_blueprint(factory_id, blueprint)) do
+		local part = self:_part_data(id, factory_id)
+
+		if part.fps_animation_weight then
+			t[id] = part.fps_animation_weight
 		end
-	return Application:production_build() and "Add ##desc_id## to ##" .. part_id .. "## in tweak_data.blackmarket.weapon_mods" or ""
+	end
+
+	return t
 end

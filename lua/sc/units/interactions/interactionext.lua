3c306934e6a2bfb8388d63ffe0e7a59914a1efc7
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2022-03-14 22:35:36 +0000

    Fix bags sticking around for a split second after pickup for clients
    Yes this is really a two line fix, thanks Overkill!

--- lua/sc/units/interactions/interactionext.lua
+++ lua/sc/units/interactions/interactionext.lua
@@ -339,3 +339,55 @@ function IntimitateInteractionExt:interact(player)
 		self._unit:brain():on_tied(player, false, managers.player:has_category_upgrade("player", "civilians_dont_flee")) --No longer uses super_syndrome.
 	end
 end
+
+function CarryInteractionExt:sync_interacted(peer, player, status, skip_alive_check)
+	local no_player = player == nil
+	player = player or peer:unit()
+
+	if peer and not managers.player:register_carry(peer, self._unit:carry_data() and self._unit:carry_data():carry_id()) then
+		return
+	end
+
+	if self._unit:damage():has_sequence("interact") then
+		self._unit:damage():run_sequence_simple("interact", {
+			unit = player
+		})
+	end
+
+	if self._unit:damage():has_sequence("load") then
+		self._unit:damage():run_sequence_simple("load", {
+			unit = player
+		})
+	end
+
+	if self._global_event then
+		managers.mission:call_global_event(self._global_event, player)
+	end
+
+	if Network:is_server() then
+		if self._remove_on_interact then
+			if self._unit == managers.interaction:active_unit() then
+				self:interact_interupt(managers.player:player_unit(), false)
+			end
+
+			self:remove_interact()
+			self:set_active(false, true)
+
+			if alive(player) then
+				self._unit:carry_data():trigger_load(player)
+			end
+
+			self._unit:set_slot(0)
+		end
+
+		if peer then
+			managers.player:set_carry_approved(peer)
+		end
+	elseif self._remove_on_interact then
+		self._unit:set_visible(false) -- hide the bag until the host despawns it so it doesn't stick around for a split second after pickup
+	end
+
+	if no_player then
+		managers.mission:call_global_event("on_picked_up_carry", self._unit)
+	end
+end

f40853edaf330ee284f53e40037d96508bca21b6
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2022-03-23 18:15:35 +0000

    Actually fix the cop XAudio stuff

--- lua/sc/units/enemies/copbase.lua
+++ lua/sc/units/enemies/copbase.lua
@@ -417,34 +417,19 @@ function CopBase:set_visibility_state(stage)
 	self:chk_freeze_anims()
 end
 
-local init_orig = CopBase.init
-function CopBase:init(unit)
-	init_orig(self, unit)
-	
-	self.my_voice = nil
-	self.voice_length = 0
-	self.voice_start_time = 0
-	self:play_voiceline(nil, nil)
-end
+Hooks:PostHook(CopBase, "init", "res_init", function(self)
+	self.voice_end_t = 0
+end)
 
 function CopBase:play_voiceline(buffer, force)
-	if buffer then
-		if force and self.my_voice and not self.my_voice:is_closed() then
+	local t = TimerManager:game():time()
+	if force or t > self.voice_end_t then
+		if self.my_voice and not self.my_voice:is_closed() then
 			self.my_voice:stop()
 			self.my_voice:close()
-			self.my_voice = nil
-			self.voice_length = 0
 		end
-		local _time = math.floor(TimerManager:game():time())
-		--if self.voice_length == 0 or self.voice_start_time < _time then
-			--[[if self.my_voice and not self.my_voice:is_closed() then
-				self.my_voice:stop()
-				self.my_voice:close()
-				self.my_voice = nil
-			end]]--
-			self.my_voice = XAudio.UnitSource:new(self._unit, buffer)
-			self.voice_length = buffer:get_length()
-			self.voice_start_time = _time + buffer:get_length()
-		--end
+
+		self.my_voice = XAudio.UnitSource:new(self._unit, buffer)
+		self.voice_end_t = t + buffer:get_length()
 	end
 end

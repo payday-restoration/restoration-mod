4ab172bd8cf5e4ca137e5fc143f802fe63d3b19f
blindcowboy24 <86583978+blindcowboy24@users.noreply.github.com>
2022-04-02 19:16:02 -0700

    Update hudteammate.lua
    God I need sleep.

--- lua/sc/managers/hud/hudteammate.lua
+++ lua/sc/managers/hud/hudteammate.lua
@@ -3,20 +3,24 @@ HUDTeammate.set_weapon_firemode_burst = HUDTeammate.set_weapon_firemode_burst or
 
 	If _G.IS_VR then
 
-		local is_secondary = id == 1
-		local secondary_weapon_panel = self._ammo_panel:child("secondary_weapon_panel")
-		local primary_weapon_panel = self._ammo_panel:child("primary_weapon_panel")
-		local weapon_selection = is_secondary and secondary_weapon_panel:child("weapon_selection") or primary_weapon_panel:child("weapon_selection")
+	local is_secondary = id == 1
+	local secondary_weapon_panel = self._ammo_panel:child("secondary_weapon_panel")
+	local primary_weapon_panel = self._ammo_panel:child("primary_weapon_panel")
+	local weapon_selection = is_secondary and secondary_weapon_panel:child("weapon_selection") or primary_weapon_panel:child("weapon_selection")
 
-		if alive(weapon_selection) then
-			local firemode_single = weapon_selection:child("firemode_single")
-			local firemode_auto = weapon_selection:child("firemode_auto")
+	if alive(weapon_selection) then
+		local firemode_single = weapon_selection:child("firemode_single")
+		local firemode_auto = weapon_selection:child("firemode_auto")
 
-			if alive(firemode_single) and alive(firemode_auto) then
-				self:set_weapon_firemode_active(firemode_single, firemode == "single")
-				self:set_weapon_firemode_active(firemode_auto, firemode ~= "single")
-			end
+		if alive(firemode_single) and alive(firemode_auto) then
+			self:set_weapon_firemode_active(firemode_single, firemode == "single")
+			self:set_weapon_firemode_active(firemode_auto, firemode ~= "single")
 		end
+	end
+end
+
+HUDTeammateVR.default_recreate_weapon_firemode = HUDTeammate.recreate_weapon_firemode
+
 	else
 
 		local is_secondary = id == 1
@@ -32,4 +36,9 @@ HUDTeammate.set_weapon_firemode_burst = HUDTeammate.set_weapon_firemode_burst or
 			end
 		end
 	end
+end
+
+function HUDTeammateVR:recreate_weapon_firemode()
+	self:setup_firemode(0, self._ammo_panel:child("primary_weapon_panel"):child("weapon_selection"))
+	self:setup_firemode(1, self._ammo_panel:child("secondary_weapon_panel"):child("weapon_selection"))
 end

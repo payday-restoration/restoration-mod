0b69038e95b8c9bb25cff92d6990306fede5f3c0
blindcowboy24 <86583978+blindcowboy24@users.noreply.github.com>
2022-04-02 18:32:34 -0700

    Update hudteammate.lua

--- lua/sc/managers/hud/hudteammate.lua
+++ lua/sc/managers/hud/hudteammate.lua
@@ -19,16 +19,17 @@ HUDTeammate.set_weapon_firemode_burst = HUDTeammate.set_weapon_firemode_burst or
 		end
 	else
 
-	local is_secondary = id == 1
-	local secondary_weapon_panel = self._player_panel:child("weapons_panel"):child("secondary_weapon_panel")
-	local primary_weapon_panel = self._player_panel:child("weapons_panel"):child("primary_weapon_panel")
-	local weapon_selection = is_secondary and secondary_weapon_panel:child("weapon_selection") or primary_weapon_panel:child("weapon_selection")
-	if alive(weapon_selection) then
-		local firemode_single = weapon_selection:child("firemode_single")
-		local firemode_auto = weapon_selection:child("firemode_auto")
-		if alive(firemode_single) and alive(firemode_auto) then
-			firemode_single:show()
-			firemode_auto:show()
+		local is_secondary = id == 1
+		local secondary_weapon_panel = self._player_panel:child("weapons_panel"):child("secondary_weapon_panel")
+		local primary_weapon_panel = self._player_panel:child("weapons_panel"):child("primary_weapon_panel")
+		local weapon_selection = is_secondary and secondary_weapon_panel:child("weapon_selection") or primary_weapon_panel:child("weapon_selection")
+		if alive(weapon_selection) then
+			local firemode_single = weapon_selection:child("firemode_single")
+			local firemode_auto = weapon_selection:child("firemode_auto")
+			if alive(firemode_single) and alive(firemode_auto) then
+				firemode_single:show()
+				firemode_auto:show()
+			end
 		end
 	end
 end

3e9fa751412e7d49fba3ef16bf950213559340d8
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2022-04-14 22:39:23 +0100

    Update huskcopbase.lua

--- lua/sc/units/enemies/huskcopbase.lua
+++ lua/sc/units/enemies/huskcopbase.lua
@@ -4,11 +4,8 @@ Hooks:PostHook(HuskCopBase, "post_init", "postinithuskbase", function(self)
 	--Cloakers lights are always on
 	if self._tweak_table == "spooc" then
 		self._unit:damage():run_sequence_simple("turn_on_spook_lights")
-	end
-	
-	--Captain spawned, enable buff icon
-	if self._tweak_table == "phalanx_vip" or self._tweak_table == "spring" or self._tweak_table == "summers" or self._tweak_table == "headless_hatman" or managers.skirmish:is_skirmish() and self._tweak_table == "autumn" then
-		managers.hud:set_buff_enabled("vip", true)
+	elseif self._tweak_table == "phalanx_vip" or self._tweak_table == "spring" or self._tweak_table == "summers" or self._tweak_table == "headless_hatman" or managers.skirmish:is_skirmish() and self._tweak_table == "autumn" then
+		managers.hud:set_buff_enabled("vip", true) --Captain spawned, enable buff icon
 	end
 	
 	if self._tweak_table == "triad_boss" then
@@ -31,4 +28,8 @@ Hooks:PostHook(HuskCopBase, "post_init", "postinithuskbase", function(self)
 		self._unit:body("rag_RightUpLeg"):set_sphere_radius(10)
 		self._unit:body("rag_RightLeg"):set_sphere_radius(7)
 	end
+		
+	self._unit:character_damage():add_listener("asu_laser_state" .. tostring(self._unit:key()), {
+		"death"
+	}, callback(self, self, "enable_asu_laser"))
 end)

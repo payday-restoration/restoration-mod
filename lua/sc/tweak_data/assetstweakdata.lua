eeacd6ca861206cd2a7c9055ba44b6a8212cb0e6
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2022-04-16 16:39:46 +0200

    Removed Murkys from HO version of Watchdogs Day 2
    I keep their HO package though

--- lua/sc/tweak_data/assetstweakdata.lua
+++ lua/sc/tweak_data/assetstweakdata.lua
@@ -40,7 +40,6 @@ function AssetsTweakData:_init_risk_assets(tweak_data)
 	table.insert(self.risk_pd.exclude_stages, "welcome_to_the_jungle_2")	
 	table.insert(self.risk_pd.exclude_stages, "pex")	
 	table.insert(self.risk_pd.exclude_stages, "fex")
-	table.insert(self.risk_pd.exclude_stages, "skm_watchdogs_stage2")
 
 	--SWAT--		
 	table.insert(self.risk_swat.exclude_stages, "shoutout_raid")
@@ -65,7 +64,6 @@ function AssetsTweakData:_init_risk_assets(tweak_data)
 	table.insert(self.risk_swat.exclude_stages, "welcome_to_the_jungle_2")	
 	table.insert(self.risk_swat.exclude_stages, "pex")
 	table.insert(self.risk_swat.exclude_stages, "fex")
-	table.insert(self.risk_swat.exclude_stages, "skm_watchdogs_stage2")
 	
 	--FBI--		
 	table.insert(self.risk_fbi.exclude_stages, "shoutout_raid")
@@ -90,7 +88,6 @@ function AssetsTweakData:_init_risk_assets(tweak_data)
 	table.insert(self.risk_fbi.exclude_stages, "welcome_to_the_jungle_2")	
 	table.insert(self.risk_fbi.exclude_stages, "pex")
 	table.insert(self.risk_fbi.exclude_stages, "fex")
-	table.insert(self.risk_fbi.exclude_stages, "skm_watchdogs_stage2")
 
 	--Overkill--		
 	table.insert(self.risk_death_squad.exclude_stages, "shoutout_raid")
@@ -115,7 +112,6 @@ function AssetsTweakData:_init_risk_assets(tweak_data)
 	table.insert(self.risk_death_squad.exclude_stages, "welcome_to_the_jungle_2")
 	table.insert(self.risk_death_squad.exclude_stages, "pex")
 	table.insert(self.risk_death_squad.exclude_stages, "fex")
-	table.insert(self.risk_death_squad.exclude_stages, "skm_watchdogs_stage2")
 
 	--Mayhem--		
 	table.insert(self.risk_easy_wish.exclude_stages, "shoutout_raid")
@@ -140,7 +136,6 @@ function AssetsTweakData:_init_risk_assets(tweak_data)
 	table.insert(self.risk_easy_wish.exclude_stages, "welcome_to_the_jungle_2")
 	table.insert(self.risk_easy_wish.exclude_stages, "pex")
 	table.insert(self.risk_easy_wish.exclude_stages, "fex")
-	table.insert(self.risk_easy_wish.exclude_stages, "skm_watchdogs_stage2")
 
 	--Deathwish--		
 	table.insert(self.risk_death_wish.exclude_stages, "shoutout_raid")
@@ -165,7 +160,6 @@ function AssetsTweakData:_init_risk_assets(tweak_data)
 	table.insert(self.risk_death_wish.exclude_stages, "welcome_to_the_jungle_2")
 	table.insert(self.risk_death_wish.exclude_stages, "pex")
 	table.insert(self.risk_death_wish.exclude_stages, "fex")
-	table.insert(self.risk_death_wish.exclude_stages, "skm_watchdogs_stage2")
 	
 	--One Down--		
 	table.insert(self.risk_sm_wish.exclude_stages, "shoutout_raid")
@@ -190,7 +184,6 @@ function AssetsTweakData:_init_risk_assets(tweak_data)
 	table.insert(self.risk_sm_wish.exclude_stages, "welcome_to_the_jungle_2")
 	table.insert(self.risk_sm_wish.exclude_stages, "pex")
 	table.insert(self.risk_sm_wish.exclude_stages, "fex")
-	table.insert(self.risk_sm_wish.exclude_stages, "skm_watchdogs_stage2")
 	
 	-- Reaper Risk Asset
 	table.insert(self.mad_russian_merc_cameras.stages, "pines")
@@ -210,8 +203,7 @@ function AssetsTweakData:_init_risk_assets(tweak_data)
 		"vit",
 		"arm_for",
 		"crojob2",
-		"mex",
-		"skm_watchdogs_stage2"
+		"mex"
 		--"wwh" Intended as we weren't expecting a fight at all on Alaskan Deal
 	}		
 	self.risk_zombie.stages = {

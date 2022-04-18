3767feb45e6825dafdeb01abaaee0a8ee6a0071e
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2022-04-18 16:04:28 +0100

    This was already fixed
    And properly, at that.

--- lua/sc/units/enemies/copmovement.lua
+++ lua/sc/units/enemies/copmovement.lua
@@ -75,15 +75,6 @@ function CopMovement:init(unit)
 	old_init(self, unit)		
 end
 
--- Fix enemies playing the suppressed stand-to-crouch animation when shot even if they are already crouching
-local action_request_original = CopMovement.action_request
-function CopMovement:action_request(action_desc, ...)
-	if action_desc.variant == "suppressed_reaction" and self._ext_anim.crouch then
-		return
-	end
-	return action_request_original(self, action_desc, ...)
-end
-
 function CopMovement:post_init()
 	local unit = self._unit
 	self._ext_brain = unit:brain()

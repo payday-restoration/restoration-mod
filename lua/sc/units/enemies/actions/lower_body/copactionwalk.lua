7a3a7b005afbf0da6ba943f2d85d9fbd5474c620
Reeeno <EvilBobarino@gmail.com>
2022-04-18 09:52:55 +0100

    missing things from sh

--- lua/sc/units/enemies/actions/lower_body/copactionwalk.lua
+++ lua/sc/units/enemies/actions/lower_body/copactionwalk.lua
@@ -1806,3 +1806,8 @@ function CopActionWalk:_husk_needs_speedup()
 		end
 	end
 end
+
+- Helper function to get the final path position
+function CopActionWalk:get_destination_pos()
+	return self._nav_point_pos(self._simplified_path and self._simplified_path[#self._simplified_path] or self._nav_path and self._nav_path[#self._nav_path])
+end

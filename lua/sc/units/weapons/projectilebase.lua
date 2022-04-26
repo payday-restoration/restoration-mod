02675ef3d35717ee027c243a7347a2decd7a24c5
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2022-04-26 11:58:36 +0200

    fixed an another smol oopsie crash

--- lua/sc/units/weapons/projectilebase.lua
+++ lua/sc/units/weapons/projectilebase.lua
@@ -86,7 +86,7 @@ Hooks:PreHook(ProjectileBase, "throw_projectile_npc", "sh_throw_projectile_npc",
 	local tweak_entry = tweak_data.blackmarket.projectiles[projectile_type]
 	local unit_name = Idstring(not Network:is_server() and tweak_entry.local_unit or tweak_entry.unit)
 	if not PackageManager:has(unit_ids, unit_name) then
-		StreamHeist:log("Loading projectile unit", projectile_type)
+		restoration:log("Loading projectile unit", projectile_type)
 		managers.dyn_resource:load(unit_ids, unit_name, managers.dyn_resource.DYN_RESOURCES_PACKAGE)
 	end
 end)

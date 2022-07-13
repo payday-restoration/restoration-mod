4588b4d712e047720056b68eca6c85826a0fd2f8
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2022-07-13 11:52:35 +0100

    test to see if that fixed it

--- lua/sc/units/enemies/actions/full_body/copactiondodge.lua
+++ lua/sc/units/enemies/actions/full_body/copactiondodge.lua
@@ -27,18 +27,8 @@ function CopActionDodge:init(action_desc, common_data)
 		self._machine:set_parameter(redir_res, action_desc.side, 1)
 		self._machine:set_parameter(redir_res, action_desc.variation, 1)
 
-		local speed = action_desc.speed or 1
-
-		if speed ~= 1 then
-			if action_desc.variation == "side_step" then
-				speed = math_min(speed, 1.2)
-			elseif action_desc.variation == "wheel" then
-				speed = math_min(speed, 1.4)
-			else
-				speed = math_min(speed, 1.6)
-			end
-
-			self._machine:set_speed(redir_res, speed)
+		if action_desc.speed then
+			self._machine:set_speed(redir_res, action_desc.speed)
 		end
 
 		CopActionAct._create_blocks_table(self, action_desc.blocks)

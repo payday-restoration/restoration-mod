717bb87ccda0cec0445529f6cd166c1be5b5b82f
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2022-07-13 17:37:44 +0100

    Should fix the crash on startup

--- lua/sc/managers/sidejobeventmanager.lua
+++ lua/sc/managers/sidejobeventmanager.lua
@@ -1,5 +1,5 @@
-function SideJobEventManager:is_event_active()
+function SideJobEventManager:is_event_active(event_id)
 	--return true
 	--Doing this for now, wouldn't mind keeping the event active at all times if we readd the Cash Blaster
-	return self._global.event_stage < 5
+	return self._global.event_stage[event_id] < 5
 end

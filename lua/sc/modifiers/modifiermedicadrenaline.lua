fadd420672517911133ef132fce9139acf6a9d29
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2022-07-18 15:02:19 +0200

    this too

--- lua/sc/modifiers/modifiermedicadrenaline.lua
+++ lua/sc/modifiers/modifiermedicadrenaline.lua
@@ -8,6 +8,7 @@ ModifierMedicAdrenaline.medics = {
 
 ModifierMedicAdrenaline.reapermedic = {
 	Idstring("units/pd2_dlc_mad/characters/ene_akan_medic_bob/ene_akan_medic_bob"),
+	Idstring("units/pd2_mod_reapers/characters/ene_drak_medic/ene_drak_medic"),
 	Idstring("units/pd2_dlc_mad/characters/ene_akan_medic_zdann/ene_akan_medic_zdann")
 }	
 
@@ -24,7 +25,7 @@ function ModifierMedicAdrenaline:modify_value(id, value)
 		if is_medic and math.random(0,100) < 10 then
 			return Idstring("units/pd2_dlc_vip/characters/ene_omnia_lpf/ene_omnia_lpf")
 		elseif is_reapermedic and math.random(0,100) < 10 then
-			return Idstring("units/pd2_dlc_mad/characters/ene_akan_lpf/ene_akan_lpf")				
+			return Idstring("units/pd2_mod_reapers/characters/ene_subject_enforcer/ene_subject_enforcer")				
 		elseif is_zombiemedic and math.random(0,100) < 10 then
 			return Idstring("units/pd2_mod_halloween/characters/ene_omnia_lpf/ene_omnia_lpf")				
 		end

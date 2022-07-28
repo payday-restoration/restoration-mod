5f4720d83c65d8a6ef04547bb8377cc825aeb024
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2022-07-28 17:43:08 +0200

    wha

--- lua/sc/units/enemies/copbase.lua
+++ lua/sc/units/enemies/copbase.lua
@@ -378,11 +378,9 @@ local weapons_map = {
 	
 	[Idstring("units/pd2_dlc_casino/characters/ene_secret_service_1_casino/ene_secret_service_1_casino"):key()] = {"m1911_npc", "mp5", "m4"},
 	
-	[Idstring("units/pd2_mod_omnia/characters/ene_omnia_crew/ene_omnia_crew"):key()] = {"deagle", "ump", "m4"},
-	[Idstring("units/pd2_mod_omnia/characters/ene_omnia_crew_2/ene_omnia_crew_2"):key()] = {"deagle", "ump", "m4"},
 	
-	[Idstring("units/pd2_mod_nypd/characters/ene_nypd_murky_1/ene_nypd_murky_1"):key()] = {"m1911_npc", "c45", "ump", "m4"},
-	[Idstring("units/pd2_mod_nypd/characters/ene_nypd_murky_2/ene_nypd_murky_2"):key()] = {"m1911_npc", "c45", "ump", "m4"},
+	[Idstring("units/pd2_mod_nypd/characters/ene_nypd_murky_1/ene_nypd_murky_1"):key()] = {"m1911_npc", "ump", "m4"},
+	[Idstring("units/pd2_mod_nypd/characters/ene_nypd_murky_2/ene_nypd_murky_2"):key()] = {"m1911_npc", "ump", "m4"},
 	
 	[Idstring("units/pd2_dlc_bex/characters/ene_bex_security_01/ene_bex_security_01"):key()] = {"c45", "mp5", "r870"},
 	[Idstring("units/pd2_dlc_bex/characters/ene_bex_security_02/ene_bex_security_02"):key()] = {"c45", "mp5", "r870"},

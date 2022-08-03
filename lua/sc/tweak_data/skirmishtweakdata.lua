c351d2a05e8bc698446c3ebdeeb4fc788f258747
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2022-08-03 15:23:35 +0200

    lowered the HO special spawn limits by 1

--- lua/sc/tweak_data/skirmishtweakdata.lua
+++ lua/sc/tweak_data/skirmishtweakdata.lua
@@ -2,41 +2,14 @@ Month = os.date("%m")
 Day = os.date("%d")	
 --This is probs unused, but setting to scaled (for the average skirmish map size) DS values to be on the safe side.
 function SkirmishTweakData:_init_special_unit_spawn_limits()
-	local map_scale_factor = 1
-	for _,vl in pairs(restoration.very_large_levels) do
-		if job == vl then
-			map_scale_factor = 1.3
-		end
-	end		
-	for _,l in pairs(restoration.large_levels) do
-		if job == l then
-			map_scale_factor = 1.15
-		end
-	end	
-	for _,t in pairs(restoration.tiny_levels) do
-		if job == t then
-			map_scale_factor = 0.85
-		end
-	end
-	for _,vt in pairs(restoration.very_tiny_levels) do
-		if job == vt then
-			map_scale_factor = 0.7
-		end
-	end
-	for _,et in pairs(restoration.extremely_tiny_levels) do
-		if job == et then
-			map_scale_factor = 0.55
-		end
-	end
-
 	self.special_unit_spawn_limits = {
 		{
-			tank = math.max(math.round(4 * map_scale_factor), 1),
-			taser = math.max(math.round(5 * map_scale_factor), 1),
-			boom = math.max(math.round(3 * map_scale_factor), 1),
-			spooc = math.max(math.round(4 * map_scale_factor), 1),
-			shield = math.max(math.round(5 * map_scale_factor), 1),
-			medic = math.max(math.round(4 * map_scale_factor), 1),
+			tank = math.max(math.round(3 * map_scale_factor), 1),
+			taser = math.max(math.round(4 * map_scale_factor), 1),
+			boom = math.max(math.round(2 * map_scale_factor), 1),
+			spooc = math.max(math.round(3 * map_scale_factor), 1),
+			shield = math.max(math.round(4 * map_scale_factor), 1),
+			medic = math.max(math.round(3 * map_scale_factor), 1),
 			phalanx_vip = 1,
 			spring = 1,
 			headless_hatman = 1,

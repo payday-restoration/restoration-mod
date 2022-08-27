51f8b4fe9a9ff421e9de693adc43a38ad890c18b
Remobit <Remobit@users.noreply.github.com>
2022-08-27 15:00:17 +0000

    Mountain Master edit (part 1) -added 3 bonus loot bags -fixed up the gold closet instance -enabled Knuckles -disabled kitchen lift secure point in loud

--- lua/sc/tweak_data/levelstweakdata.lua
+++ lua/sc/tweak_data/levelstweakdata.lua
@@ -274,8 +274,9 @@ Hooks:PostHook( LevelsTweakData, "init", "SC_levels", function(self)
 	self.chca.player_style = "continental"
 
 	self.pent.ai_group_type = lapd
-	self.pent.package = {"packages/job_pent", "levels/narratives/h_alex_must_die/stage_1/world_sounds", "levels/narratives/dentist/mia/stage2/world_sounds", "packages/miscassets"}
+	self.pent.package = {"packages/job_pent", "levels/narratives/h_alex_must_die/stage_1/world_sounds", "levels/narratives/dentist/mia/stage2/world_sounds", "packages/miscassets", "levels/instances/unique/sah/sah_office/world/world"}
 	self.pent.player_style = "enforcer"
+	self.pent.max_bags = 13
 	
 	--///NEW OUTFITS\\\--
 	

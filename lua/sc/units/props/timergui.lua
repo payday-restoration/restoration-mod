1b6f6b30e3cdf2902824afb02b2b6fcd0e9c5e70
SC <gametwisterdotnet@yahoo.com>
2022-09-11 13:49:06 -0700

    Fixed Hack Timers in stealth (hopefully)

--- lua/sc/units/props/timergui.lua
+++ lua/sc/units/props/timergui.lua
@@ -1,13 +1,19 @@
-local jam = TimerGui.set_jammed
-function TimerGui:set_jammed(jammed,...)
-	if jammed and managers.groupai:state():whisper_mode() then 
-		--2STELTHY4JAM
-		return
-	end
-	return jam(self,jammed,...)
-end
-
 Hooks:PostHook(TimerGui, "setup", "ResDrillFix", function(self)
 	--Lowers drill screen brightness
 	self._gui_script.panel:set_alpha(0.6)
 end)
+
+function DrillTimerGui:set_jammed(jammed)
+	if jammed and managers.groupai:state():whisper_mode() then 
+		--2STELTHY4JAM
+		return
+	end
+
+	if managers.network:session() then
+		local event_id = jammed and DrillTimerGui.EVENT_IDS.jammed or DrillTimerGui.EVENT_IDS.unjammed
+
+		managers.network:session():send_to_peers_synched("sync_unit_event_id_16", self._unit, "DrillTimerGui", event_id)
+	end
+
+	self:_set_jammed(jammed)
+end

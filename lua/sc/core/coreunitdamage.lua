418d55b8a6594aa995bc86b9a048f013156a0035
Noep <everyonewins@live.ca>
2022-09-13 17:16:56 -0400

    this should do it

--- lua/sc/core/coreunitdamage.lua
+++ lua/sc/core/coreunitdamage.lua
@@ -9,3 +9,10 @@ Hooks:PostHook(CoreBodyDamage, "init", "sh_init", function (self, hit_body)
 		self._body_element._damage_multiplier = self._body_element._name:find("glass") and CoreBodyDamage._tank_glass_multiplier or CoreBodyDamage._tank_armor_multiplier
 	end
 end)
+
+function CoreBodyDamage:damage_damage(attack_unit, normal, position, direction, damage, unevadable)
+	local object_damage_mult = alive(attack_unit) and attack_unit:inventory():equipped_unit():base():weapon_tweak_data().object_damage_mult or 1
+	damage = damage * object_damage_mult
+	damage = self:damage_endurance("damage", attack_unit, normal, position, direction, damage, Vector3(0, 0, 0))
+	return self._unit_extension:damage_damage(attack_unit, self._body, normal, position, direction, damage, unevadable)
+end

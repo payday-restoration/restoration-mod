0f08d1d0ed0093d0612f27f6d3f38fd7f3d4d898
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2022-09-25 16:16:04 +0100

    Better loose loot handling
    Loose loot is hidden instantly on pickup for clients now while waiting for the host to despawn

---
+++ lua/sc/units/props/smalllootbase.lua
@@ -0,0 +1,22 @@
+Hooks:PreHook(SmallLootBase, "take", "promod_take", function(self)
+    if not self._empty and not Network:is_server() then
+		self:_set_empty() -- hide on pickup
+	end
+end)
+
+function SmallLootBase:_set_empty()
+	self._empty = true
+
+	if not self.skip_remove_unit then
+		if Network:is_server() or self._unit:id() == -1 then
+			self._unit:set_slot(0)
+		else
+			-- clients don't have the authority to remove network synced units
+			if self._unit:interaction() then
+				self._unit:interaction():set_active(false)
+			end
+
+			self._unit:set_enabled(false)
+		end
+	end
+end

2f9edb831c5ce3ead5727a058e8e1f46f70e0e54
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2022-09-26 14:46:45 +0100

    One last edge case

--- lua/sc/units/pickups/gageassignmentbase.lua
+++ lua/sc/units/pickups/gageassignmentbase.lua
@@ -1,15 +1,16 @@
 -- Show pickup message and hide the unit instantly instead of waiting for response from host
 function GageAssignmentBase:sync_pickup(peer)
-	if not managers.gage_assignment:is_unit_an_assignment(self._unit) then
+	if self._picked_up then
+		return -- client picked it up but just after someone else did
+	elseif not managers.gage_assignment:is_unit_an_assignment(self._unit) then
 		return self:consume()
 	end
 
-	local peer_id = peer and peer:id() or 1
-
 	managers.gage_assignment:on_unit_interact(self._unit, self._assignment)
-	self:show_pickup_msg(peer_id)
+	self:show_pickup_msg(peer)
 
 	if Network:is_server() then
+		local peer_id = peer and peer:id() or 1
 		managers.network:session():send_to_peers_synched_except(peer_id, "sync_unit_event_id_16", self._unit, "base", peer_id) -- don't sync back to the peer that picked it up since they've already handled it on their end
 	end
 
@@ -21,15 +22,20 @@ function GageAssignmentBase:_pickup(unit)
 		return
 	end
 
-	self._picked_up = true
-
 	self:sync_pickup(managers.network:session():local_peer())
+	self._picked_up = true
 
 	if Network:is_client() then
 		managers.network:session():send_to_host("sync_pickup", self._unit)
 	end
 
 	return true
+end
+
+function GageAssignmentBase:show_pickup_msg(peer)
+	if peer then
+		managers.gage_assignment:present_progress(self._assignment, peer:name())
+	end
 end
 
 function GageAssignmentBase:sync_net_event(event_id)

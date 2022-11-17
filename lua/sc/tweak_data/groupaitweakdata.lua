8614cd69b19ca3fd2766e65b29115ed97fa089fe
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2022-11-17 15:19:08 +0100

    Update groupaitweakdata.lua

--- lua/sc/tweak_data/groupaitweakdata.lua
+++ lua/sc/tweak_data/groupaitweakdata.lua
@@ -1910,10 +1910,10 @@ function GroupAITweakData:_init_unit_categories(difficulty_index)
 					Idstring("units/pd2_dlc_vip/characters/ene_titan_taser/ene_titan_taser")							
 				},
 				omnia = {
-					Idstring("units/pd2_mod_omnia/characters/ene_omnia_tazer/ene_omnia_tazer"),
-					Idstring("units/pd2_mod_omnia/characters/ene_omnia_tazer/ene_omnia_tazer"),
-					Idstring("units/pd2_mod_omnia/characters/ene_omnia_tazer/ene_omnia_tazer"),
-					Idstring("units/pd2_mod_omnia/characters/ene_omnia_tazer/ene_omnia_tazer"),					
+					Idstring("units/pd2_mod_omnia/characters/ene_omnia_taser/ene_omnia_taser"),
+					Idstring("units/pd2_mod_omnia/characters/ene_omnia_taser/ene_omnia_taser"),
+					Idstring("units/pd2_mod_omnia/characters/ene_omnia_taser/ene_omnia_taser"),
+					Idstring("units/pd2_mod_omnia/characters/ene_omnia_taser/ene_omnia_taser"),					
 					Idstring("units/pd2_dlc_vip/characters/ene_titan_taser/ene_titan_taser")							
 				}
 			},

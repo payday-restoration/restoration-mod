3b4aa2e94e7945b8dbca3dac2ed0938f4633f7fa
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2022-11-26 18:20:47 +0100

    Moved Diamond/Scarface Mansion stuff to missionman

--- lua/sc/managers/missionmanager.lua
+++ lua/sc/managers/missionmanager.lua
@@ -52,9 +52,286 @@ local level_mission_script_elements = {
 			}
 		}
 	},
+	
 	friend = {
 		[101612] = {
-			enabled = false -- Sosa retreat spot SO selection
+			values = {
+				enabled = false -- Sosa retreat spot SO selection
+			}
+		},
+		--forcing boat escape
+		[100213] = {
+			values = {
+				enabled = false
+			}
+		},
+		[100214] = {
+			values = {
+				enabled = false
+			}
+		},
+		[103446] = {
+			values = {
+				enabled = false --don't even try.....
+			}
+		},
+		--You're Sosa's men, not undercover cops
+		[100852] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[100854] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[100855] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[100856] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[100857] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[100858] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[100858] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[100859] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[100860] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[100861] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[100862] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[100863] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[101185] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[101185] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[101189] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[101771] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102301] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102395] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102399] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102404] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102408] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102419] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102421] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102532] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102535] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102538] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102540] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102542] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102544] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102546] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102548] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102550] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102553] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102557] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102559] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102562] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102565] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102569] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102572] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102576] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102578] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102581] = { 
+			values = {
+				team = "mobster1"
+			}
+		},
+		[102583] = { 
+			values = {
+				team = "mobster1"
+			}
+		}
+	},
+	mus = {
+		[102232] = {
+			values = {
+				enabled = false
+			}
+		},
+		[102233] = {
+			values = {
+				enabled = false
+			}
+		},
+		[102234] = {
+			values = {
+				enabled = false 
+			}
+		},
+		[102235] = {
+			values = {
+				enabled = false 
+			}
+		},
+		[102236] = {
+			values = {
+				enabled = false 
+			}
+		},
+		[102237] = {
+			values = {
+				enabled = false 
+			}
+		},
+		[102238] = {
+			values = {
+				enabled = false 
+			}
+		},
+		[102239] = {
+			values = {
+				enabled = false 
+			}
 		}
 	},
 	help = {
@@ -171,7 +448,7 @@ local level_mission_script_elements = {
 			}
 		}
 	}
-}
+}	
 local mission_script_elements = level_mission_script_elements[level_id]
 if not mission_script_elements then
 	return

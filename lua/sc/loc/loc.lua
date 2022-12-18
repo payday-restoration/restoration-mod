30c868950d3675e90495be05d3b0d3002387a5b1
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2022-12-18 13:36:08 +0100

    Update loc.lua

--- lua/sc/loc/loc.lua
+++ lua/sc/loc/loc.lua
@@ -2809,7 +2809,7 @@ Hooks:Add("LocalizationManagerPostInit", "SC_Localization_Skills", function(loc)
 		["menu_cs_modifier_medic_adrenaline"] = "All Medic units have an additional 15% chance to become an OMNIA LPF/DRAK Flamer depending on the faction.",
 		["menu_cs_modifier_cloaker_arrest"] = "Cloaker melee strikes will now cuff players.",
 		["menu_cs_modifier_cloaker_smoke"] = "Cloakers will now have a 50% chance to drop a flashbang when they dodge.",
-		["menu_cs_modifier_cloaker_tear_gas"] = "All HRT units have an additional 15% chance to become a OMNIA/DRAK ASU.",
+		["menu_cs_modifier_cloaker_tear_gas"] = "All HRT units have an additional 15% chance to become a ASU unit.",
 		["menu_cs_modifier_dozer_lmg"] = "Whenever a Green or Black Bulldozer spawns, there is a chance that it will be replaced by a Skulldozer.",
 		["menu_cs_modifier_10secondsresponsetime"] = "All police assaults now start at maximum intensity.",
 

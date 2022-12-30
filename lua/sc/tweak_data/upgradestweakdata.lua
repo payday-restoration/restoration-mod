9c107c3315f8f46edccabfe0ec406670076a21c3
Noep <everyonewins@live.ca>
2022-12-30 13:41:47 -0500

    Fix Hitman not having the right stats
    - Fixed the temp HP decay being -6 temp HP/sec when it should be -5 temp HP/sec
    - Fixed the speed bonus while you have temp HP only being 10% when it should be 20%

--- lua/sc/tweak_data/upgradestweakdata.lua
+++ lua/sc/tweak_data/upgradestweakdata.lua
@@ -1414,10 +1414,10 @@ Hooks:PostHook(UpgradesTweakData, "_init_pd2_values", "ResSkillsInit", function(
 		{7.5, 2.5},
 		{12, 4}
 	}
-	self.temp_health_decay = 0.6
+	self.temp_health_decay = 0.5
 	self.temp_health_max = 24
 	self.values.player.revive_temp_health = { 12 }
-	self.values.player.temp_health_speed = { 1.1 }
+	self.values.player.temp_health_speed = { 1.2 }
 	self.values.player.temp_health_deflection = { 0.1 }
 	self.values.player.armor_regen_dodge = { 1 }
 

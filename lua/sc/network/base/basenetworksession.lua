143952fa7527433d056d0bcf771f979e7564efe4
Noep <everyonewins@live.ca>
2023-02-06 06:00:15 -0500

    Increased network timeout durations

--- lua/sc/network/base/basenetworksession.lua
+++ lua/sc/network/base/basenetworksession.lua
@@ -1,6 +1,6 @@
-BaseNetworkSession.CONNECTION_TIMEOUT = 20 --not sure, 20 by default.
-BaseNetworkSession.LOADING_CONNECTION_TIMEOUT = SystemInfo:platform() == Idstring("WIN32") and 40 --what is this? 40 by default.
-BaseNetworkSession._LOAD_WAIT_TIME = 15 -- this is 10 by default.  suspicion this is our package culprit, so i've increased it slightly to 15s for testing over the next couple of days.  increase it again if results aren't noticed
+BaseNetworkSession.CONNECTION_TIMEOUT = 30 --not sure, 10 by default.
+BaseNetworkSession.LOADING_CONNECTION_TIMEOUT = SystemInfo:platform() == Idstring("WIN32") and 60 or 60 --what is this? 20 by default.
+BaseNetworkSession._LOAD_WAIT_TIME = 15 -- this is 3 by default.  suspicion this is our package culprit, so i've increased it slightly to 15s for testing over the next couple of days.  increase it again if results aren't noticed
 
 Hooks:PostHook(BaseNetworkSession,"on_peer_sync_complete","resmod_send_sync_env_data",function(self,peer,peer_id)
 	if not self._local_peer then

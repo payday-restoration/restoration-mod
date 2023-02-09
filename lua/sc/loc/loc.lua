78ab3860f65039f44644a6d26cf89816e1133188
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2023-02-09 21:53:58 +0100

    Revert "Added Jackal's note to Bomb Forest briefing"
    This reverts commit 7e402650b90581ce6d55692b87dfd0ca1fcb15b8.

--- lua/sc/loc/loc.lua
+++ lua/sc/loc/loc.lua
@@ -918,10 +918,6 @@ Hooks:Add("LocalizationManagerPostInit", "ResMod_english_Localization", function
 		["wp_heat_street_bile"] = "Bile",
 		["wp_heat_street_question"] = "?",
 		
-		--The Bomb: Forest Breifing
-		["heist_crojob3_briefing"] = "Those thermobaric explosives were loaded onto a freight train this morning. It's heading down to Norfolk. Maybe to be sold on, maybe to be decommissioned - it doesn't matter, because it won't reach there. A moving train is tough to stop, so we're doing this Old West style. Blow a section of bridge and the whole thing comes tumbling down. It's loud and messy, but effective. The wagons will be scattered to hell. One of them has a vault where our explosives are - search each of 'em til you find it. \n\nNOTE FROM JACKAL:\nLooks like Akan wants to solve his problems too and bribed the local Sheriff Department. Expect brutal response from Reaper teams.",
-		
-		
 		--Watchdogs Holdout
 		["heist_skm_watchdogs_stage2_briefing"] = "Mia Calienté is a real piece of work, friends. For years she's been the brains behind the Murkywater's operations in DC. Not too long ago, Murkywater took over a warehouse at the docks and they've using it as an additional distribution point. Our target is there checking in on some loot recently plundered from abroard. We're going to get in there, grab the dwankie and take her hostage. The Murkies can't afford to lose her, they will pay for her release, maybe with the aforementioned and rather valuable plunder, what do you say gang?"
 	})

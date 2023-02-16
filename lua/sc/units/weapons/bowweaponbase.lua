8fd7b3727898c6f19f3e69b2542e9b29cb3ccab2
Noep <everyonewins@live.ca>
2023-02-16 01:12:10 -0500

    removed failed shots for bows
    Damage and velocity still scale off the how far along you are on the draw
    - 50% to 100% for damage
    - 25% to 100% for velocity

--- lua/sc/units/weapons/bowweaponbase.lua
+++ lua/sc/units/weapons/bowweaponbase.lua
@@ -1,3 +1,7 @@
+--Kill
+function BowWeaponBase:charge_fail()
+	return false
+end
 
 --Kills adjust_z scaling with charge
 function BowWeaponBase:_adjust_throw_z(m_vec)
@@ -5,12 +9,12 @@ end
 
 --Uncharged shots deal less damage
 function BowWeaponBase:projectile_damage_multiplier()
-	return math.lerp(0.0001, 1, self:charge_multiplier())
+	return math.lerp(0.5, 1, self:charge_multiplier())
 end
 
---Uncharged shots have less velocity (failed shots also drop closer to you)
+--Uncharged shots have less velocity
 function BowWeaponBase:projectile_speed_multiplier()
-	return math.lerp(0.0001, 1, self:charge_multiplier())
+	return math.lerp(0.25, 1, self:charge_multiplier())
 end
 
 --Overkill literally made bows aim at 1/2 speed

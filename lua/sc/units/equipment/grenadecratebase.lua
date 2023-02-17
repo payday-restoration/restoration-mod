a854cf878ec82eebef87f2d18df5372075f48b09
SC <gametwisterdotnet@yahoo.com>
2023-02-17 14:30:10 -0800

    Ordnance Bag
    * Ordnance bag now properly gives you back 1 throwable and fills 25% of your ammo reserves on use. Use amount increased to 4 (from 3).
    * Fixed the Turtle Always Wins caring if you had a Grenade Crate equipped.

--- lua/sc/units/equipment/grenadecratebase.lua
+++ lua/sc/units/equipment/grenadecratebase.lua
@@ -26,9 +26,48 @@ function GrenadeCrateBase:take_grenade(unit)
 	return can_take_grenade
 end
 
---Overkill Grenade Case, setting it to just be a reskin of our grenade case for now
+--Overkill Grenade Case
 function GrenadeCrateDeployableBase:setup()
-	self._max_grenade_amount = 3
+	self._max_grenade_amount = 4
 	
 	GrenadeCrateDeployableBase.super.setup(self)
+end
+
+function GrenadeCrateDeployableBase:take_grenade(unit)
+	if self._empty or not self:_can_take_grenade() or not managers.network:session() then
+		return
+	end
+
+	unit:sound():play("pickup_ammo")
+
+	local grenade_amount = managers.player:add_grenade_amount(1, true)
+	
+	--So you take some ammo back too
+	local inventory = unit:inventory()
+
+	self._ammo_amount = 0.25
+
+	if inventory then
+		for id, weapon in pairs(inventory:available_selections()) do
+			local took = self:round_value(weapon.unit:base():add_ammo_from_bag(self._ammo_amount))
+			managers.hud:set_ammo_amount(id, weapon.unit:base():ammo_info())
+		end
+	end
+	
+	managers.network:session():send_to_peers_synched("sync_unit_event_id_16", self._unit, "base", 1)
+
+	self._grenade_amount = self._grenade_amount - 1
+
+	if self._grenade_amount <= 0 then
+		self:_set_empty()
+	end
+
+	self:_set_visual_stage()
+
+	return grenade_amount
+end
+
+local dec_mul = 10000
+function GrenadeCrateDeployableBase:round_value(val)
+	return math.floor(val * dec_mul) / dec_mul
 end

25613cb29a4dd026e515f2bbb413cf795d7cbbe9
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2023-02-23 08:22:21 +0100

    remove nasty killzones

---
+++ req/mission_script/corp.lua
@@ -0,0 +1,63 @@
+return {
+	-- Don't kill off enemies in various areas
+	[102154] = {
+		values = {
+			enabled = false
+		}
+	},
+	[102155] = {
+		values = {
+			enabled = false
+		}
+	},
+	[102162] = {
+		values = {
+			enabled = false
+		}
+	},
+	[102160] = {
+		values = {
+			enabled = false
+		}
+	},
+	[102170] = {
+		values = {
+			enabled = false
+		}
+	},
+	[102167] = {
+		values = {
+			enabled = false
+		}
+	},
+	[102178] = {
+		values = {
+			enabled = false
+		}
+	},
+	[102177] = {
+		values = {
+			enabled = false
+		}
+	},
+	[102186] = {
+		values = {
+			enabled = false
+		}
+	},
+	[102181] = {
+		values = {
+			enabled = false
+		}
+	},
+	[102191] = {
+		values = {
+			enabled = false
+		}
+	},
+	[102189] = {
+		values = {
+			enabled = false
+		}
+	}
+}

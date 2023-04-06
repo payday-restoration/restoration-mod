40b8e378cf9afb4625c6f24e0d37fc36233dd532
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2023-04-06 09:38:13 +0200

    PONR Stuff (again)

--- lua/sc/managers/dialogmanager.lua
+++ lua/sc/managers/dialogmanager.lua
@@ -77,23 +77,6 @@ function DialogManager:queue_dialog(id, ...)
 			end		
 		end
 	
-		--Four Stores--				
-		if id == "pln_fost_vh_01" then
-			if difficulty_index <= 2 then
-				escape_time = 450 
-			elseif difficulty_index == 3 then
-				escape_time = 420
-			elseif difficulty_index == 4 then
-				escape_time = 390
-			elseif difficulty_index == 5 then
-				escape_time = 360	
-			elseif difficulty_index == 6 or difficulty_index == 7 then
-				escape_time = 330						
-			else
-				escape_time = 300
-			end
-		end	
-	
 		--Brook 10-10 on crackhead mode? This brings new insanity--				
 		if id == "Play_cha_spa_14" then
 			if difficulty_index <= 2 then
@@ -109,58 +92,7 @@ function DialogManager:queue_dialog(id, ...)
 			else
 				escape_time = 300
 			end
-		end	
-	
-		--Brooklyn Bank--				
-		if id == "Play_loc_brb_53" then
-			if difficulty_index <= 2 then
-				escape_time = 690
-			elseif difficulty_index == 3 then
-				escape_time = 660
-			elseif difficulty_index == 4 then
-				escape_time = 630
-			elseif difficulty_index == 5 then
-				escape_time = 600	
-			elseif difficulty_index == 6 or difficulty_index == 7 then
-				escape_time = 570						
-			else
-				escape_time = 540
-			end	
 		end
-	
-		--Jewelery Store & Ukrainian Job--				
-		if id == "Play_pln_ukranian_stage1_28" then
-			if difficulty_index <= 2 then
-				escape_time = 690
-			elseif difficulty_index == 3 then
-				escape_time = 660
-			elseif difficulty_index == 4 then
-				escape_time = 630
-			elseif difficulty_index == 5 then
-				escape_time = 600	
-			elseif difficulty_index == 6 or difficulty_index == 7 then
-				escape_time = 570						
-			else
-				escape_time = 540
-			end	
-		end
-	
-		--Train Heist--				
-		if id == "Play_pln_tr1b_02" then
-			if difficulty_index <= 2 then
-				escape_time = 1050
-			elseif difficulty_index == 3 then
-				escape_time = 1020
-			elseif difficulty_index == 4 then
-				escape_time = 990
-			elseif difficulty_index == 5 then
-				escape_time = 960	
-			elseif difficulty_index == 6 or difficulty_index == 7 then
-				escape_time = 930					
-			else
-				escape_time = 900
-			end		
-		end	
 	
 		--White House--				
 		if id == "Play_loc_vit_137" then
@@ -177,57 +109,6 @@ function DialogManager:queue_dialog(id, ...)
 			else
 				escape_time = 900
 			end		
-		end		
-		
-		--Firestarter Day 1--
-		if id == "pln_fs1_09" then
-			if difficulty_index <= 2 then
-				escape_time = 1200 
-			elseif difficulty_index == 3 then
-				escape_time = 1170
-			elseif difficulty_index == 4 then
-				escape_time = 1140
-			elseif difficulty_index == 5 then
-				escape_time = 1080	
-			elseif difficulty_index == 6 or difficulty_index == 7 then
-				escape_time = 1080						
-			else
-				escape_time = 1050
-			end
-		end	
-		
-		--Firestarter Day 2--
-		if id == "pln_fs2_20" then
-			if difficulty_index <= 2 then
-				escape_time = 450 
-			elseif difficulty_index == 3 then
-				escape_time = 420
-			elseif difficulty_index == 4 then
-				escape_time = 390
-			elseif difficulty_index == 5 then
-				escape_time = 360	
-			elseif difficulty_index == 6 or difficulty_index == 7 then
-				escape_time = 330						
-			else
-				escape_time = 300
-			end
-		end			
-	
-		--Firestarter Day 3--
-		if id == "pln_fs3_07" then
-			if difficulty_index <= 2 then
-				escape_time = 450
-			elseif difficulty_index == 3 then
-				escape_time = 420
-			elseif difficulty_index == 4 then
-				escape_time = 390
-			elseif difficulty_index == 5 then
-				escape_time = 360	
-			elseif difficulty_index == 6 or difficulty_index == 7 then
-				escape_time = 330						
-			else
-				escape_time = 300
-			end
 		end
 	
 		--Golden Grin Casino--		
@@ -261,23 +142,6 @@ function DialogManager:queue_dialog(id, ...)
 				escape_time = 570						
 			else
 				escape_time = 540
-			end
-		end
-	
-		--Big Bank--			
-		if id == "Play_pln_bb1_64" or id == "Play_pln_bb1_38" then
-			if difficulty_index <= 2 then
-				escape_time = 1200 
-			elseif difficulty_index == 3 then
-				escape_time = 1170
-			elseif difficulty_index == 4 then
-				escape_time = 1140
-			elseif difficulty_index == 5 then
-				escape_time = 1080	
-			elseif difficulty_index == 6 or difficulty_index == 7 then
-				escape_time = 1080						
-			else
-				escape_time = 1050
 			end
 		end
 	
@@ -330,40 +194,6 @@ function DialogManager:queue_dialog(id, ...)
 			else
 				escape_time = 540
 			end
-		end
-	
-		--Big Oil Day 1--
-		if id == "pln_bo1_05" then
-			if difficulty_index <= 2 then
-				escape_time = 390
-			elseif difficulty_index == 3 then
-				escape_time = 360
-			elseif difficulty_index == 4 then
-				escape_time = 330
-			elseif difficulty_index == 5 then
-				escape_time = 300	
-			elseif difficulty_index == 6 or difficulty_index == 7 then
-				escape_time = 270						
-			else
-				escape_time = 240
-			end	
-		end	
-	
-		--Big Oil Day 2--
-		if id == "pln_bo2_36" or id == "pln_bo2_40" or id == "pln_bo2_39" then
-			if difficulty_index <= 2 then
-				escape_time = 630
-			elseif difficulty_index == 3 then
-				escape_time = 600
-			elseif difficulty_index == 4 then
-				escape_time = 570
-			elseif difficulty_index == 5 then
-				escape_time = 540	
-			elseif difficulty_index == 6 or difficulty_index == 7 then
-				escape_time = 510					
-			else
-				escape_time = 480
-			end		
 		end			
 
 		--Aftershock--
@@ -381,41 +211,7 @@ function DialogManager:queue_dialog(id, ...)
 			else
 				escape_time = 360
 			end			
-		end		
-	
-		--H&T Bank Heist (all versions)--
-		if id == "pln_branchbank_stage1_08" or id == "pln_branchbank_stage1_13" or id == "pln_branchbank_stage1_44" or id == "pln_branchbank_stage1_29" then
-			if difficulty_index <= 2 then
-				escape_time = 450
-			elseif difficulty_index == 3 then
-				escape_time = 420
-			elseif difficulty_index == 4 then
-				escape_time = 390
-			elseif difficulty_index == 5 then
-				escape_time = 360	
-			elseif difficulty_index == 6 or difficulty_index == 7 then
-				escape_time = 330						
-			else
-				escape_time = 300
-			end
-		end		
-		
-		--Hoxton Revenge--
-		if id == "Play_pln_hb3_42" then
-			if difficulty_index <= 2 then
-				escape_time = 570
-			elseif difficulty_index == 3 then
-				escape_time = 540
-			elseif difficulty_index == 4 then
-				escape_time = 510
-			elseif difficulty_index == 5 then
-				escape_time = 480	
-			elseif difficulty_index == 6 or difficulty_index == 7 then
-				escape_time = 450						
-			else
-				escape_time = 420
-			end			
-		end		
+		end	
 
 		--Counterfeit--
 		--Escaping
--- req/mission_script/arm_for.lua
+++ req/mission_script/arm_for.lua
@@ -1,4 +1,34 @@
+if Global.game_settings and Global.game_settings.one_down and not managers.groupai:state():whisper_mode() then
+ponr_value = 0
+ponr_toggle = false
+
+	if tweak_data:difficulty_to_index(difficulty) <= 2 then
+		ponr_value = 1050
+		ponr_toggle = true
+	elseif tweak_data:difficulty_to_index(difficulty) == 3 then
+		ponr_value = 1020
+		ponr_toggle = true	
+	elseif tweak_data:difficulty_to_index(difficulty) == 4 then
+		ponr_value = 990
+		ponr_toggle = true	
+	elseif tweak_data:difficulty_to_index(difficulty) == 5 then
+		ponr_value = 960
+		ponr_toggle = true	
+	elseif tweak_data:difficulty_to_index(difficulty) == 6 or tweak_data:difficulty_to_index(difficulty) == 7 then
+		ponr_value = 930
+		ponr_toggle = true	
+	elseif tweak_data:difficulty_to_index(difficulty) == 8 then
+		ponr_value = 900
+		ponr_toggle = true		
+	end
+end
+
 return {
+	--Pro Job PONR 
+	[102845] = {
+		ponr = ponr_value,
+		enabled = ponr_toggle
+	},
 	-- restores unused sniper spawn
 	[100370] = {
 		values = {

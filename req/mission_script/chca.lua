d81e2d89bdf9186c5a670eff1043b6968bee2dab
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2023-04-07 10:46:03 +0200

    e

--- req/mission_script/chca.lua
+++ req/mission_script/chca.lua
@@ -4,40 +4,42 @@ ponr_toggle_1 = false
 
 ponr_value_2 = 0
 ponr_toggle_2 = false
-if tweak_data:difficulty_to_index(difficulty) <= 2 then
+	if tweak_data:difficulty_to_index(difficulty) <= 2 then
 		ponr_value_1 = 1050
 		ponr_toggle_1 = true
-		
-		ponr_value_2 = 690
-		ponr_toggle_2 = true
 	elseif tweak_data:difficulty_to_index(difficulty) == 3 then
 		ponr_value_1 = 1020
-		ponr_toggle_1 = true	
-		
-		ponr_value_2 = 660
-		ponr_toggle_2 = true
+		ponr_toggle_1 = true
 	elseif tweak_data:difficulty_to_index(difficulty) == 4 then
 		ponr_value_1 = 990
 		ponr_toggle_1 = true
-		
-		ponr_value_2 = 630
-		ponr_toggle_2 = true
 	elseif tweak_data:difficulty_to_index(difficulty) == 5 then
 		ponr_value_1 = 960
-		ponr_toggle_1 = true	
-		
-		ponr_value_2 = 600
-		ponr_toggle_2 = true
+		ponr_toggle_1 = true
 	elseif tweak_data:difficulty_to_index(difficulty) == 6 or tweak_data:difficulty_to_index(difficulty) == 7 then
 		ponr_value_1 = 930
 		ponr_toggle_1 = true
+	elseif tweak_data:difficulty_to_index(difficulty) == 8 then
+		ponr_value_1 = 900
+		ponr_toggle_1 = true
+	end
 
+if tweak_data:difficulty_to_index(difficulty) <= 2 then
+		ponr_value_2 = 690
+		ponr_toggle_2 = true
+	elseif tweak_data:difficulty_to_index(difficulty) == 3 then
+		ponr_value_2 = 660
+		ponr_toggle_2 = true
+	elseif tweak_data:difficulty_to_index(difficulty) == 4 then
+		ponr_value_2 = 630
+		ponr_toggle_2 = true
+	elseif tweak_data:difficulty_to_index(difficulty) == 5 then
+		ponr_value_2 = 600
+		ponr_toggle_2 = true
+	elseif tweak_data:difficulty_to_index(difficulty) == 6 or tweak_data:difficulty_to_index(difficulty) == 7 then
 		ponr_value_2 = 570
 		ponr_toggle_2 = true
 	elseif tweak_data:difficulty_to_index(difficulty) == 8 then
-		ponr_value_1 = 900
-		ponr_toggle_1 = true	
-
 		ponr_value_2 = 540
 		ponr_toggle_2 = true
 	end

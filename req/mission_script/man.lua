07c6b3643263ef1b156c0509c723f16105443dcb
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2023-04-11 17:52:28 +0200

    Flashlights for Undercover

---
+++ req/mission_script/man.lua
@@ -0,0 +1,13 @@
+return {
+	 --flashlights, flashlights, flashlights!!!!!!!!!!
+	[100756] = {
+		values = {
+			flashlight = true
+		}
+	},
+	[101801] = {
+		values = {
+			enabled = false
+		}
+	}
+}	

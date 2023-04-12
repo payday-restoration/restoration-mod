5fbb4411702558e68275e9f7f0a3bbc202fc38a0
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2023-04-12 10:35:31 +0200

    Update nail.lua

--- req/mission_script/nail.lua
+++ req/mission_script/nail.lua
@@ -2,37 +2,44 @@ return {
 	--titan spawns (reduce possible spam with titandozers)
 	[100344] = {
 		values = {
-            enemy = "units/pd2_mod_halloween/characters/ene_titan_sniper/ene_titan_sniper"
+            enemy = "units/pd2_mod_halloween/characters/ene_titan_sniper/ene_titan_sniper",
+			participate_to_group_ai = true
 		}
 	},
 	[100345] = {
 		values = {
-            enemy = "units/pd2_mod_halloween/characters/ene_veteran_cop_1/ene_veteran_cop_1"
+            enemy = "units/pd2_mod_halloween/characters/ene_veteran_cop_1/ene_veteran_cop_1",
+			participate_to_group_ai = true
 		}
 	},
 	[100346] = {
 		values = {
-            enemy = "units/pd2_mod_halloween/characters/ene_skele_swat_2/ene_skele_swat_2"
+            enemy = "units/pd2_mod_halloween/characters/ene_skele_swat_2/ene_skele_swat_2",
+			participate_to_group_ai = true
 		}
 	},
 	[100347] = {
 		values = {
-            enemy = "units/pd2_mod_halloween/characters/ene_titan_taser/ene_titan_taser"
+            enemy = "units/pd2_mod_halloween/characters/ene_titan_taser/ene_titan_taser",
+			participate_to_group_ai = true
 		}
 	},
 	[100348] = {
 		values = {
-            enemy = "units/pd2_mod_halloween/characters/ene_skele_swat/ene_skele_swat"
+            enemy = "units/pd2_mod_halloween/characters/ene_skele_swat/ene_skele_swat",
+			participate_to_group_ai = true
 		}
 	},
 	[100349] = {
 		values = {
-            enemy = "units/pd2_mod_halloween/characters/ene_skele_swat_2/ene_skele_swat_2"
+            enemy = "units/pd2_mod_halloween/characters/ene_skele_swat_2/ene_skele_swat_2",
+			participate_to_group_ai = true
 		}
 	},
 	[100350] = {
 		values = {
-            enemy = "units/pd2_mod_halloween/characters/ene_skele_swat/ene_skele_swat"
+            enemy = "units/pd2_mod_halloween/characters/ene_skele_swat/ene_skele_swat",
+			participate_to_group_ai = true
 		}
 	}
 }

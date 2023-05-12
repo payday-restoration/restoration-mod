760b40e72edb4aba9dbb1a324a405df885676152
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2023-05-12 12:25:28 +0200

    fixed flashlights not working in Undercover

--- req/mission_script/man.lua
+++ req/mission_script/man.lua
@@ -1,13 +1,9 @@
 return {
 	 --flashlights, flashlights, flashlights!!!!!!!!!!
 	[100756] = {
-		values = {
 			flashlight = true
-		}
 	},
 	[101801] = {
-		values = {
 			flashlight = false
-		}
 	}
 }	

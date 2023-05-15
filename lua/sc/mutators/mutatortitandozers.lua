c3db1819d0c30fdd759cea0f129466520bae892e
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2023-05-15 13:00:10 +0200

    Update mutatortitandozers.lua

--- lua/sc/mutators/mutatortitandozers.lua
+++ lua/sc/mutators/mutatortitandozers.lua
@@ -35,7 +35,6 @@ function MutatorTitandozers:setup()
 	tweak_data.group_ai.unit_categories.BLACK_tank.unit_types.nypd[1] = "units/payday2/characters/ene_bulldozer_2_hw/ene_bulldozer_2_hw"
 	tweak_data.group_ai.unit_categories.BLACK_tank.unit_types.lapd[1] = "units/payday2/characters/ene_bulldozer_2_hw/ene_bulldozer_2_hw"
 	tweak_data.group_ai.unit_categories.BLACK_tank.unit_types.fbi[1] = "units/payday2/characters/ene_bulldozer_2_hw/ene_bulldozer_2_hw"
-	tweak_data.group_ai.unit_categories.BLACK_tank.unit_types.omnia[1] = "units/payday2/characters/ene_bulldozer_2_hw/ene_bulldozer_2_hw"
 	
 	tweak_data.group_ai.unit_categories.TIT_tank.unit_types.america[1] = "units/payday2/characters/ene_bulldozer_4/ene_bulldozer_4"
 	tweak_data.group_ai.unit_categories.TIT_tank.unit_types.russia[1] = "units/payday2/characters/ene_bulldozer_4/ene_bulldozer_4"
@@ -45,7 +44,6 @@ function MutatorTitandozers:setup()
 	tweak_data.group_ai.unit_categories.TIT_tank.unit_types.nypd[1] = "units/payday2/characters/ene_bulldozer_4/ene_bulldozer_4"
 	tweak_data.group_ai.unit_categories.TIT_tank.unit_types.lapd[1] = "units/payday2/characters/ene_bulldozer_4/ene_bulldozer_4"
 	tweak_data.group_ai.unit_categories.TIT_tank.unit_types.fbi[1] = "units/payday2/characters/ene_bulldozer_4/ene_bulldozer_4"
-	tweak_data.group_ai.unit_categories.TIT_tank.unit_types.omnia[1] = "units/payday2/characters/ene_bulldozer_4/ene_bulldozer_4"
 	
 	--Replace Spring, not a pretty way to do it but it works
 	if difficulty_index <= 5 then
@@ -62,7 +60,7 @@ function MutatorTitandozers:setup()
 				}
 			}
 		}	
-	elseif difficulty_index == 6 or difficulty_index == 7 then
+	elseif difficulty_index == 6 then
 		tweak_data.group_ai.enemy_spawn_groups.Cap_Spring = {
 			amount = {3, 3},
 			spawn = {
@@ -83,8 +81,8 @@ function MutatorTitandozers:setup()
 					rank = 2
 				}
 			}
-		}				
-	else
+		}
+	elseif difficulty_index == 7 then
 		tweak_data.group_ai.enemy_spawn_groups.Cap_Spring = {
 			amount = {5, 5},
 			spawn = {
@@ -105,13 +103,43 @@ function MutatorTitandozers:setup()
 					rank = 2
 				},
 				{
+					unit = "HVH_Boss_Spooc_Normal",
+					freq = 1,
+					amount_min = 2,
+					amount_max = 2,
+					tactics = tweak_data.group_ai._tactics.HVH_boss,
+					rank = 2
+				}
+			}
+		}	
+	else
+		tweak_data.group_ai.enemy_spawn_groups.Cap_Spring = {
+			amount = {5, 5},
+			spawn = {
+				{
+					unit = "HVH_Boss",
+					freq = 1,
+					amount_min = 1,
+					amount_max = 1,
+					tactics = tweak_data.group_ai._tactics.HVH_boss,
+					rank = 1
+				},
+				{
+					unit = "HVH_Boss_Headless_White",
+					freq = 1,
+					amount_min = 2,
+					amount_max = 2,
+					tactics = tweak_data.group_ai._tactics.HVH_boss,
+					rank = 2
+				},
+				{
 					unit = "HVH_Boss_Spooc",
 					freq = 1,
 					amount_min = 2,
 					amount_max = 2,
 					tactics = tweak_data.group_ai._tactics.HVH_boss,
 					rank = 2
-				},					
+				}					
 			}
 		}			
 	end	

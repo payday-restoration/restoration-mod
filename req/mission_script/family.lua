2308d5f546f80a55c34c576a464cb6f477c3aa61
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2023-06-08 12:46:05 +0200

    Increased PONR timers in Diamond Store

--- req/mission_script/family.lua
+++ req/mission_script/family.lua
@@ -3,17 +3,17 @@ local difficulty_index = tweak_data:difficulty_to_index(difficulty)
 
 if Global.game_settings and Global.game_settings.one_down then
 	if tweak_data:difficulty_to_index(difficulty) <= 2 then
-		ponr_value = 690
+		ponr_value = 900
 	elseif tweak_data:difficulty_to_index(difficulty) == 3 then
-		ponr_value = 660	
+		ponr_value = 900
 	elseif tweak_data:difficulty_to_index(difficulty) == 4 then
-		ponr_value = 630	
+		ponr_value = 860
 	elseif tweak_data:difficulty_to_index(difficulty) == 5 then
-		ponr_value = 600	
+		ponr_value = 860
 	elseif tweak_data:difficulty_to_index(difficulty) == 6 or tweak_data:difficulty_to_index(difficulty) == 7 then
-		ponr_value = 570	
+		ponr_value = 830
 	elseif tweak_data:difficulty_to_index(difficulty) == 8 then
-		ponr_value = 540		
+		ponr_value = 780	
 	end
 end
 

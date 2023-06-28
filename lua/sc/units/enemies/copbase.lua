5d663660c67fa88365562f7dcc246d2ebb2122b5
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2023-06-28 16:32:03 +0200

    Weapons For Bellmead Guards

--- lua/sc/units/enemies/copbase.lua
+++ lua/sc/units/enemies/copbase.lua
@@ -476,6 +476,10 @@ local weapons_map = {
 	[Idstring("units/payday2/characters/ene_hoxton_breakout_responder_1/ene_hoxton_breakout_responder_1"):key()] = {"ump", "r870", "m416_npc"},
 	[Idstring("units/payday2/characters/ene_hoxton_breakout_responder_2/ene_hoxton_breakout_responder_2"):key()] = {"ump", "r870", "m416_npc"},
 	
+	[Idstring("units/pd2_dlc_deep/characters/ene_deep_security_1/ene_deep_security_1"):key()] = {"m1911_npc", "mp5", "m4"},
+	[Idstring("units/pd2_dlc_deep/characters/ene_deep_security_2/ene_deep_security_2"):key()] = {"m1911_npc", "mp5", "m4"},
+	[Idstring("units/pd2_dlc_deep/characters/ene_deep_security_3/ene_deep_security_3"):key()] = {"m1911_npc", "r870", "m4"},
+	
 	--Vanilla Murkies with varierty weapons
 	[Idstring("units/payday2/characters/ene_murkywater_1/ene_murkywater_1"):key()] = {"ump", "m4", "r870", "scar_murky"},
 	[Idstring("units/payday2/characters/ene_murkywater_2/ene_murkywater_2"):key()] = {"ump", "m4", "r870", "scar_murky"},

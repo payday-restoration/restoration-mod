01583aaa89eac73b326571068010d2ce0ba055d5
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2023-07-24 18:54:49 +0200

    Update ranc.lua

--- req/mission_script/ranc.lua
+++ req/mission_script/ranc.lua
@@ -5,31 +5,40 @@ if Global.game_settings and Global.game_settings.one_down then
 	if tweak_data:difficulty_to_index(difficulty) <= 2 then
 		ponr_value_1 = 1650
 		ponr_value_2 = 300
+		ponr_value_3 = 900
 	elseif tweak_data:difficulty_to_index(difficulty) == 3 then
 		ponr_value_1 = 1620
-		ponr_value_2 = 270	
+		ponr_value_2 = 270
+		ponr_value_3 = 870
 	elseif tweak_data:difficulty_to_index(difficulty) == 4 then
 		ponr_value_1 = 1590
 		ponr_value_2 = 240	
+		ponr_value_3 = 840
 	elseif tweak_data:difficulty_to_index(difficulty) == 5 then
 		ponr_value_1 = 1560
 		ponr_value_2 = 210	
+		ponr_value_3 = 840
 	elseif tweak_data:difficulty_to_index(difficulty) == 6 or tweak_data:difficulty_to_index(difficulty) == 7 then
 		ponr_value_1 = 1530
-		ponr_value_2 = 210	
+		ponr_value_2 = 210
+		ponr_value_3 = 810
 	elseif tweak_data:difficulty_to_index(difficulty) == 8 then
 		ponr_value_1 = 1500
-		ponr_value_2 = 180		
+		ponr_value_2 = 180
+		ponr_value_3 = 780
 	end
 end
 
 return {
 	--Pro Job PONR
-	[103507] = {
+	[101425] = {
 		ponr = ponr_value_1
 	},
 	[100622] = {
 		ponr = ponr_value_2
+	},
+	[100929] = {
+		ponr = ponr_value_3
 	},
 	--fixes some spawn typos
 	[100683] = {

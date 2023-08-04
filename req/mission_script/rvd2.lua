d861f25e9291401295bd03c8f1688c1216660c00
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2023-08-04 09:35:53 +0200

    Rats Stuff

--- req/mission_script/rvd2.lua
+++ req/mission_script/rvd2.lua
@@ -61,36 +61,5 @@ return {
 		values = {
             enemy = "units/pd2_dlc_gitgud/characters/ene_bulldozer_minigun/ene_bulldozer_minigun"
 		}
-	},
-	--ambush vault (DS only)
-	[101400] = { 
-		values = {
-            enemy = "units/pd2_dlc_gitgud/characters/ene_zeal_city_3/ene_zeal_city_3"
-		}
-	},
-	[101401] = { 
-		values = {
-            enemy = "units/pd2_dlc_gitgud/characters/ene_zeal_swat_heavy_r870_sc/ene_zeal_swat_heavy_r870_sc"
-		}
-	},
-	[101402] = { 
-		values = {
-            enemy = "units/pd2_dlc_vip/characters/ene_phalanx_1_assault/ene_phalanx_1_assault"
-		}
-	},
-	[101403] = { 
-		values = {
-            enemy = "units/pd2_dlc_vip/characters/ene_titan_taser/ene_titan_taser"
-		}
-	},
-	[101404] = { 
-		values = {
-            enemy = "units/pd2_dlc_vip/characters/ene_spook_cloak_1/ene_spook_cloak_1" --:)
-		}
-	},
-	[101405] = { 
-		values = {
-            enemy = "units/pd2_dlc_gitgud/characters/ene_bulldozer_minigun/ene_bulldozer_minigun"
-		}
 	}
 }

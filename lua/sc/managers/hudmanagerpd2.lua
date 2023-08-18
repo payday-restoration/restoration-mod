d3164d7f950ab8e2dcbbe316f90810da069a847f
SC <gametwisterdotnet@yahoo.com>
2023-08-17 20:05:07 -0700

    Updated Balloon Mutator
    * Updated the Payday Balloon Popper minigame to work with the hud updates. ISSUE I COULDN'T FIGURE OUT: Buffs present on the hud won't tick down, no idea why managers.hud:update_mutator_hud(t, dt) isn't working.
    * Removed unnecessary code.

--- lua/sc/managers/hudmanagerpd2.lua
+++ lua/sc/managers/hudmanagerpd2.lua
@@ -37,27 +37,3 @@ function HUDManager:on_effective_hit_confirmed(damage_scale)
 
 	self._hud_hit_confirm:on_effective_hit_confirmed(damage_scale)
 end
-
-function HUDManager:_create_mutator(hud)
-	if not _G.IS_VR then
-		hud = hud or managers.hud:script(PlayerBase.PLAYER_INFO_HUD_PD2)
-		self._hud_mutator = HUDMutator:new(hud)
-	end
-end
-
-function HUDManager:add_buff(data)
-	if not _G.IS_VR then
-		self._hud_mutator:add_buff(data)
-	end
-end
-
-function HUDManager:update_mutator_hud(t, dt)
-	if not _G.IS_VR then
-		self._hud_mutator:update(t, dt)
-	end
-end
-
-Hooks:PostHook(HUDManager, "_setup_player_info_hud_pd2", "res_setup_player_info_hud_pd2", function(self)
-	local hud = managers.hud:script(PlayerBase.PLAYER_INFO_HUD_PD2)
-	self:_create_mutator(hud)
-end)

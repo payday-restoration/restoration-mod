26d0d31e7485893ba19089d6530bdedc128caee8
SC <gametwisterdotnet@yahoo.com>
2023-08-19 15:22:08 -0700

    Transporter Event Fix
    * Fixed the Event overriding the innate bonus of Transporter.

--- lua/sc/mutators/mutatorpiggyrevenge.lua
+++ lua/sc/mutators/mutatorpiggyrevenge.lua
@@ -2,3 +2,8 @@
 function MutatorPiggyRevenge:get_interaction_override()
 	return self:is_buff_active("bag_speed") and (self._tweakdata.buffs.bag_speed.interaction_override or 0.1) or 0.25
 end
+
+--Fix it overriding transporter
+function MutatorPiggyRevenge:get_bag_throw_multiplier(carry_id)
+	return self:is_buff_active("bag_throw") and carry_id == "pda9_feed" and (self._tweakdata.buffs.bag_throw.throw_multiplier or 2) or 1.5
+end

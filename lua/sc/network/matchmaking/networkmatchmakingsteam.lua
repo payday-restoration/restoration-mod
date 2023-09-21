43df7b5c927d436e64acc112bf5c8bf99ad84973
Hater-mater <swetlaniki@gmail.com>
2023-09-21 18:54:46 +0300

    Update MM keys

--- lua/sc/network/matchmaking/networkmatchmakingsteam.lua
+++ lua/sc/network/matchmaking/networkmatchmakingsteam.lua
@@ -1,4 +1,4 @@
-NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = "restoration_12.2.1"
+NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = "restoration_12.2.2"
 --Compatibility for BeardLib Editor just in case
 if BLE and BLE:RunningFix() then
 NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY .. "_editor"

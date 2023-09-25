1b230b5ece8cc0734bbf010d806a6538fee6d623
SC <gametwisterdotnet@yahoo.com>
2023-09-24 20:38:44 -0700

    Updated Version Numbers

--- lua/sc/network/base/networkmanager.lua
+++ lua/sc/network/base/networkmanager.lua
@@ -9,6 +9,6 @@ NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY = "restoration_12.2.2"
 NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = "restoration_12.2.2"
 --Compatibility for BeardLib Editor just in case. Maybe need shift MM key stuff in networkmanager
 if BLE and BLE:RunningFix() then
-NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY  = NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY .. "_editor"
-NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY .. "_editor"
+	NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY  = NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY .. "_editor"
+	NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY .. "_editor"
 end

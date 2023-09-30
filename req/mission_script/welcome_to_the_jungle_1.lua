c3cfec49dc852d7e2e632b3d03df9248c0d8aca3
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2023-09-30 17:29:32 +0200

    Fixed Big Oil Day 1 PONR not triggering

--- req/mission_script/welcome_to_the_jungle_1.lua
+++ req/mission_script/welcome_to_the_jungle_1.lua
@@ -1,7 +1,7 @@
 local difficulty = Global.game_settings and Global.game_settings.difficulty or "normal"
 local difficulty_index = tweak_data:difficulty_to_index(difficulty)
 
-	elseif tweak_data:difficulty_to_index(difficulty) <= 5 then
+	if tweak_data:difficulty_to_index(difficulty) <= 5 then
 		ponr_value = 240	
 	elseif tweak_data:difficulty_to_index(difficulty) == 6 or tweak_data:difficulty_to_index(difficulty) == 7 then
 		ponr_value = 210

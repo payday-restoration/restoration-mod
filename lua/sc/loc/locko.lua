6d65aa19d12a6918d9ab090f04d895ba92d93ad3
Gullwing-door <58391080+Gullwing-door@users.noreply.github.com>
2023-10-05 23:17:41 +0900

    Update locko.lua

--- lua/sc/loc/locko.lua
+++ lua/sc/loc/locko.lua
@@ -3043,7 +3043,7 @@ Hooks:Add("LocalizationManagerPostInit", "SC_Localization_Weapons", function(loc
 				["bm_w_scarl"] = "VF-6M",
 				["bm_w_schakal"] = "AMP 45",		
 				["bm_w_tecci"] = "SG 416C",
-				["bm_w_l85a2"] = "ROS L85",
+				["bm_w_l85a2"] = "L-95",
 				["bm_w_x_judge"] = "아킴보 Judges",
 				["bm_w_vhs"] = "HVH-2",
 				["bm_w_contraband"] = "SG 417D",				

ae296fbd2560e9e6afbfa9be1beb1d5e24e79637
Hater-mater <swetlaniki@gmail.com>
2023-10-14 14:07:51 +0300

    Update escape_park.lua

--- req/mission_script/escape_park.lua
+++ req/mission_script/escape_park.lua
@@ -4,5 +4,16 @@ return {
 		values = {
             spawn_action = "e_sp_down_16m_right"
 		}
+	},
+	-- fixed some sniping swats not spawning
+	[102486] = {
+		on_executed = {
+            {id = 100677, delay = 0}
+		}
+	},
+	[102457] = {
+		on_executed = {
+           {id = 100677, delay = 0}
+		}
 	}
 }

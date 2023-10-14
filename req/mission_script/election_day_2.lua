f7c5243628943a4e7b10287bd43f6d2c631b0980
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2023-10-14 19:53:44 +0200

    Harasser Tweak Spawns for Election Day (Day 2)

--- req/mission_script/election_day_2.lua
+++ req/mission_script/election_day_2.lua
@@ -1,5 +1,8 @@
 local difficulty = Global.game_settings and Global.game_settings.difficulty or "normal"
 local difficulty_index = tweak_data:difficulty_to_index(difficulty)
+local swat_normal = "units/payday2/characters/ene_swat_1_sc/ene_swat_1_sc"
+local swat_hard = "units/payday2/characters/ene_swat_heavy_1_sc/ene_swat_heavy_1_sc"
+local swat_overkill = "units/payday2/characters/ene_fbi_heavy_1_sc/ene_fbi_heavy_1_sc"
 
 	if difficulty_index <= 5 then
 		ponr_value = 360	
@@ -8,10 +11,137 @@ local difficulty_index = tweak_data:difficulty_to_index(difficulty)
 	elseif difficulty_index == 8 then
 		ponr_value = 300		
 	end
+	
+if Global.game_settings and Global.game_settings.one_down then
+	swat_normal = "units/payday2/characters/ene_swat_heavy_1_sc/ene_swat_heavy_1_sc"
+	swat_hard = "units/payday2/characters/ene_fbi_heavy_1_sc/ene_fbi_heavy_1_sc"
+	swat_overkill = "units/pd2_dlc_vip/characters/ene_titan_sniper/ene_titan_sniper"
+end
 
 return {
 	--Pro Job PONR 
 	[100931] = {
 		ponr = ponr_value
+	},
+	--Harasser Tweak Spawns
+	[102732] = {
+		values = {
+            enemy = swat_normal
+		}
+	},
+	[102733] = {
+		values = {
+            enemy = swat_hard
+		}
+	},
+	[102742] = {
+		values = {
+            enemy = swat_overkill
+		}
+	},
+	[102428] = {
+		values = {
+            enemy = swat_normal
+		}
+	},
+	[102429] = {
+		values = {
+            enemy = swat_hard
+		}
+	},
+	[102430] = {
+		values = {
+            enemy = swat_overkill
+		}
+	},
+	[102444] = {
+		values = {
+            enemy = swat_normal
+		}
+	},
+	[102445] = {
+		values = {
+            enemy = swat_hard
+		}
+	},
+	[102446] = {
+		values = {
+            enemy = swat_overkill
+		}
+	},
+	[102460] = {
+		values = {
+            enemy = swat_normal
+		}
+	},
+	[102461] = {
+		values = {
+            enemy = swat_hard
+		}
+	},
+	[102462] = {
+		values = {
+            enemy = swat_overkill
+		}
+	},
+	[102762] = {
+		values = {
+            enemy = swat_normal
+		}
+	},
+	[102763] = {
+		values = {
+            enemy = swat_hard
+		}
+	},
+	[102764] = {
+		values = {
+            enemy = swat_overkill
+		}
+	},
+	[102778] = {
+		values = {
+            enemy = swat_normal
+		}
+	},
+	[102779] = {
+		values = {
+            enemy = swat_hard
+		}
+	},
+	[102780] = {
+		values = {
+            enemy = swat_overkill
+		}
+	},
+	[102794] = {
+		values = {
+            enemy = swat_normal
+		}
+	},
+	[102795] = {
+		values = {
+            enemy = swat_hard
+		}
+	},
+	[102796] = {
+		values = {
+            enemy = swat_overkill
+		}
+	},
+	[102810] = {
+		values = {
+            enemy = swat_normal
+		}
+	},
+	[102811] = {
+		values = {
+            enemy = swat_hard
+		}
+	},
+	[102812] = {
+		values = {
+            enemy = swat_overkill
+		}
 	}
 }

a997850f72c317abff66d03e7547942f01c80ace
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2023-10-20 21:56:28 +0200

    Change Not Hard Enough's desc to be accurate

--- lua/sc/loc/loczh.lua
+++ lua/sc/loc/loczh.lua
@@ -1108,7 +1108,11 @@ Hooks:Add("LocalizationManagerPostInit", "ResMod_english_Localization", function
         ["menu_alex_1_zipline_desc"] = "用于快速运包的滑索",
 
         -- Watchdogs Holdout
-        ["heist_skm_watchdogs_stage2_briefing"] = "劫持Mia Calienté是个真正的活，朋友。她已经在幕后主使黑水在特区的行动好几年了。就在不久之前，黑水接管了码头的一个仓库作为额外的分配点。我们的目标正在那里清点从船上刚掠夺不久的货。我们将突入这里，抓住这小崽子作为人质。黑水的人无法接受失去她，他们将会愿意去花钱赎人，没准在上述条件下我们还能要价更高，伙计们你们觉得呢？"
+        ["heist_skm_watchdogs_stage2_briefing"] = "劫持Mia Calienté是个真正的活，朋友。她已经在幕后主使黑水在特区的行动好几年了。就在不久之前，黑水接管了码头的一个仓库作为额外的分配点。我们的目标正在那里清点从船上刚掠夺不久的货。我们将突入这里，抓住这小崽子作为人质。黑水的人无法接受失去她，他们将会愿意去花钱赎人，没准在上述条件下我们还能要价更高，伙计们你们觉得呢？",
+		
+		--Side Jobs
+		["menu_challenge_menu_challenge_moon_6_obj"] = "Complete the Slaughterhouse job on the OVERKILL difficulty or above while Captain Spring is still present on the level."
+		
     })
 
     local difficulty = Global.game_settings and Global.game_settings.difficulty or "normal"

6d836fcc631816d235e47b66c37ed548f60376a3
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2023-10-20 22:20:16 +0200

    Improve Crime.Net's videos

--- lua/sc/tweak_data/narrativetweakdata.lua
+++ lua/sc/tweak_data/narrativetweakdata.lua
@@ -337,7 +337,8 @@ Hooks:PostHook( NarrativeTweakData, "init", "SC_Narratives", function(self)
 		700000
 	}
 	
-	--Dentist Crime.net videos tweaks
+	--Improve Crime.net videos on some heists
+	--Dentist's jobs
 	self.jobs.mia.crimenet_videos = {
 	        "cn_hlm1",
 			"cn_hlm2",
@@ -365,6 +366,31 @@ Hooks:PostHook( NarrativeTweakData, "init", "SC_Narratives", function(self)
 			"cn_big2",
 			"cn_big3"
 	}
+	--Vlad's jobs
+	--for some reason these 2 Vlad's last heists have a crimenet_video from codex instead of regular ones
+	self.jobs.fex.crimenet_videos = {
+	        "cn_ukr1",
+			"cn_ukr2",
+			"cn_ukr3",
+			"cn_nightc1",
+			"cn_nightc2",
+			"cn_nightc3"
+	}
+	self.jobs.chca.crimenet_videos = {
+	        "cn_ukr1",
+			"cn_ukr2",
+			"cn_ukr3",
+			"cn_nightc1",
+			"cn_nightc2",
+			"cn_nightc3"
+	}
+	
+	--Halloween heists gets Stonecold's (PDTH Alpha) menu movie
+	self.jobs.haunted.crimenet_videos = {"menu"}
+	self.jobs.nail.crimenet_videos = {"menu"}
+	self.jobs.help.crimenet_videos = {"menu"}
+	self.jobs.hvh.crimenet_videos = {"menu"}
+	
 	--Ukrainian Prisoner now use Jiu Feng video instead of Vlad
 	self.jobs.sand.crimenet_videos = {"codex/jiufeng1"}
 end)

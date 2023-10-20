4d0719d808714d6248ceaf61593a9cc4f91b2173
SonicSoapyBoi <tomasz.tylkowski3@wp.pl>
2023-10-20 15:56:33 +0200

    Revert "Update elementspecialobjective.lua"
    This reverts commit c9934616692cd8136016f19116f6985d9ccd04a5.

--- lua/sc/managers/mission/elementspecialobjective.lua
+++ lua/sc/managers/mission/elementspecialobjective.lua
@@ -11,13 +11,6 @@ Hooks:PostHook(ElementSpecialObjective, "clbk_objective_failed", "res_clbk_objec
 	end
 end)
 
--- Keep position saved for AI SOs to fix some older map scripting
-Hooks:PreHook(ElementSpecialObjective, "_finalize_values", "sh__finalize_values", function (self, values)
-	if self:value("so_action"):begins("AI") and values.path_style == "destination" then
-		self._AI_SO_pos = values.position
-	end
-end)
-
 -- Keep hunt and search as actual objective types instead of making it defend_area
 -- This is done to be able to differentiate between those objectives and make hunt work properly (search is currently unused)
 local get_objective_original = ElementSpecialObjective.get_objective
@@ -28,11 +21,6 @@ function ElementSpecialObjective:get_objective(...)
 		local objective_type = self._values.so_action:sub(4)
 		if objective_type == "hunt" or objective_type == "search" then
 			objective.type = objective_type
-		end
-
-		if not objective.nav_seg and self._AI_SO_pos then
-			objective.nav_seg = managers.navigation:get_nav_seg_from_pos(self._AI_SO_pos)
-			objective.area = managers.groupai:state():get_area_from_nav_seg_id(objective.nav_seg)
 		end
 	end
 

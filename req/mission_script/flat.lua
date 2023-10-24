f0c7eea80764d38ddc189d57cc246087edb65c7c
Hater-mater <swetlaniki@gmail.com>
2023-10-24 14:39:23 +0300

    Disable(?) unreachable giant tooth brush spawn
    Maybe

--- req/mission_script/flat.lua
+++ req/mission_script/flat.lua
@@ -1,4 +1,10 @@
 return {
+	-- Disable giant tooth brush spawn behind closed door on 5th floor
+	[104746] = {
+		values = {
+			enabled = false
+		}
+	},
 	-- Disable roof/stairs reinforcement
 	[102501] = {
 		values = {

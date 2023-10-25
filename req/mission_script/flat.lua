38ecbf07307a64e08234fbbcadc5c97c658aa5e6
Hater-mater <swetlaniki@gmail.com>
2023-10-25 12:42:23 +0300

    Missions changes
    Panic Room:
    * Unreachable giant tooth brush position is tweaked
    * Removed vanilla ladders that became cosmetic in resmod edit
    
    Dragon Heist and Goat Sim D1:
    * Removed stuff that should show up only in editor mode
    
    Shacklethorne Auction:
    * Ziplines speed is increased

--- req/mission_script/flat.lua
+++ req/mission_script/flat.lua
@@ -1,10 +1,4 @@
 return {
-	-- Disable giant tooth brush spawn behind closed door on 5th floor
-	[104746] = {
-		values = {
-			enabled = false
-		}
-	},
 	-- Disable roof/stairs reinforcement
 	[102501] = {
 		values = {

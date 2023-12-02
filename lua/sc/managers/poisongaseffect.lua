8dc62d8bce68b0508864fd2e7bac45fc7c8c5604
Noep <everyonewins@live.ca>
2023-12-02 01:25:01 -0500

    Update poisongaseffect.lua

--- lua/sc/managers/poisongaseffect.lua
+++ lua/sc/managers/poisongaseffect.lua
@@ -32,12 +32,15 @@ function PoisonGasEffect:init(position, normal, projectile_tweak, grenade_unit)
 	})
 end
 
+--Add sound kill
 function PoisonGasEffect:update(t, dt)
 	if self._timer then
+
 		self._timer = self._timer - dt
 
 		if not self._started_fading and self._timer <= self._fade_time then
 			World:effect_manager():fade_kill(self._effect)
+
 			--We actually need this
 			if not self._sound_killed then
 				self._sound_source:post_event("lung_loop_end")
@@ -54,8 +57,8 @@ function PoisonGasEffect:update(t, dt)
 		if self._timer <= 0 then
 			self._timer = nil
 
-			if alive(self._grenade_unit) and Network:is_server() then
-				managers.enemy:add_delayed_clbk("PoisonGasEffect", callback(PoisonGasEffect, PoisonGasEffect, "remove_grenade_unit"), TimerManager:game():time() + self._dot_data.dot_length)
+			if alive(self._grenade_unit) and (Network:is_server() or self._grenade_unit:id() == -1) then
+				managers.enemy:add_delayed_clbk("PoisonGasEffect" .. tostring(self._grenade_unit:key()), callback(PoisonGasEffect, PoisonGasEffect, "remove_grenade_unit"), TimerManager:game():time() + self._dot_data.dot_length + 1)
 			end
 		end
 
@@ -67,30 +70,22 @@ function PoisonGasEffect:update(t, dt)
 				local nearby_units = World:find_units_quick("sphere", self._position, self._range, managers.slot:get_mask("enemies"))
 
 				for _, unit in ipairs(nearby_units) do
-					if not table.contains(self._unit_list, unit) then
-						local hurt_animation = not self._dot_data.hurt_animation_chance or math.rand(1) < self._dot_data.hurt_animation_chance
+					if not self._unit_list[unit:key()] then
+						self._unit_list[unit:key()] = true
+						local data = {
+							unit = unit,
+							dot_data = self._dot_data,
+							hurt_animation = not self._dot_data.hurt_animation_chance or math.rand(1) < self._dot_data.hurt_animation_chance,
+							weapon_id = self._grenade_id,
+							weapon_unit = alive(self._grenade_unit) and self._grenade_unit or nil,
+							attacker_unit = alive(self._user_unit) and self._user_unit or nil
+						}
 
-						managers.dot:add_doted_enemy(unit, TimerManager:game():time(), self._grenade_unit, self._dot_data.dot_length, self._dot_data.dot_damage, hurt_animation, "poison", self._grenade_id, true)
-						table.insert(self._unit_list, unit)
+						managers.dot:add_doted_enemy(data)
 					end
 				end
 			end
-		else
-			self._damage_tick_timer = self._damage_tick_timer - dt
-
-			if self._damage_tick_timer <= 0 then
-				self._damage_tick_timer = self._tweak_data.poison_gas_tick_time or 0.1
-				local nearby_units = World:find_units_quick("sphere", self._position, self._range, managers.slot:get_mask("enemies"))
-
-				for _, unit in ipairs(nearby_units) do
-					if not table.contains(self._unit_list, unit) then
-						local hurt_animation = not self._dot_data.hurt_animation_chance or math.rand(1) < self._dot_data.hurt_animation_chance
-
-						managers.dot:add_doted_enemy(unit, TimerManager:game():time(), self._grenade_unit, self._dot_data.dot_length, self._dot_data.dot_damage, hurt_animation, "poison", self._grenade_id, true)
-						table.insert(self._unit_list, unit)
-					end
-				end
-			end		
 		end
+
 	end
 end

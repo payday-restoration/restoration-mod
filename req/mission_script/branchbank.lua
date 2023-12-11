afba9b57d9515b65c38eaa38660b748a1b0755f4
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2023-12-11 15:40:31 +0100

    SH Stuff for Bank/Diamond Hiest

--- req/mission_script/branchbank.lua
+++ req/mission_script/branchbank.lua
@@ -72,6 +72,50 @@ return {
 		ponr_player_mul = ponr_timer_player_mul,
 		ponr = ponr_value
 	},
+	-- Special ambush chance increase
+	[103072] = {
+		values = {
+			chance = 75
+		}
+	},
+	[105563] = {
+		values = {
+			player_1 = true
+		}
+	},
+	[105574] = {
+		values = {
+			player_1 = true
+		}
+	},
+	-- Enable spawns sooner
+	[103882] = {
+		on_executed = {
+			{ id = 100251, delay = 30 },
+			{ id = 105774, delay = 20 }
+		}
+	},
+	-- Enable all street reinforce spots when first responders arrive
+	[104727] = {
+		on_executed = {
+			{ id = 100364, delay = 0 }
+		}
+	},
+	[104728] = {
+		on_executed = {
+			{ id = 100364, delay = 0 }
+		}
+	},
+	[104729] = {
+		on_executed = {
+			{ id = 100364, delay = 0 }
+		}
+	},
+	[104730] = {
+		on_executed = {
+			{ id = 100364, delay = 0 }
+		}
+	},
 	--Don't repeat the same dialog
 	[105362] = {
 		values = {

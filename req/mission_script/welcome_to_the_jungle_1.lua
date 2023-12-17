2c1d9ff83cc9f389ec687656375f85a801da8c8a
Hater-mater <swetlaniki@gmail.com>
2023-12-17 14:33:57 +0300

    Update welcome_to_the_jungle_1.lua

--- req/mission_script/welcome_to_the_jungle_1.lua
+++ req/mission_script/welcome_to_the_jungle_1.lua
@@ -2,11 +2,11 @@ local difficulty = Global.game_settings and Global.game_settings.difficulty or "
 local difficulty_index = tweak_data:difficulty_to_index(difficulty)
 
 	if difficulty_index <= 5 then
-		ponr_value = 440
+		ponr_value = 560
 	elseif difficulty_index == 6 or difficulty_index == 7 then
-		ponr_value = 400
+		ponr_value = 520
 	else
-		ponr_value = 360		
+		ponr_value = 480		
 	end
 
 return {

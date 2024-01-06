d4e3519ccd4b36cd4c7f603ee576509947e323d5
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2024-01-06 12:33:56 +0100

    Update charactertweakdata.lua

--- lua/sc/tweak_data/charactertweakdata.lua
+++ lua/sc/tweak_data/charactertweakdata.lua
@@ -917,7 +917,7 @@ function CharacterTweakData:_init_heavy_swat(presets)
 	self.heavy_swat_sniper.static_dodge_preset = true
 	self.heavy_swat_sniper.static_melee_preset = true	
 	self.heavy_swat_sniper.custom_voicework = nil
-	self.heavy_swat_sniper.spawn_sound_event = "cloaker_spawn"
+	self.heavy_swat_sniper.spawn_sound_event_2 = "cloaker_spawn"
 	if self:get_ai_group_type() == "russia" or self:get_ai_group_type() == "federales" then
 		self.heavy_swat_sniper.custom_voicework = "tswat_ru"
 	else

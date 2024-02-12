8f43bf601c93cad7113ebe234be313d30c9cf00e
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2024-02-12 15:24:02 +0100

    Update Core.lua

--- Core.lua
+++ Core.lua
@@ -797,6 +797,8 @@ function restoration:send_sync_environment(to)
 	end
 end
 
+	restoration.loaded_elements = false
+
 --Stealing this from SH cause it's way better
 function restoration:require(file)
 	local path = ModPath .. "req/" .. file .. ".lua"
@@ -812,3 +814,168 @@ function restoration:mission_script_patches()
 	end
 	return self._mission_script_patches
 end
+
+
+--mission_script_add allows to add actual custom stuff to heists
+function restoration:mission_script_add()
+		restoration.loaded_elements = false
+		if self._mission_script_add == nil then
+			local level_id = Global.game_settings and Global.game_settings.level_id
+			if level_id then
+				self._mission_script_add = self:require("mission_script_add/" .. level_id:gsub('_skip1$', ''):gsub('_skip2$', ''):gsub("_night$", ""):gsub("_day$", "")) or false
+			end
+		end
+		return self._mission_script_add
+	end
+
+	function restoration:gen_dummy(id, name, pos, rot, opts)
+		opts = opts or {}
+		return {
+			id = id,
+			editor_name = name,
+			class = "ElementSpawnEnemyDummy",
+			values = {
+				execute_on_startup = opts.execute_on_startup or false,
+				participate_to_group_ai = opts.participate_to_group_ai or false,
+				position = pos,
+				force_pickup = opts.force_pickup or "none",
+				voice = opts.voice or 0,
+				enemy = opts.enemy or "units/payday2/characters/ene_swat_1/ene_swat_1",
+				trigger_times = opts.trigger_times or 0,
+				spawn_action = opts.spawn_action or "none",
+				accessibility = opts.accessibility or "any",
+				on_executed = opts.on_executed or {},
+				rotation = rot,
+				team = opts.team or "default",
+				base_delay = opts.base_delay or 0,
+				enabled = opts.enabled or false,
+				amount = opts.amount or 0,
+				interval = opts.interval or 5,
+			},
+		}
+	end
+
+	function restoration:gen_spawngroup(id, name, elements, interval)
+		return {
+			id = id,
+			editor_name = name,
+			class = "ElementSpawnEnemyGroup",
+			values = {
+				on_executed = {},
+				trigger_times = 0,
+				base_delay = 0,
+				ignore_disabled = false,
+				amount = 0,
+				spawn_type = "ordered",
+				team = "default",
+				execute_on_startup = false,
+				enabled = true,
+				preferred_spawn_groups = {
+					"tac_shield_wall_charge",
+					"FBI_spoocs",
+					"tac_tazer_charge",
+					"tac_tazer_flanking",
+					"tac_shield_wall",
+					"tac_swat_rifle_flank",
+					"tac_shield_wall_ranged",
+					"tac_bull_rush",
+				},
+				elements = elements,
+				interval = interval or 0,
+			},
+		}
+	end
+
+	function restoration:gen_so(id, name, pos, rot, opts)
+		opts = opts or {}
+		return {
+			id = id,
+			editor_name = name,
+			class = "ElementSpecialObjective",
+			values = {
+				path_style = opts.path_style or "destination",
+				align_position = opts.align_position or false,
+				ai_group = "enemies",
+				is_navigation_link = false,
+				position = pos,
+				scan = opts.scan or false,
+				needs_pos_rsrv = opts.needs_pos_rsrv or false,
+				enabled = true,
+				execute_on_startup = false,
+				rotation = rot,
+				base_delay = 0,
+				action_duration_min = 0,
+				search_position = pos,
+				use_instigator = true,
+				trigger_times = 0,
+				trigger_on = "none",
+				search_distance = 0,
+				so_action = opts.so_action or "none",
+				path_stance = opts.path_stance or "hos",
+				path_haste = "run",
+				repeatable = false,
+				attitude = "engage",
+				interval = opts.interval or -1,
+				action_duration_max = 0,
+				align_rotation = opts.align_rotation or false,
+				pose = opts.pose or "none",
+				forced = true,
+				base_chance = 1,
+				interaction_voice = "none",
+				SO_access = opts.SO_access or "512", -- default to sniper
+				chance_inc = 0,
+				interrupt_dmg = 1,
+				interrupt_objective = false,
+				on_executed = {},
+				interrupt_dis = opts.interrupt_dis or 1,
+				patrol_path = "none",
+			},
+		}
+	end
+
+	function restoration:gen_areatrigger(id, name, pos, rot, opts)
+		opts = opts or {}
+		return {
+			id = id,
+			editor_name = name,
+			class = "ElementAreaTrigger",
+			module = "CoreElementArea",
+			values = {
+				execute_on_startup = false,
+				trigger_times = opts.trigger_times or 1,
+				on_executed = opts.on_executed or {},
+				base_delay = opts.base_delay or 0,
+				position = pos,
+				rotation = rot,
+				enabled = true,
+				interval = 0.1,
+				trigger_on = "on_enter",
+				instigator = "player",
+				shape_type = opts.shape_type or "box",
+				width = opts.width or 500,
+				depth = opts.depth or 500,
+				height = opts.height or 500,
+				radius = opts.radius or 250,
+				spawn_unit_elements = {},
+				amount = opts.amount or "1",
+				instigator_name = "",
+				use_disabled_shapes = false,
+				substitute_object = "",
+			},
+		}
+	end
+
+	function restoration:log(...)
+		if self.logging then
+			log("[StreamlinedHeistingAI] " .. table.concat({...}, " "))
+		end
+	end
+
+	function restoration:warn(...)
+		log("[StreamlinedHeistingAI][Warning] " .. table.concat({...}, " "))
+	end
+
+	function restoration:error(...)
+		log("[StreamlinedHeistingAI][Error] " .. table.concat({...}, " "))
+	end
+

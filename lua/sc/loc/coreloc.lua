efd9f3f60edd36e26c9b7932bc5ba231f6f724b7
LR_Daring <78366427+LeftOwlRight@users.noreply.github.com>
2024-03-20 05:37:16 +0800

    Delete Language Chinese
    I won't support the Chinese Language for new Resmod anymore, neither the official site nor being in my own mod. 
    You know the reason, and there's no point keeping it sincce it's been outdated and no one else support it.
    There's nothing bad for you, and I'll just make my changes and my own experience on Resmod myself.
    What do we say? "Each to their own", huh? heh

--- lua/sc/loc/coreloc.lua
+++ lua/sc/loc/coreloc.lua
@@ -1,14 +1,14 @@
 local russian = Idstring("russian"):key() == SystemInfo:language():key()
 local english = Idstring("english"):key() == SystemInfo:language():key()
-local schinese = Idstring("schinese"):key() == SystemInfo:language():key()
+--local schinese = Idstring("schinese"):key() == SystemInfo:language():key()
 local korean = Idstring("korean"):key() == SystemInfo:language():key()
 
 if english then
     dofile(ModPath .. "lua/sc/loc/loc.lua")
 elseif russian then
     dofile(ModPath .. "lua/sc/loc/locru.lua")
-elseif schinese then
-    dofile(ModPath .. "lua/sc/loc/loczh.lua")
+--[[elseif schinese then
+    dofile(ModPath .. "lua/sc/loc/loczh.lua")--]]
 elseif korean then
     dofile(ModPath .. "lua/sc/loc/locko.lua")
 else

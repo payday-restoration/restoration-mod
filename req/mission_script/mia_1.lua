bbc5c073d82a6728d7152afaf1d9c8b537d30cd8
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2024-04-09 19:53:42 +0200

    SH Stuff

--- req/mission_script/mia_1.lua
+++ req/mission_script/mia_1.lua
@@ -12,5 +12,25 @@ return {
 	--Pro Job PONR 
 	[101289] = {
 		ponr = ponr_value
+	},
+	-- Improve reinforce spots
+	[100022] = {
+		reinforce = {
+			{
+				name = "touch_grass",
+				force = 3,
+				position = Vector3(2000, -900, 30)
+			}
+		}
+	},
+	[100589] = {
+		values = {
+			enabled = false
+		}
+	},
+	[100590] = {
+		values = {
+			enabled = false
+		}
 	}
 }	
--- req/mission_script/mia_2.lua
+++ req/mission_script/mia_2.lua
@@ -299,5 +299,26 @@ return {
 	-- Boss dead
 	[100533] = {
 		difficulty = 1
+	},
+	-- Fix nav links
+	[101433] = {
+		values = {
+			spawn_instigator_ids = false
+		}
+	},
+	[101434] = {
+		values = {
+			spawn_instigator_ids = false
+		}
+	},
+	[101435] = {
+		values = {
+			spawn_instigator_ids = false
+		}
+	},
+	[101562] = {
+		values = {
+			spawn_instigator_ids = false
+		}
 	}
 }

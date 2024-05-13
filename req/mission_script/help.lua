dfc086c79b14c325e8c18eb311782796b3d7e362
SonicSoapyBoi <99328709+SonicSoapyBoi@users.noreply.github.com>
2024-05-13 08:28:44 +0200

    Update help.lua

--- req/mission_script/help.lua
+++ req/mission_script/help.lua
@@ -1,52 +1,22 @@
+local so_action_fix = {
+	values = {
+		so_action = "e_nl_down_9_3m_rappel"
+	}
+}
+
 return {
 	-- Fix Prison Nightmare SO animations
-	[100347] = {
-		values = {
-			so_action = "e_nl_down_9_3m_rappel"
-		}
-	},
-	[100348] = {
-		values = {
-			so_action = "e_nl_down_9_3m_rappel"
-		}
-	},
-	[100349] = {
-		values = {
-			so_action = "e_nl_down_9_3m_rappel"
-		}
-	},
-	[100351] = {
-		values = {
-			so_action = "e_nl_down_9_3m_rappel"
-		}
-	},
-	[100352] = {
-		values = {
-			so_action = "e_nl_down_9_3m_rappel"
-		}
-	},
-	[100353] = {
-		values = {
-			so_action = "e_nl_down_9_3m_rappel"
-		}
-	},
-	[100354] = {
-		values = {
-			so_action = "e_nl_down_9_3m_rappel"
-		}
-	},
-	[100355] = {
-		values = {
-			so_action = "e_nl_down_9_3m_rappel"
-		}
-	},
-	[100360] = {
-		values = {
-			so_action = "e_nl_down_9_3m_rappel"
-		}
-	},
+	[100347] = so_action_fix,
+	[100348] = so_action_fix,
+	[100349] = so_action_fix,
+	[100351] = so_action_fix,
+	[100352] = so_action_fix,
+	[100353] = so_action_fix,
+	[100354] = so_action_fix,
+	[100355] = so_action_fix,
+	[100360] = so_action_fix,
 	--PONR at the end of the heist
 	[101715] = {
-    ponr = 300
+		ponr = 300
 	}
 }

2a787b6f5184873ad30012f3d54b8b3876a5bad2
SC <gametwisterdotnet@yahoo.com>
2024-10-05 13:03:11 -0700

    Hell no it wasn't

--- req/mission_script_add/watchdogs_2.lua
+++ req/mission_script_add/watchdogs_2.lua
@@ -1,4 +1,5 @@
 local difficulty = tweak_data:difficulty_to_index(Global.game_settings and Global.game_settings.difficulty or "normal")
+local pro_job = Global.game_settings and Global.game_settings.one_down
 --Gotta be initialized prior to being used, otherwise it'll never return true
 local oops_all_titan_snipers = math.random() < 0.2
 local sniper = ((difficulty == 8 and pro_job and oops_all_titan_snipers) and "units/pd2_dlc_vip/characters/ene_titan_sniper_scripted/ene_titan_sniper_scripted" or difficulty == 8 and "units/pd2_dlc_gitgud/characters/ene_zeal_sniper/ene_zeal_sniper" or difficulty == 7 and "units/pd2_mod_lapd/characters/ene_sniper_3/ene_sniper_3" or difficulty == 6 or difficulty == 5 and "units/payday2/characters/ene_sniper_2_sc/ene_sniper_2_sc") or "units/payday2/characters/ene_sniper_1_sc/ene_sniper_1_sc"
@@ -7,7 +8,6 @@ local fbi_ready_team_2 = "units/payday2/characters/ene_hoxton_breakout_responder
 local fbi_ready_team_dozer = "units/pd2_mod_lapd/characters/ene_bulldozer_3/ene_bulldozer_3"
 local tank = (difficulty == 8 and "units/pd2_dlc_gitgud/characters/ene_bulldozer_minigun/ene_bulldozer_minigun") or "units/payday2/characters/ene_bulldozer_1_sc/ene_bulldozer_1_sc"
 local deathwish_above = difficulty >= 7
-local pro_job = Global.game_settings and Global.game_settings.one_down
 
 local optsSniper_1 = {
 	enemy = sniper,

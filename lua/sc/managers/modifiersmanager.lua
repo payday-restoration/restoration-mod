5d7a56fc65c76e05aa133d71be54c4fa8d5b4226
Doshyy <haritharsyadcp@gmail.com>
2025-02-27 15:04:13 +0800

    added OMNIA Medic Dozer to medic dozer mutator

--- lua/sc/managers/modifiersmanager.lua
+++ lua/sc/managers/modifiersmanager.lua
@@ -116,6 +116,7 @@ elseif ai_type == m then
 		["units/pd2_mod_sharks/characters/ene_murky_fbi_tank_r870/ene_murky_fbi_tank_r870"] = "units/pd2_mod_omnia/characters/ene_bulldozer_minigun/ene_bulldozer_minigun",
 		["units/pd2_mod_sharks/characters/ene_murky_fbi_tank_saiga/ene_murky_fbi_tank_saiga"] = "units/pd2_mod_omnia/characters/ene_bulldozer_2/ene_bulldozer_2",
 		["units/pd2_mod_sharks/characters/ene_murky_fbi_tank_m249/ene_murky_fbi_tank_m249"] = "units/pd2_mod_omnia/characters/ene_bulldozer_3/ene_bulldozer_3",
+		["units/pd2_mod_sharks/characters/ene_murky_fbi_tank_medic/ene_murky_fbi_tank_medic"] = "units/pd2_mod_omnia/characters/ene_bulldozer_medic/ene_bulldozer_medic",
 		
 		--Beat Cops
 		["units/pd2_mod_sharks/characters/ene_murky_cs_cop_c45/ene_murky_cs_cop_c45"] = "units/pd2_mod_sharks/characters/ene_fbi_1/ene_fbi_1",

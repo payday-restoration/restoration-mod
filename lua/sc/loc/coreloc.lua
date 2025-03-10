13d2a64a4a56fa86b348218f0a0728a957a22258
Hater-mater <swetlaniki@gmail.com>
2025-03-11 02:06:40 +0300

    Missing strings prevention (1/2)

--- lua/sc/loc/coreloc.lua
+++ lua/sc/loc/coreloc.lua
@@ -1,19 +1,18 @@
 local russian = Idstring("russian"):key() == SystemInfo:language():key()
-local english = Idstring("english"):key() == SystemInfo:language():key()
+--local english = Idstring("english"):key() == SystemInfo:language():key()
 local schinese = Idstring("schinese"):key() == SystemInfo:language():key()
 local korean = Idstring("korean"):key() == SystemInfo:language():key()
 local spanish = Idstring("spanish"):key() == SystemInfo:language():key()
 
-if english then
-    dofile(ModPath .. "lua/sc/loc/loc.lua")
-elseif russian then
+-- Always load english loc to avoid missing strings in other loc files
+dofile(ModPath .. "lua/sc/loc/loc.lua")
+
+if russian then
     dofile(ModPath .. "lua/sc/loc/locru.lua")
 elseif schinese then
     dofile(ModPath .. "lua/sc/loc/loczh.lua")
 elseif korean then
     dofile(ModPath .. "lua/sc/loc/locko.lua")
 elseif spanish then
-    dofile(ModPath .. "lua/sc/loc/loces.lua")	
-else
-    dofile(ModPath .. "lua/sc/loc/loc.lua")
+    dofile(ModPath .. "lua/sc/loc/loces.lua")
 end

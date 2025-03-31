213f1961fb77d3bd3a680de9eb7124326012bfea
moon <108492539+erushinj@users.noreply.github.com>
2025-03-31 09:07:36 -0600

    Update some PONRs for anti-grief
    - Bank Heist, Counterfeit currently updated
    - Balance multipliers are now supported on PONR elements
    - Clean up formatting in affected mission script files

--- req/mission_script/branchbank.lua
+++ req/mission_script/branchbank.lua
@@ -6,44 +6,20 @@ local teargasmayhem = ((pro_job and difficulty == 8) and 4) or 3
 local vaultdoor = (difficulty == 8 and 100) or 45
 local copcars = (difficulty >= 7 and 2) or 1
 local snipers = (difficulty == 8 and 3 or (difficulty == 7 or difficulty == 6) and 2) or 1
-local ponr_value = (difficulty <= 5 and 600 or (difficulty == 6 or difficulty == 7) and 570) or 540
 local bulldozer = ((pro_job and difficulty == 8) and "units/pd2_mod_omnia/characters/ene_bulldozer_minigun/ene_bulldozer_minigun")
 local titanswat = ((pro_job and difficulty == 8) and "units/pd2_dlc_vip/characters/ene_titan_rifle/ene_titan_rifle")
 local woman_spooc = ((pro_job and difficulty == 8) and "units/pd2_dlc_vip/characters/ene_spook_cloak_1/ene_spook_cloak_1")
 
-local ponr_timer_player_mul = {
-		1,
-		0.85,
-		0.7,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65
-}
 local ponr = {
-	ponr_player_mul = ponr_timer_player_mul,
-	ponr = ponr_value
+	on_executed = {
+		{ id = 400020, delay = 0, },
+	},
 }
 local spooc = {
 	values = {
 		enabled = true,
-		enemy = woman_spooc
-	}
+		enemy = woman_spooc,
+	},
 }
 
 return {
@@ -54,24 +30,24 @@ return {
 	-- Special ambush chance increase
 	[103072] = {
 		values = {
-			chance = 75
-		}
+			chance = 75,
+		},
 	},
 	[105563] = {
 		values = {
-			player_1 = true
-		}
+			player_1 = true,
+		},
 	},
 	[105574] = {
 		values = {
-			player_1 = true
-		}
+			player_1 = true,
+		},
 	},
 	-- Enable max diff after 2 instead of 3 assault waves
 	[101307] = {
 		values = {
-			amount = 2
-		}
+			amount = 2,
+		},
 	},
 	--Restores unused cloaker ambush spawns+Titan Cloaker on DSPJ
 	[105571] = spooc,
@@ -80,71 +56,71 @@ return {
 	--More cop cars arrive on DW+ (similiar to Firestarter Day 3)
 	[103879] = {
 		values = {
-			amount = copcars
-		}
+			amount = copcars,
+		},
 	},
 	--more snipers on high diffs
 	[101200] = {
 		values = {
-			amount = snipers
-		}
+			amount = snipers,
+		},
 	},
 	-- Enable spawns sooner
 	[103882] = {
 		on_executed = {
-			{ id = 100251, delay = 30 },
-			{ id = 105774, delay = 20 }
-		}
+			{ id = 100251, delay = 30, },
+			{ id = 105774, delay = 20, },
+		},
 	},
 	-- Enable all street reinforce spots when first responders arrive
 	[104727] = {
 		on_executed = {
-			{ id = 100364, delay = 0 }
-		}
+			{ id = 100364, delay = 0, },
+		},
 	},
 	[104728] = {
 		on_executed = {
-			{ id = 100364, delay = 0 }
-		}
+			{ id = 100364, delay = 0, },
+		},
 	},
 	[104729] = {
 		on_executed = {
-			{ id = 100364, delay = 0 }
-		}
+			{ id = 100364, delay = 0, },
+		},
 	},
 	[104730] = {
 		on_executed = {
-			{ id = 100364, delay = 0 }
-		}
+			{ id = 100364, delay = 0, },
+		},
 	},
 	--Don't repeat the same dialog with chopper deploying units and/or telling about the tear gas
 	[105362] = {
 		values = {
-			trigger_times = 1
-		}
+			trigger_times = 1,
+		},
 	},
 	[105435] = {
 		values = {
-			trigger_times = 1
-		}
+			trigger_times = 1,
+		},
 	},
 	--Made Tear Gas heli have loopable spawn
 	--set this to 100% so it will always trigger the chance of tear gas heli squad
 	[105496] = { 
 		values = {
-			chance = 100
-		}
+			chance = 100,
+		},
 	},
 	[100631] = {
 		on_executed = {
-			{id = 101747, delay = 150, delay_rand = 120}
-		}
+			{ id = 101747, delay = 150, delay_rand = 120, },
+		},
 	},
 	--the heli1 spawn's trigger_times is set to 1 for some reason, switching to 0 to make the chopper still spawn after once
 	[101424] = {
 		values = {
-			trigger_times = 0
-		}
+			trigger_times = 0,
+		},
 	},
 	--kill chopper spawns once heli squad deploys tear gas
 	[102297] = {
@@ -154,54 +130,54 @@ return {
 			if turn_this_shit_off then
 				turn_this_shit_off:set_enabled(false)
 			end
-		end
+		end,
 	},
 	--The vault door is always locked on DS
 	[100195] = {
 		values = {
-			chance = vaultdoor
-		}
+			chance = vaultdoor,
+		},
 	},
 	[100196] = {
 		values = {
-			chance = vaultdoor
-		}
+			chance = vaultdoor,
+		},
 	},
 	--Pro Job Stuff
 	--2 tear gas choppers instead of 1
 	[105610] = {
 		values = {
-			amount = teargaschopper
+			amount = teargaschopper,
 		},
 		on_executed = {
-			{id = 105496, delay = 15}
-		}
+			{ id = 105496, delay = 15, },
+		},
 	},
 	--More rooms filled with tear gas
 	[102195] = {
 		values = {
-			amount = teargas
-		}
+			amount = teargas,
+		},
 	},
 	[101355] = {
 		values = {
-			amount = teargasmayhem
-		}
+			amount = teargasmayhem,
+		},
 	},
 	--Heli spawns
 	[101785] = {
 		values = {
-			enemy = bulldozer
-		}
+			enemy = bulldozer,
+		},
 	},
 	[101432] = {
 		values = {
-			enemy = titanswat
-		}
+			enemy = titanswat,
+		},
 	},
 	[101433] = {
 		values = {
-			enemy = titanswat
-		}
+			enemy = titanswat,
+		},
 	}
 }
--- req/mission_script_add/branchbank.lua
+++ req/mission_script_add/branchbank.lua
@@ -1,11 +1,46 @@
---[[
+local pro_job = Global.game_settings and Global.game_settings.one_down
 local difficulty = tweak_data:difficulty_to_index(Global.game_settings and Global.game_settings.difficulty or "normal")
+local ponr_value = (difficulty <= 5 and 600 or (difficulty == 6 or difficulty == 7) and 570) or 540
+local ponr_timer_player_mul = {
+	1,
+	0.85,
+	0.7,
+	0.65,
+}
+
+local opts_pro_job_ponr = {
+	elements = { 104722, 104723, 100512, },
+	trigger_times = 0,
+	time_balance_mul = ponr_timer_player_mul,
+	time_easy = ponr_value,
+	time_normal = ponr_value,
+	time_hard = ponr_value,
+	time_overkill = ponr_value,
+	time_overkill_145 = ponr_value,
+	time_easy_wish = ponr_value,
+	time_overkill_290 = ponr_value,
+	time_sm_wish = ponr_value,
+	enabled = pro_job,
+}
+
+return {
+	elements = {
+		restoration:gen_pointofnoreturn(
+			400020,
+			"pro_job_ponr",
+			Vector3(0, 0, 0),
+			Rotation(0, 0, 0),
+			opts_pro_job_ponr
+		),
+	},
+}
+
+--[[
 local tank_skull = (difficulty == 8 and "units/pd2_dlc_gitgud/characters/ene_zeal_bulldozer_3_sc/ene_zeal_bulldozer_3_sc") or "units/payday2/characters/ene_bulldozer_3_sc/ene_bulldozer_3_sc"
 local cloaker = (difficulty == 8 and "units/pd2_dlc_gitgud/characters/ene_zeal_cloaker_sc/ene_zeal_cloaker_sc") or "units/payday2/characters/ene_spook_1/ene_spook_1"
 local cop_1 = "units/payday2/characters/ene_cop_1/ene_cop_1"
 local cop_2 = "units/payday2/characters/ene_cop_2/ene_cop_2"
 local cop_4 = "units/payday2/characters/ene_cop_4/ene_cop_4"
-local pro_job = Global.game_settings and Global.game_settings.one_down
 local death_wish_above = difficulty >= 7
 local death_sentence = difficulty == 8
 local diff_scaling = 0.065 * difficulty
@@ -13,88 +48,88 @@ local diff_scaling_2 = 0.085 * difficulty
 
 
 local optsBulldozer = {
-    enemy = tank_skull,
-    on_executed = { 
-		{ id = 400003, delay = 3 }
+	enemy = tank_skull,
+	on_executed = {
+		{ id = 400003, delay = 3, },
 	},
-	enabled = death_wish
+	enabled = death_wish,
 }
 local optsBulldozerDS = {
-    enemy = tank_skull,
-    on_executed = { 
-		{ id = 400004, delay = 3 }
+	enemy = tank_skull,
+	on_executed = {
+		{ id = 400004, delay = 3, },
 	},
-	enabled = death_sentence
+	enabled = death_sentence,
 }
 local optsPatrolCop_1 = {
-    enemy = patrolman_1,
+	enemy = patrolman_1,
 	participate_to_group_ai = true,
-	on_executed = { 
-		{ id = 400013, delay = 0 } 
+	on_executed = {
+		{ id = 400013, delay = 0, },
 	},
-    enabled = true
+	enabled = true,
 }
 local optsPatrolCop_2 = {
-    enemy = patrolman_2,
+	enemy = patrolman_2,
 	participate_to_group_ai = true,
-	on_executed = { 
-		{ id = 400013, delay = 0 } 
+	on_executed = {
+		{ id = 400013, delay = 0, },
 	},
-    enabled = true
+	enabled = true,
 }
 local optsPatrolCop_3 = {
-    enemy = patrolman_3,
+	enemy = patrolman_3,
 	participate_to_group_ai = true,
-	on_executed = { 
-		{ id = 400015, delay = 0 } 
+	on_executed = {
+		{ id = 400015, delay = 0, },
 	},
-    enabled = true
+	enabled = true,
 }
 local optsPatrolCop_4 = {
-    enemy = patrolman_1,
+	enemy = patrolman_1,
 	participate_to_group_ai = true,
-	on_executed = { 
-		{ id = 400015, delay = 0 } 
+	on_executed = {
+		{ id = 400015, delay = 0, },
 	},
-    enabled = true
+	enabled = true,
 }
 local optsPatrolCop_5 = {
-    enemy = patrolman_2,
+	enemy = patrolman_2,
 	participate_to_group_ai = true,
-	on_executed = { 
-		{ id = 400014, delay = 0 } 
+	on_executed = {
+		{ id = 400014, delay = 0, },
 	},
-    enabled = true
+	enabled = true,
 }
 local optsPatrolCop_6 = {
-    enemy = patrolman_3,
+	enemy = patrolman_3,
 	participate_to_group_ai = true,
-	on_executed = { 
-		{ id = 400014, delay = 0 } 
+	on_executed = {
+		{ id = 400014, delay = 0, },
 	},
-    enabled = true
+	enabled = true,
 }
 local optsPatrolCop_7 = {
-    enemy = patrolman_1,
+	enemy = patrolman_1,
 	participate_to_group_ai = true,
-	on_executed = { 
-		{ id = 400016, delay = 0 } 
+	on_executed = {
+		{ id = 400016, delay = 0, },
 	},
-    enabled = true
+	enabled = true,
 }
 local optsPatrolCop_8 = {
-    enemy = patrolman_2,
+	enemy = patrolman_2,
 	participate_to_group_ai = true,
-	on_executed = { 
-		{ id = 400016, delay = 0 } 
+	on_executed = {
+		{ id = 400016, delay = 0, },
 	},
-    enabled = true
+	enabled = true,
 }
 local optsCloaker = {
-    enemy = cloaker,
+	enemy = cloaker,
 	participate_to_group_ai = true,
 	spawn_action = "e_sp_clk_over_2_5m",
-    enabled = true
+	enabled = true,
 }
 local optsDefend_SO = {
 	SO_access = "4096",
@@ -103,7 +138,7 @@ local optsDefend_SO = {
 	needs_pos_rsrv = true,
 	align_rotation = true,
 	interval = 2,
-    so_action = "AI_defend"
+	so_action = "AI_defend",
 }
 local optsWalkToSpot = {
 	SO_access = "64",
@@ -111,151 +146,151 @@ local optsWalkToSpot = {
 	align_position = true,
 	needs_pos_rsrv = true,
 	align_rotation = true,
-	interval = 2
+	interval = 2,
 }
 local Bain_senddozers = {
 	dialogue = "Play_ban_s02_b",
-	can_not_be_muted = true
+	can_not_be_muted = true,
 }
 
 
 return {
-    elements = {
-        -- Skulldozer nearby the van (based on DW Trailer)
-        restoration:gen_dummy(
-            400001,
-            "van_dozer_1",
-            Vector3(-8305, -3511, 0),
-            Rotation(-90, 0, -0),
-            optsBulldozer
-        ),
-        restoration:gen_dummy(
-            400002,
-            "van_dozer_2",
-            Vector3(-8305, -3593, 0),
-            Rotation(-90, 0, -0),
-            optsBulldozerDS
-        ),
-        restoration:gen_so(
-            400003,
-            "dozer_defend_so_1",
-            Vector3(-7273, -2895, -19.999),
-            Rotation(0, 0, -0),
-            optsDefend_SO
-        ),
+	elements = {
+		-- Skulldozer nearby the van (based on DW Trailer)
+		restoration:gen_dummy(
+			400001,
+			"van_dozer_1",
+			Vector3(-8305, -3511, 0),
+			Rotation(-90, 0, -0),
+			optsBulldozer
+		),
+		restoration:gen_dummy(
+			400002,
+			"van_dozer_2",
+			Vector3(-8305, -3593, 0),
+			Rotation(-90, 0, -0),
+			optsBulldozerDS
+		),
 		restoration:gen_so(
-            400004,
-            "dozer_defend_so_2",
-            Vector3(-7177, -2895, -19.999),
-            Rotation(0, 0, 0),
-            optsDefend_SO
-        ),
+			400003,
+			"dozer_defend_so_1",
+			Vector3(-7273, -2895, -19.999),
+			Rotation(0, 0, -0),
+			optsDefend_SO
+		),
+		restoration:gen_so(
+			400004,
+			"dozer_defend_so_2",
+			Vector3(-7177, -2895, -19.999),
+			Rotation(0, 0, 0),
+			optsDefend_SO
+		),
 		--Patrol Cops
 		restoration:gen_dummy(
-            400005,
-            "patrolman_1",
-            Vector3(-6140, -6534, 0),
-            Rotation(90, -0, -0),
-            optsPatrolCop_1
-        ),
-        restoration:gen_dummy(
-            400006,
-            "patrolman_2",
+			400005,
+			"patrolman_1",
+			Vector3(-6140, -6534, 0),
+			Rotation(90, -0, -0),
+			optsPatrolCop_1
+		),
+		restoration:gen_dummy(
+			400006,
+			"patrolman_2",
 			Vector3(-6190, -6534, 0),
-            Rotation(90, -0, -0),
-            optsPatrolCop_2
-        ),
+			Rotation(90, -0, -0),
+			optsPatrolCop_2
+		),
 		restoration:gen_dummy(
-            400007,
-            "patrolman_3",
-            Vector3(6602, 134, 5),
-            Rotation(-180, 0, -0),
-            optsPatrolCop_3
-        ),
-        restoration:gen_dummy(
-            400008,
-            "patrolman_4",
-            Vector3(6602, 76, 5),
-            Rotation(-180, 0, -0),
-            optsPatrolCop_4
-        ),
+			400007,
+			"patrolman_3",
+			Vector3(6602, 134, 5),
+			Rotation(-180, 0, -0),
+			optsPatrolCop_3
+		),
 		restoration:gen_dummy(
-            400009,
-            "patrolman_5",
-            Vector3(2604, -2207, 0),
-            Rotation(0, 0, -0),
-            optsPatrolCop_5
-        ),
-        restoration:gen_dummy(
-            400010,
-            "patrolman_6",
-            Vector3(2604, -2294, 0),
-            Rotation(0, 0, -0),
-            optsPatrolCop_6
-        ),
+			400008,
+			"patrolman_4",
+			Vector3(6602, 76, 5),
+			Rotation(-180, 0, -0),
+			optsPatrolCop_4
+		),
 		restoration:gen_dummy(
-            400011,
-            "patrolman_7",
-            Vector3(2665, 6771, 0),
-            Rotation(90, -0, -0),
-            optsPatrolCop_7
-        ),
+			400009,
+			"patrolman_5",
+			Vector3(2604, -2207, 0),
+			Rotation(0, 0, -0),
+			optsPatrolCop_5
+		),
 		restoration:gen_dummy(
-            400012,
-            "patrolman_8",
-            Vector3(2586, 6771, 0),
-            Rotation(90, -0, -0),
-            optsPatrolCop_8
-        ),
+			400010,
+			"patrolman_6",
+			Vector3(2604, -2294, 0),
+			Rotation(0, 0, -0),
+			optsPatrolCop_6
+		),
+		restoration:gen_dummy(
+			400011,
+			"patrolman_7",
+			Vector3(2665, 6771, 0),
+			Rotation(90, -0, -0),
+			optsPatrolCop_7
+		),
+		restoration:gen_dummy(
+			400012,
+			"patrolman_8",
+			Vector3(2586, 6771, 0),
+			Rotation(90, -0, -0),
+			optsPatrolCop_8
+		),
 		restoration:gen_so(
-            400013,
-            "walk_to_the_spot_1",
-            Vector3(6502, -2060, 1.001),
-            Rotation(-180, 0, -0),
-            optsWalkToSpot
-        ),
+			400013,
+			"walk_to_the_spot_1",
+			Vector3(6502, -2060, 1.001),
+			Rotation(-180, 0, -0),
+			optsWalkToSpot
+		),
 		restoration:gen_so(
-            400014,
-            "walk_to_the_spot_2",
-            Vector3(-5801, -6560, 0),
-            Rotation(-90, 0, -0),
-            optsWalkToSpot
-        ),
+			400014,
+			"walk_to_the_spot_2",
+			Vector3(-5801, -6560, 0),
+			Rotation(-90, 0, -0),
+			optsWalkToSpot
+		),
 		restoration:gen_so(
-            400015,
-            "walk_to_the_spot_3",
-            Vector3(3001, 6407, 0),
-            Rotation(-90, 0, -0),
-            optsWalkToSpot
-        ),
+			400015,
+			"walk_to_the_spot_3",
+			Vector3(3001, 6407, 0),
+			Rotation(-90, 0, -0),
+			optsWalkToSpot
+		),
 		restoration:gen_so(
-            400016,
-            "walk_to_the_spot_4",
-            Vector3(6637, 297, 0),
-            Rotation(0, 0, -0),
-            optsWalkToSpot
-        ),
+			400016,
+			"walk_to_the_spot_4",
+			Vector3(6637, 297, 0),
+			Rotation(0, 0, -0),
+			optsWalkToSpot
+		),
 		restoration:gen_dummy(
-            4000017,
-            "cloaker_1",
-            Vector3(-1147, 4391, 0),
-            Rotation(-180, 0, -0),
-            optsCloaker
-        ),
-        restoration:gen_dummy(
-            400018,
-            "cloaker_2",
-            Vector3(-1233, 4395, 0),
-            Rotation(-180, 0, -0),
-            optsCloaker
-        ),
+			4000017,
+			"cloaker_1",
+			Vector3(-1147, 4391, 0),
+			Rotation(-180, 0, -0),
+			optsCloaker
+		),
 		restoration:gen_dummy(
-            400019,
-            "cloaker_3",
-            Vector3(-1317, 4395, 0),
-            Rotation(-180, 0, -0),
-            optsCloaker
-        )
-    }
+			400018,
+			"cloaker_2",
+			Vector3(-1233, 4395, 0),
+			Rotation(-180, 0, -0),
+			optsCloaker
+		),
+		restoration:gen_dummy(
+			400019,
+			"cloaker_3",
+			Vector3(-1317, 4395, 0),
+			Rotation(-180, 0, -0),
+			optsCloaker
+		),
+	},
 }
 ]]--

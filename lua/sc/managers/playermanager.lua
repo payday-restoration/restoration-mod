810b9d676e8bf94d568263c2bdaf0640b1bc68e5
SC <gametwisterdotnet@yahoo.com>
2025-04-12 14:36:01 -0700

    Lower bag pickup cooldown and make it consistent (From SH)

--- lua/sc/managers/playermanager.lua
+++ lua/sc/managers/playermanager.lua
@@ -1523,3 +1523,15 @@ Hooks:PostHook(PlayerManager, "sync_tag_team", "sync_tag_team_sound_effect", fun
 		self:local_player():sound():play(tweak_data.blackmarket.projectiles.tag_team.sounds.activate)
 	end
 end)
+
+-- Make cooldown for picking up bags consistent instead of random
+local drop_carry_original = PlayerManager.drop_carry
+function PlayerManager:drop_carry(...)
+	local carry_data = self:get_my_carry_data()
+
+	drop_carry_original(self, ...)
+
+	if carry_data then
+		self._carry_blocked_cooldown_t = Application:time() + 0.5
+	end
+end

6c4c39572d2262c65aae7952d16508e8ef89669a
moon <108492539+erushinj@users.noreply.github.com>
2025-05-17 23:17:15 -0600

    Watchdogs day 2 PONR
    - Manually specify escape zone elements and clean up formatting
    - Mission script add NOT yet reworked, currently only re-enabled

--- req/mission_script/watchdogs_2.lua
+++ req/mission_script/watchdogs_2.lua
@@ -1,44 +1,17 @@
-local difficulty = tweak_data:difficulty_to_index(Global.game_settings and Global.game_settings.difficulty or "normal")	
-local ponr_value = (difficulty <= 5 and 1080 or (difficulty == 6 or difficulty == 7) and 1050) or 1020	
-	
-	local ponr_timer_player_mul = {
-		1,
-		0.85,
-		0.7,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65
-}
-
 local enabled = {
 	values = {
-        enabled = true
-	}
-}
-return {
-	--Pro Job PONR 
-	[100324] = {
-		ponr_player_mul = ponr_timer_player_mul,
-		ponr = ponr_value
+		enabled = true,
 	},
-	--Enable unused Reinforce spots
-	--didn't knew that day 2 had reinforce spots to begin with
+}
+
+return {
+	-- Pro Job PONR
+	[100324] = {
+		on_executed = {
+			{ id = 400061, delay = 0, },
+		},
+	},
+	-- Enable unused reinforce points
 	[101954] = enabled,
 	[101955] = enabled,
 	[101984] = enabled,
@@ -48,33 +21,33 @@ return {
 	[102126] = enabled,
 	[100210] = {
 		values = {
-			enabled = false
-		}
+			enabled = false,
+		},
 	},
-	--Spawn FBI Ready Teams
-	--Spawn Ground Snipers after 3 minutes
-	--Spawn a scripted dozer after 150 seconds
-	--[[[100486] = {
+	-- Spawn FBI Ready Teams
+	-- Spawn a scripted dozer after 150 seconds
+	-- Spawn Ground Snipers after 3 minutes
+	[100486] = {
 		on_executed = {
-			{ id = 400054, delay = 25 },
-			{ id = 400058, delay = 150 },
-			{ id = 400056, delay = 180 }
-		}
+			{ id = 400054, delay = 25, },
+			{ id = 400058, delay = 150, },
+			{ id = 400056, delay = 180, },
+		},
 	},
-	--Spawn Snipers on the ships
+	-- Spawn Snipers on the ships
 	[102182] = {
 		on_executed = {
-			{ id = 400013, delay = 20 }
-		}
+			{ id = 400013, delay = 20, },
+		},
 	},
 	[102388] = {
 		on_executed = {
-			{ id = 400014, delay = 20 }
-		}
+			{ id = 400014, delay = 20, },
+		},
 	},
 	[102335] = {
 		on_executed = {
-			{ id = 400015, delay = 20 }
-		}
-	} ]]--
+			{ id = 400015, delay = 20, },
+		},
+	},
 }

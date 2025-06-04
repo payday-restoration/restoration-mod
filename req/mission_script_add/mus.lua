46fb4d6a3b927aa631b7f9d7883f30b4723bbfdd
moon <108492539+erushinj@users.noreply.github.com>
2025-06-03 23:44:39 -0600

    Forgot to save the code comment

--- req/mission_script_add/mus.lua
+++ req/mission_script_add/mus.lua
@@ -1,6 +1,8 @@
 local pro_job = Global.game_settings and Global.game_settings.one_down
 local difficulty = tweak_data:difficulty_to_index(Global.game_settings and Global.game_settings.difficulty or "normal")
 
+-- Without serious crowd control like multiple ECM feedback loops, it's suicidal to try and get all loot
+-- Team AI are excluded from PONR duration calculation
 local ponr_value = (difficulty <= 5 and 750 or (difficulty == 6 or difficulty == 7) and 750) or 750
 local ponr_timer_player_mul = {
 	1,

856a8d876329502cc44c7500372b21143d0aa6da
Doshyy <haritharsyadcp@gmail.com>
2025-06-09 16:45:47 +0800

    Mountain Master PONR

--- req/mission_script/pent.lua
+++ req/mission_script/pent.lua
@@ -1,30 +1,4 @@
 local difficulty = tweak_data:difficulty_to_index(Global.game_settings and Global.game_settings.difficulty or "normal")
-local ponr_value = (difficulty <= 5 and 1200 or (difficulty == 6 or difficulty == 7) and 1140) or 1080
-
-local ponr_timer_player_mul = {
-		1,
-		0.85,
-		0.7,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65,
-		0.65
-}
 local sniper_trigger_times = {
 	values = {
        trigger_times = 0
@@ -32,10 +6,16 @@ local sniper_trigger_times = {
 }
 
 return {
-	--Pro Job PONR 
+	--Pro Job PONR, whisper state is off 
 	[101226] = {
-		ponr_player_mul = ponr_timer_player_mul,
-		ponr = ponr_value
+		on_executed = {
+			{id = 100057, delay = 0,},
+		}
+	},
+	[100680] = {
+		on_executed = {
+			{id = 100057, delay = 0,},
+		}
 	},
 	--Fixed snipers being able to spawn only once
 	[100368] = sniper_trigger_times,
@@ -55,7 +35,7 @@ return {
 	[103831] = {
 		reinforce = {
 			{
-				name = "main_room"
+				name = "main_room",
 			},
 			{
 				name = "helipad",
---
+++ req/mission_script_add/pent.lua
@@ -0,0 +1,39 @@
+local pro_job = Global.game_settings and Global.game_settings.one_down
+local difficulty = tweak_data:difficulty_to_index(Global.game_settings and Global.game_settings.difficulty or "normal")
+local ponr_value = (difficulty <= 5 and 1200 or (difficulty == 6 or difficulty == 7) and 1140) or 1080
+local ponr_timer_player_mul = {
+	1,
+	0.9,
+	0.8,
+	0.7,  -- 4+ players
+}
+local opts_pro_job_ponr = {
+    elements_in_instances = {
+        ["pent_helicopter_extraction_001"] = { 100010, },
+    },
+    trigger_times = 0,
+	time_balance_mul_include_team_ai = false,
+	time_balance_mul = ponr_timer_player_mul,
+	time_easy = ponr_value,
+	time_normal = ponr_value,
+	time_hard = ponr_value,
+	time_overkill = ponr_value,
+	time_overkill_145 = ponr_value,
+	time_easy_wish = ponr_value,
+	time_overkill_290 = ponr_value,
+	time_sm_wish = ponr_value,
+	enabled = pro_job,
+}
+local opts_pro_job_ponr_counter = {
+    enabled = true,
+    counter_target = 2, 
+    on_executed = {
+        {id = 100044, delay = 0, }
+    },
+}
+return {
+    elements = {
+        restoration:gen_pointofnoreturn(100044, "pro_job_ponr", Vector3(0, 0, 0), Rotation(0, 0, 0), opts_pro_job_ponr),
+        restoration:gen_counter(100057, "pro_job_ponr_counter",Vector3(0, 0, 0), Rotation(0, 0, 0), opts_pro_job_ponr_counter),
+    },
+}

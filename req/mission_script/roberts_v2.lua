29fca5041867ca61559730f9d386224259b5536a
Doshyy <haritharsyadcp@gmail.com>
2025-06-11 00:38:30 +0800

    last min catch up

--- req/mission_script/roberts_v2.lua
+++ req/mission_script/roberts_v2.lua
@@ -1,21 +1,26 @@
 return {
     -- Add early reinforce around the bank
 	[100109] = {
-		reinforce = {
+	reinforce = {
 			{
 				name = "bank_left",
-				force = 3,
+				force = 2,
 				position = Vector3(-500, -3000, -75),
 			},
 			{
+				name = "bank_right",
+				force = 2,
+				position = Vector3(450, 1750, -75),
+			},
+			{
 				name = "bank_front",
-				force = 3,
-				position = Vector3(3000, 0, -75),
+				force = 2,
+				position = Vector3(2950, -650, -75),
 			},
 			{
 				name = "bank_back",
-				force = 3,
-				position = Vector3(-3000, -1400, -60),
+				force = 2,
+				position = Vector3(-3250, -1375, -60),
 			},
 		},
 	},

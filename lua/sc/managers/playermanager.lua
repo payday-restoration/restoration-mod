5ccc4ffe5c29c27c85920403d2e509c3aa0133b1
DeadMansChest <everyonewins@live.ca>
2025-06-14 14:19:32 -0400

    less ouchies when driving

--- lua/sc/managers/playermanager.lua
+++ lua/sc/managers/playermanager.lua
@@ -766,6 +766,8 @@ function PlayerManager:damage_reduction_skill_multiplier(damage_type)
 	multiplier = multiplier * self:get_hostage_bonus_multiplier("damage_dampener") --Might be unused.
 	multiplier = multiplier * self._properties:get_property("revive_damage_reduction", 1)
 	multiplier = multiplier * self._temporary_properties:get_property("revived_damage_reduction", 1)
+	local driving = self:current_state() == "driving"
+	multiplier = multiplier * ((driving and 0.5) or 1) --less ouchies when in a vehicle
 	--Removed vanilla crew chief team DR.
 	if self._buildup_meter and self:has_category_upgrade("player", "buildup_meter_pacify") then
 		local dr_stats = self:upgrade_value("player", "buildup_meter_pacify", 0)

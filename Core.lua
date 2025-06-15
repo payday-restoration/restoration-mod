69414409ad16d8122368153b0328306bea292f5c
moon <108492539+erushinj@users.noreply.github.com>
2025-06-15 13:12:54 -0600

    The Ukrainian Prisoner PONR
    - PONR has min diff 0.7 (third assault equivalent) and 20s Bravos delay
    - Adapt to new functionality instead of using alternate behaviour table
    - Low difficulties Dozer ambush can have 1-2 instead of always 2 on Pro Jobs
    - Tweak Dozer ambush chance (no longer strictly guaranteed on DSPJ)

--- Core.lua
+++ Core.lua
@@ -211,7 +211,6 @@ function restoration:Init()
 	-- Disable Bravos spawning on PONRs for these heists, usually for heists that have PONRs that go on/off. Also kills forced 1 diff and music changes on Pro Job
 	-- TODO: make use of new PONR element functionality instead of this table
 	restoration.alternate_ponr_behavior = {
-		"sand",  -- The Ukrainian Prisoner
 		"trai",  -- Lost in Transit
 		"fuel",  -- Fueled Feuds
 	}

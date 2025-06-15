d69df2e8a33fa2c82a072da48041be3eba670810
Doshyy <haritharsyadcp@gmail.com>
2025-06-16 07:38:34 +0800

    forgot to format  this one

--- req/mission_script/born.lua
+++ req/mission_script/born.lua
@@ -95,7 +95,7 @@ return {
 	},
 	-- Have Vest Biker beat up Mike
 	[100920] = guard_biker,
-	--Biker replacements
+	-- Biker replacements
 	[100254] = guard_biker,
 	[100295] = guard_biker,
 	[100301] = guard_biker,

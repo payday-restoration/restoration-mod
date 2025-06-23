ee3bebc8e4abcd82183158a26ca0e0bb482d76c9
Doshyy <haritharsyadcp@gmail.com>
2025-06-23 17:03:05 +0800

    re-enabled swat turret on Diamond store for the trophy thing
    also add in new reinforce stuff from Eclipse (and ASS)

--- req/mission_script/family.lua
+++ req/mission_script/family.lua
@@ -10,22 +10,59 @@ local disabled = {
 }	
 
 return {
-	--Pro Job PONR 
+	-- Pro Job PONR 
 	[101095] = {
 		ponr = ponr_value,
 		hunt = hunt_projob
 	},
-	--Disable Titan Cams
+	-- Disable Titan Cams
 	[102211] = disabled,
-	-- restores unused sniper spawn
+	-- Restores unused sniper spawn
 	[100370] = {
 		values = {
 			enabled = true
 		}
 	},
-	--disable the bad van escape spots
+	-- Add custom reinforce from ASS
+	[100324] = disabled, -- reenforce
+	[100022] = {
+		on_executed = { -- delay Twitch from leaving the area after the heist goes loud
+			{ id = 100168, delay = 8 },
+		},
+		reinforce = {
+			{
+				name = "street1",
+				force = 2,
+				position = Vector3(-750, 450, 0),
+			},
+			{
+				name = "street2",
+				force = 2,
+				position = Vector3(-1750, 450, 0),
+			},
+			{
+				name = "street3",
+				force = 2,
+				position = Vector3(-1000, -4000, 0),
+			},
+			{
+				name = "street4",
+				force = 2,
+				position = Vector3(3000, -4000, 0),
+			},
+			{
+				name = "alley",
+				force = 2,
+				position = Vector3(2200, 1000, 0),
+			},
+			{
+				name = "plaza",
+				force = 3,
+				position = Vector3(4250, -1700, 0),
+			},
+		},
+	},
+	-- Disable the bad van escape spots
 	[100754] = disabled,
 	[100755] = disabled,
-	--Disable the SWAT Turret, 2015 really meant the slowly removal of reinforce units
-	[102124] = disabled
 }	

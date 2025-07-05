91e46463d93d31ed97424e7f8c3eb8fcec0eb627
LR_Daring <78366427+LeftOwlRight@users.noreply.github.com>
2025-07-05 17:29:29 +0800

    do some compatibility

--- lua/sc/loc/coreloc.lua
+++ lua/sc/loc/coreloc.lua
@@ -1,18 +1,39 @@
 local russian = Idstring("russian"):key() == SystemInfo:language():key()
---local english = Idstring("english"):key() == SystemInfo:language():key()
+local english = Idstring("english"):key() == SystemInfo:language():key()
 local schinese = Idstring("schinese"):key() == SystemInfo:language():key()
 local korean = Idstring("korean"):key() == SystemInfo:language():key()
 local spanish = Idstring("spanish"):key() == SystemInfo:language():key()
 
--- Always load english loc to avoid missing strings in other loc files
-dofile(ModPath .. "lua/sc/loc/loc.lua")
+-- I'm sorry to do these bullshit, but this is because "Always load english loc" would result in a weird incompatible issue with my(LR_Daring) mod for Resmod, while I can't do nothing in my side
+if ChinStringFixes and ChinStringFixes.settings and ChinStringFixes.settings.Mod_Support and ChinStringFixes.settings.Mod_Support.Resmod and ChinStringFixes.settings.Mod_Support.Resmod.Resmod_Compat and ChinStringFixes.settings.Mod_Support.Resmod.Resmod_Compat ~= 1 and ChinStringFixes.settings.Enable_String then 
 
-if russian then
-    dofile(ModPath .. "lua/sc/loc/locru.lua")
-elseif schinese then
-    dofile(ModPath .. "lua/sc/loc/loczh.lua")
-elseif korean then
-    dofile(ModPath .. "lua/sc/loc/locko.lua")
-elseif spanish then
-    dofile(ModPath .. "lua/sc/loc/loces.lua")
+    if english then   
+        dofile(ModPath .. "lua/sc/loc/loc.lua")
+    elseif russian then
+        dofile(ModPath .. "lua/sc/loc/locru.lua")
+    elseif schinese then
+        dofile(ModPath .. "lua/sc/loc/loczh.lua")
+    elseif korean then
+        dofile(ModPath .. "lua/sc/loc/locko.lua")
+    elseif spanish then
+        dofile(ModPath .. "lua/sc/loc/loces.lua")
+    else
+        dofile(ModPath .. "lua/sc/loc/loc.lua")
+    end
+    
+else
+
+    -- Always load english loc to avoid missing strings in other loc files
+    dofile(ModPath .. "lua/sc/loc/loc.lua")
+
+    if russian then
+        dofile(ModPath .. "lua/sc/loc/locru.lua")
+    elseif schinese then
+        dofile(ModPath .. "lua/sc/loc/loczh.lua")
+    elseif korean then
+        dofile(ModPath .. "lua/sc/loc/locko.lua")
+    elseif spanish then
+        dofile(ModPath .. "lua/sc/loc/loces.lua")
+    end
+
 end

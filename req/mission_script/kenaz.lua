5cf221e7ee4b86c3a08731eea5dee9c5b03ec228
moon <108492539+erushinj@users.noreply.github.com>
2025-07-05 11:04:06 -0600

    Golden Grin reinforce
    - Disable vanilla reinforce on the main mission script
    - Likely still needs tweaking (changing location of some, more points, and/or higher min force) but it should be less awful than before

--- req/mission_script/kenaz.lua
+++ req/mission_script/kenaz.lua
@@ -31,29 +31,64 @@ return {
 			{ id = 400005, delay = 0, },
 		},
 	},
-	-- Reinforce points
-	[101620] = {
+	-- Remove vanilla reinforce
+	[103204] = disable,
+	[103205] = disable,
+	[103206] = disable,
+	[103207] = disable,
+	[103209] = disable,
+	-- New reinforce points
+	[100229] = {
 		reinforce = {
 			{
-				name = "stairs_right",
-				force = 2,
-				position = Vector3(375, -3675, 300),
+				name = "exterior_entrance",  -- Just outside reception, on the red carpet
+				force = 4,
+				position = Vector3(0, -8250, 2.701),
 			},
 			{
-				name = "stairs_left",
+				name = "exterior_gate1",  -- Outside side entrance to the right
 				force = 2,
-				position = Vector3(-350, -3675, 300),
+				position = Vector3(2350, -5525, -0),
 			},
-		},
-	},
-	[100379] = {
-		reinforce = {
-			{ name = "stairs_right", },
-			{ name = "stairs_left", },
 			{
-				name = "security",
+				name = "exterior_gate2",  -- Near exterior bar
+				force = 2,
+				position = Vector3(-2425, -5800, -0),
+			},
+			{
+				name = "exterior_relax",  -- A back corner in the pool area
+				force = 2,
+				position = Vector3(-4075, -2775, 0),
+			},
+			{
+				name = "exterior_balcony",  -- Overlooking VIP area entrance
+				force = 2,
+				position = Vector3(1925, -2575, 602.5),
+			},
+			{
+				name = "interior_balcony1",  -- Overlooking BFD, far
+				force = 2,
+				position = Vector3(-1300, -550, 602.5),
+			},
+			{
+				name = "interior_balcony2",  -- Overlooking BFD, near
+				force = 2,
+				position = Vector3(1075, -700, 602.5),
+			},
+			{
+				name = "security1",  -- On one side of the security gate
+				force = 2,
+				position = Vector3(-1100, 1475, 100),
+			},
+			{
+				name = "security2",  -- On the other side
+				force = 2,
+				position = Vector3(1100, 1400, 100),
+			},
+			{
+				name = "bar",  -- Bar by reception area
 				force = 3,
-				position = Vector3(125, 1575, 100),
+				position = Vector3(1100, 1400, 100),
 			},
 		},
 	},

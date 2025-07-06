e813ba6932cc0a5ee7868ed1b7e490b9d44976be
DeadMansChest <everyonewins@live.ca>
2025-07-06 19:48:09 -0400

    Update tweakdata.lua

--- lua/sc/tweak_data/tweakdata.lua
+++ lua/sc/tweak_data/tweakdata.lua
@@ -57,7 +57,7 @@ tweak_data.pistol = {
 	swap_bonus = 3.2,
 	ads_move_speed_mult = 0.8,
 	moving_spread_mult = 0.5,
-	hipfire_spread_mult = 0.75
+	hipfire_spread_mult = 0.6
 }
 	tweak_data.pistol_pdw = {
 		ads_move_speed_mult = 0.7 / tweak_data.pistol.ads_move_speed_mult
@@ -137,8 +137,8 @@ tweak_data.shotgun = {
 tweak_data.smg = {
 	swap_bonus = 1.65,
 	ads_move_speed_mult = 0.7,
-	moving_spread_mult = 0.75,
-	hipfire_spread_mult = 0.75
+	moving_spread_mult = 0.7,
+	hipfire_spread_mult = 0.7
 }
 	tweak_data.pdw = {
 		ads_move_speed_mult = 0.5 / tweak_data.smg.ads_move_speed_mult

9738db126669aafd5893039dcefa37133f80f840
Doshyy <haritharsyadcp@gmail.com>
2025-08-16 00:13:23 +0800

    theres an "o" in my type
    (a little typo adjustment)

--- req/mission_script_add/election_day_2.lua
+++ req/mission_script_add/election_day_2.lua
@@ -53,7 +53,7 @@ return {
 		restoration:gen_dummy(400009, "swat_van_spawn_6", Vector3(-2260.706, 1937.698, -19.999), Rotation(74, 0, 0), optsBesiegeDummy),
 		restoration:gen_dummy(400010, "swat_van_spawn_7", Vector3(-2292.224, 2016.435, -19.999), Rotation(74, 0, 0), optsBesiegeDummy),
 		restoration:gen_dummy(400011, "swat_van_spawn_8", Vector3(-2310.692, 1952.031, -19.999), Rotation(74, 0, 0), optsBesiegeDummy),
-		restoration:gen_missionscript(400012, "spawn_swats_2", optsspawnvanSWATs_1),
+		restoration:gen_missionscript(400012, "spawn_swats_2", optsspawnvanSWATs_2),
 		restoration:objecteditor(400013, "open_swat_doors_2", Vector3(0, 0, 0), Rotation(0, 0, -0), optsOpenSwatVanDoors_1),
 		restoration:gen_spawngroup(400014, "swat_group_2", { 400001, 400002, 400003, 400004 }, 0, opts_swat_group),
 	},

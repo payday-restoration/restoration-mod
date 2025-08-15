6a4628146edb8030a6bec01730d8f971e25ef792
Doshyy <haritharsyadcp@gmail.com>
2025-08-16 00:17:50 +0800

    another adjustment

--- req/mission_script_add/election_day_2.lua
+++ req/mission_script_add/election_day_2.lua
@@ -55,6 +55,6 @@ return {
 		restoration:gen_dummy(400011, "swat_van_spawn_8", Vector3(-2310.692, 1952.031, -19.999), Rotation(74, 0, 0), optsBesiegeDummy),
 		restoration:gen_missionscript(400012, "spawn_swats_2", optsspawnvanSWATs_2),
 		restoration:objecteditor(400013, "open_swat_doors_2", Vector3(0, 0, 0), Rotation(0, 0, -0), optsOpenSwatVanDoors_1),
-		restoration:gen_spawngroup(400014, "swat_group_2", { 400001, 400002, 400003, 400004 }, 0, opts_swat_group),
+		restoration:gen_spawngroup(400014, "swat_group_2", { 400008, 400009, 400010, 400011 }, 0, opts_swat_group),
 	},
 }

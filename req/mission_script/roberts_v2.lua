5fec97518b6f6d1ca9b93c6bdd4ab840d56f95d6
Doshyy <haritharsyadcp@gmail.com>
2025-08-24 10:34:56 +0800

    Update roberts_v2.lua

--- req/mission_script/roberts_v2.lua
+++ req/mission_script/roberts_v2.lua
@@ -1,6 +1,5 @@
 local difficulty = tweak_data:difficulty_to_index(Global.game_settings and Global.game_settings.difficulty or "normal")
 local pro_job = Global.game_settings and Global.game_settings.one_down
-local bravo_dozer_ambush = ((pro_job and difficulty == 7 or pro_job and difficulty == 8) and "units/pd2_mod_bravo/characters/ene_bravo_bulldozer/ene_bravo_bulldozer")
 local street_spawn = {
 	values = {
 		interval = 5,
@@ -21,11 +20,7 @@ local sewer_spawn = {
 		interval = 20,
 	},
 }
-local bravo_dozer = {
-	values = {
-		enemy = bravo_dozer_ambush,
-	},
-}
+
 return {
     -- Add early reinforce around the bank
 	[100109] = {
@@ -59,8 +54,6 @@ return {
 			{ id = 400005, delay = 0, delay_rand = 5 },
 		},
 	},
-	-- National Guard Dozer kicks down door, only on DW/DS PJ
-	[106414] = bravo_dozer,
 	-- Spawngroup delays
 	[100128] = street_spawn,
 	[100132] = street_spawn,

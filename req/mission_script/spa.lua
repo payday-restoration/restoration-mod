7ec8491134cab2f5de76bfac6043db36ad840c5c
moon <108492539+erushinj@users.noreply.github.com>
2025-11-04 21:32:17 -0600

    Restore Brooklyn 10-10 edit with a bandaid
    - Was using non-bloody NYPD vetcop regardless of difficulty, even though he's unloaded on DW/DS
    - TODO: properly fix later with auto-faction scaling for more/all Res units

--- req/mission_script/spa.lua
+++ req/mission_script/spa.lua
@@ -1,19 +1,26 @@
 local difficulty = tweak_data:difficulty_to_index(Global.game_settings and Global.game_settings.difficulty or "normal")
 local ponr_value = (difficulty <= 5 and 360 or (difficulty == 6 or difficulty == 7) and 330) or 300
-	
+
 local low_interval = {
 	values = {
-			interval = 20
-		}
-}	
+		interval = 20,
+	},
+}
+local vetcop_bandaid = difficulty > 6 and {
+	values = {
+		enemy = "units/pd2_mod_nypd/characters/ene_nypd_veteran_cop_1/ene_nypd_veteran_cop_1",
+	},
+} or nil
 return {
-	--Pro Job PONR 
+	-- Pro Job PONR
 	[100483] = {
-		ponr = ponr_value
+		ponr = ponr_value,
 	},
 	-- Slow down window spawns
 	[100750] = low_interval,
 	[101012] = low_interval,
 	[102138] = low_interval,
-	[104338] = low_interval
+	[104338] = low_interval,
+	-- TODO: properly fix later with auto-faction scaling for more/all Res units
+	[100216] = vetcop_bandaid,
 }

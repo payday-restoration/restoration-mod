db9908d850aaae5f946985a0eb0b1a4e1c05980c
Doshyy <haritharsyadcp@gmail.com>
2025-11-22 09:32:50 +0800

    Update kenaz.lua

--- req/mission_script_add/kenaz.lua
+++ req/mission_script_add/kenaz.lua
@@ -4,6 +4,8 @@ local tank_skull =  ((difficulty >= 8 and pro_job) and "units/pd2_dlc_gitgud/cha
 local surprise_cloaker = "units/payday2/characters/ene_spook_1/ene_spook_1"
 local shields = ((difficulty >= 8 and pro_job) and "units/pd2_dlc_vip/characters/ene_phalanx_1_assault/ene_phalanx_1_assault" or "units/payday2/characters/ene_shield_2/ene_shield_2")
 local sniper = ((difficulty >= 7 and pro_job) and "units/pd2_dlc_vip/characters/ene_titan_sniper/ene_titan_sniper" or "units/payday2/characters/ene_sniper_1/ene_sniper_1")
+local death_wish_above = difficulty >= 7
+local overkill_above = diff_scaling >= 5
 local diff_scaling = (death_wish_above and 0.7 or overkill_above and 0.15 or 0.3) + (pro_job and 0.5 or 0)
 local enabled_chance_ambush = math.random() < diff_scaling
 local enabled_ambush = pro_job or overkill_above and enabled_chance_ambush
@@ -204,33 +206,45 @@ return {
 		-- PONR 
 		restoration:gen_pointofnoreturn(400001, "pro_job_ponr_limo", Vector3(0, 0, 0), Rotation(0, 0, 0), opts_pro_job_ponr_limo),
 		restoration:gen_pointofnoreturn(400002, "pro_job_ponr_bfd", Vector3(0, 0, 0), Rotation(0, 0, 0), opts_pro_job_ponr_bfd),
+
 		restoration:gen_pointofnoreturn(400003, "pro_job_ponr_van", Vector3(0, 0, 0), Rotation(0, 0, 0), opts_pro_job_ponr_van),
 		restoration:gen_toggleelement(400004, "enable_ponr_bfd", opts_enable_ponr_bfd),
+
 		restoration:gen_toggleelement(400005, "enable_ponr_van", opts_enable_ponr_van),
 		restoration:gen_counter(400006, "pro_job_ponr_counter", Vector3(0, 0, 0), Rotation(0, 0, 0), opts_pro_job_ponr_counter),
+
 		restoration:gen_toggleelement(400007, "disable_ponrs", opts_disable_ponrs),
 		-- FWB styled vault ambush 
 		-- Shields 
 		restoration:gen_dummy(400008, "shield_blockade_vault_01", Vector3(-200, 800, 100), Rotation(180, 0, -0), optsShield_01), -- top stairs, security room
+
 		restoration:gen_dummy(400009, "shield_blockade_vault_02", Vector3(299.985, -98.255, -499.500), Rotation(-176, 0, -0), optsShield_02), -- stairs near the vault
+
 		restoration:gen_dummy(400010, "shield_blockade_vault_03", Vector3(200,-100, -499.500), Rotation(175, 0, -0), optsShield_03), -- stairs near the vault
+
 		restoration:gen_dummy(400011, "shield_blockade_vault_04", Vector3(400, -100, -499.500), Rotation(179, 0, -0), optsShield_04), -- stairs near the vault
 		restoration:gen_so(400012, "shield_defend_so_1", Vector3(-200, 800, 100.001), Rotation( -169, -0, -0), optsDefend_SO),
+
 		restoration:gen_so(400013, "shield_defend_so_2",  Vector3(299.985, -98.255, -499.500), Rotation(-168, 0, -0), optsDefend_SO),
+
 		restoration:gen_so(400014, "shield_defend_so_3", Vector3(200, -100, -499.500), Rotation( -177, -0, -0), optsDefend_SO),
+
 		restoration:gen_so(400015, "shield_defend_so_4", Vector3(399.970, -96.510, -499.500), Rotation( -176, -0, -0), optsDefend_SO),
 		restoration:gen_toggleelement(400016, "enable_shields", opts_enabale_shields),
 		-- Dozer 
 		restoration:gen_dummy(400017, "dozer_vault_ambush", Vector3(0, -500, -900), Rotation(-180, 0, -0), opts_surprise_dozer),
 		restoration:gen_so(400018, "dozer_defend_so_1", Vector3(-13.777, -610.714, -900), Rotation(-178, 0, -0), optsDefend_SO),
+
 		restoration:gen_toggleelement(400019, "enable_dozer", opts_enable_dozer),
 		-- Cloaker 
 		restoration:gen_dummy(400020, "cloaker_ambush", Vector3(300.137, -994.766, -900), Rotation(87, 0 , -0), opts_cloaker_surprise),
 		restoration:gen_so(400021, "surprise_motherfucker", Vector3(300, -1000, -900), Rotation(92, 0, -0), optsCloaker_Hide_SO_1),
+
 		restoration:gen_toggleelement(400022, "enable_cloaker", opts_enable_surprse_cloaker),
 		-- Sniper 
 		restoration:gen_dummy(400023, "sniper_vault", Vector3(-100, -100, -499.500), Rotation(-174, 0, -0), optsSniper),
 		restoration:gen_so(400024, "sniper_defend_so_1", Vector3(-100, -100, -499.500), Rotation(-174, 0, -0), optsDefend_SO),
+		
 		restoration:gen_toggleelement(400025, "enable_sniper", opts_enable_sniper),
 
 	},

d87c005a675a44c4a13e729b67c8a569b9a13a7f
Doshyy <haritharsyadcp@gmail.com>
2026-01-09 23:40:36 +0800

    idk anymore, have this reinforce while i lose my mind how to unfuck tis

--- req/mission_script/mus.lua
+++ req/mission_script/mus.lua
@@ -24,6 +24,30 @@ return {
 			{ id = 400005, delay = 0,},
 		},
 	},
+	[100109] = { -- reinforce stuff 
+		reinforce = {
+			{
+				name = "reception",
+				force = 3,
+				position = Vector3(-3121, 226, -700.005),
+			},
+			{
+				name = "middle", 
+				force = 3, 
+				position = Vector3(-440, 85, -363.758),
+			},
+			{
+				name = "mummy_room", 
+				force = 2, 
+				position = Vector3(67, 2280, -300),
+			},
+			{
+				name = "artifact_room_01", 
+				force = 2, 
+				position = Vector3(-31, -1614, -300),
+			},
+		},
+	},
 	-- spawn cloaker as the door opens 
 	[101893] = {
 		on_executed = {
@@ -31,7 +55,7 @@ return {
 		},
 	},
 	-- start the Spook Hiders in first assault wave 
-	[100115] = {
+	[100116] = {
 		on_executed = {
 			{ id = 400028, delay = 0,}, 
 		},

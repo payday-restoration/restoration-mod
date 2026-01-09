061bf738eedcc7ee33be85161a86d92ae475d7b0
DeadMansChest <everyonewins@live.ca>
2026-01-09 03:57:36 -0500

    actually lets try this
    hopefully no adverse side effects like causing a stack overflow or some infinite feedback loop from trying to give missing dlc items, failing, trying to give missing dlc items, failing, etc.

--- lua/sc/managers/dlcmanager.lua
+++ lua/sc/managers/dlcmanager.lua
@@ -19,9 +19,12 @@ function GenericDLCManager:give_missing_package()
 				if check_loot_drop and loot_drop.type_items == "armor_skins" then
 					entry = tweak_data.economy.armor_skins[loot_drop.item_entry]
 					has_item = managers.blackmarket:armor_skin_unlocked(loot_drop.item_entry)
-					log("economy: " .. tostring( loot_drop.item_entry ))
-					if not entry.steam_economy and not has_item then
-						managers.blackmarket:on_aquired_armor_skin(loot_drop.item_entry)
+					if entry then
+						if not entry.steam_economy and not has_item then
+							managers.blackmarket:on_aquired_armor_skin(loot_drop.item_entry)
+						end
+					else 
+						log("something went wrong trying to give an item from: " .. tostring( loot_drop.item_entry ))
 					end
 
 					check_loot_drop = false
@@ -53,41 +56,44 @@ function GenericDLCManager:give_missing_package()
 
 				if check_loot_drop then
 					entry = tweak_data.blackmarket[loot_drop.type_items][loot_drop.item_entry]
-					log("loot_drop: " .. tostring( loot_drop.item_entry ))
 					global_value = loot_drop.global_value or data.content.loot_global_value or package_id
 					passed = false
 
-					if (loot_drop.type_items == "weapon_mods" or loot_drop.type_items == "weapon_skins") and entry.is_a_unlockable then
-						has_item = managers.blackmarket:get_item_amount(global_value, loot_drop.type_items, loot_drop.item_entry, true) > 0
-						passed = not has_item
-					elseif loot_drop.type_items ~= "weapon_mods" and entry.value == 0 then
-						has_item = managers.blackmarket:get_item_amount(global_value, loot_drop.type_items, loot_drop.item_entry, true) > 0
+					if entry then
+						if (loot_drop.type_items == "weapon_mods" or loot_drop.type_items == "weapon_skins") and entry.is_a_unlockable then
+							has_item = managers.blackmarket:get_item_amount(global_value, loot_drop.type_items, loot_drop.item_entry, true) > 0
+							passed = not has_item
+						elseif loot_drop.type_items ~= "weapon_mods" and entry.value == 0 then
+							has_item = managers.blackmarket:get_item_amount(global_value, loot_drop.type_items, loot_drop.item_entry, true) > 0
 
-						if not has_item then
-							if loot_drop.type_items == "masks" then
-								for slot, crafted in pairs(Global.blackmarket_manager.crafted_items.masks) do
-									if slot ~= 1 and crafted.mask_id == loot_drop.item_entry and crafted.global_value == global_value then
-										has_item = true
-
-										break
-									end
-								end
-							elseif loot_drop.type_items == "materials" or loot_drop.type_items == "textures" or loot_drop.type_items == "colors" then
-								for slot, crafted in pairs(Global.blackmarket_manager.crafted_items.masks) do
-									if slot ~= 1 then
-										name = name_converter[loot_drop.type_items]
-
-										if crafted.blueprint[name].id == loot_drop.item_entry and crafted.blueprint[name].global_value == global_value then
+							if not has_item then
+								if loot_drop.type_items == "masks" then
+									for slot, crafted in pairs(Global.blackmarket_manager.crafted_items.masks) do
+										if slot ~= 1 and crafted.mask_id == loot_drop.item_entry and crafted.global_value == global_value then
 											has_item = true
 
 											break
 										end
 									end
-								end
-							end
+								elseif loot_drop.type_items == "materials" or loot_drop.type_items == "textures" or loot_drop.type_items == "colors" then
+									for slot, crafted in pairs(Global.blackmarket_manager.crafted_items.masks) do
+										if slot ~= 1 then
+											name = name_converter[loot_drop.type_items]
 
-							passed = not has_item
+											if crafted.blueprint[name].id == loot_drop.item_entry and crafted.blueprint[name].global_value == global_value then
+												has_item = true
+
+												break
+											end
+										end
+									end
+								end
+
+								passed = not has_item
+							end
 						end
+					else
+						log("something went wrong trying to give an item from: " .. tostring( loot_drop.item_entry ))
 					end
 
 					if passed then

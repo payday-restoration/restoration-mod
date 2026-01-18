7e9f301b40c88a61f4eb265f14f7b5929e8a549a
Doshyy <haritharsyadcp@gmail.com>
2026-01-18 22:21:58 +0800

    crude awakening spawn intervals (?)

--- req/mission_script/deep.lua
+++ req/mission_script/deep.lua
@@ -4,14 +4,33 @@ local timer_achievement = (solo and 200) or 20
 local sniper_trigger_times = {
 	values = {
         trigger_times = 0
-	}
+	},
 }
 local medium_interval = {
 	values = {
-			interval = 15
-		}
+			interval = 20
+		},
 }
-
+local catwalk_spawn = {
+	values = {
+		interval = 20
+	},	
+}
+local lab_spawn = {
+	values = {
+		interval = 30
+	},
+}
+local bridge_spawn = {
+	values = {
+		interval = 20
+	},
+}
+local slide_gate = {
+	values = {
+		interval = 20 
+	},
+}
 return {
 	-- Increase PONR timers, seriously there are a lot of skulldozers on high diffs especially with 2 pushover minidozers at the end on DS (luckily they are bendozers)
 	[102646] = {
@@ -55,10 +74,22 @@ return {
 	[100375] = sniper_trigger_times,
 	[100376] = sniper_trigger_times,
 	[100377] = sniper_trigger_times,
-	-- Increase spawn group intervals in pipe arena
+	-- spawn group intervals in pipe arena
 	[105278] = medium_interval,
 	[102086] = medium_interval,
 	[103986] = medium_interval,
 	[101777] = medium_interval,
-	[101778] = medium_interval
+	[101778] = medium_interval,
+	-- catwalk spawngroup 
+	[100132] = catwalk_spawn,
+	-- lab spawngroup 
+	[104573] = lab_spawn,
+	-- bridge spawngroups 
+	[101770] = bridge_spawn, 
+	[101775] = bridge_spawn,
+	[101776] = bridge_spawn,
+	[101769] = bridge_spawn,
+	-- slide gate spawngroups 
+	[104030] = slide_gate,
+
 }

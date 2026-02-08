a085fb4929671a82bbe2d17387dfc93e129f5b5c
Cerapter <cerap@protonmail.com>
2026-02-08 11:15:17 +0100

    Play "pager operator fooled" sound when check-ins happen
    Was originally going to use normal idle chatter noises, but this one actually does make it a bit more distinct.
    Communicates to players a bit more clearly that something is happening.

--- lua/sc/managers/groupaistatebase.lua
+++ lua/sc/managers/groupaistatebase.lua
@@ -1056,6 +1056,11 @@ function GroupAIStateBase:update(t, dt)
 					self:_delay_whisper_suspicion_mul_decay()
 				end
 
+				if data.unit:sound() then
+					data.unit:sound():stop()
+					data.unit:sound():play(data.unit:brain():_get_radio_id("dsp_radio_fooled_1"), nil, true)
+				end
+
 				data.t = data.t + time_since_intimidation
 			end
 

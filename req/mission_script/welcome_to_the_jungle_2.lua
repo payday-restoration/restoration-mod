ad54cf1bd8ebe4457fedc58a1ef2ecb3388e799e
Doshyy <haritharsyadcp@gmail.com>
2026-03-01 22:51:23 +0800

    big oil day 2 reinforce slop

--- req/mission_script/welcome_to_the_jungle_2.lua
+++ req/mission_script/welcome_to_the_jungle_2.lua
@@ -6,7 +6,51 @@ local ponr = {
 }	
 
 return {
-	--Pro Job PONR
+	-- Reinforce stuff 
+	[100533] = { -- Police
+		reinforce = {
+			{
+				name = "cars",
+				force = 3,
+				position = Vector3(-1789, 1465, -0.017),
+			},
+			{
+				name = "near_escape", 
+				force = 2, 
+				position = Vector3(-4669, -865, 441.44),
+			},
+			{
+				name = "bbq", 
+				force = 2,
+				position = Vector3(-1804, -2882, 0),
+			},
+		},
+	},
+	[100267] = { -- When the lab door is open 
+		reinforce  = {
+			{
+				name = "kitchen",
+				force = 2, 
+				position = Vector3(-1278, -1888, 0),
+			},
+			{
+				name = "snooker",
+				force = 2,
+				position = Vector3(76, -2251, 0), 
+			},
+			{
+				name = "tabul", 
+				force = 3, 
+				position = Vector3(948, -2236, 0), 
+			},
+			{
+				name = "da_pool",
+				force = 3, 
+				position = Vector3(2513, -3479, 0),
+			},
+		},
+	},
+	-- Pro Job PONR
 	[103328] = ponr,
 	[102207] = ponr,
 	-- Add power cut SO delay
@@ -15,5 +59,5 @@ return {
 			base_delay = 30,
 			base_delay_rand = 30
 		}
-	}
+	},
 }

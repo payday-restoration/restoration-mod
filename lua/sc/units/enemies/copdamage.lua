820db0eeb8a44aa0807dd36d2748c465e06bd15c
Noep <everyonewins@live.ca>
2026-04-30 13:53:49 -0400

    Update copdamage.lua

--- lua/sc/units/enemies/copdamage.lua
+++ lua/sc/units/enemies/copdamage.lua
@@ -4120,7 +4120,9 @@ function CopDamage:do_medic_heal()
 end
 
 function CopDamage:_check_melee_achievements(attack_data)
-	if tweak_data.blackmarket.melee_weapons[attack_data.name_id] then
+	local player_unit = managers.player:player_unit()
+
+	if alive(player_unit) and tweak_data.blackmarket.melee_weapons[attack_data.name_id] then
 		local is_civlian = CopDamage.is_civilian(self._unit:base()._tweak_table)
 		local is_gangster = CopDamage.is_gangster(self._unit:base()._tweak_table)
 		local is_cop = not is_civlian and not is_gangster
@@ -4130,7 +4132,13 @@ function CopDamage:_check_melee_achievements(attack_data)
 		local enemy_movement = self._unit:movement()
 		local enemy_type = enemy_base._tweak_table
 		local unit_weapon = enemy_base._default_weapon_id
-		local health_ratio = managers.player:player_unit():character_damage():health_ratio() * 100
+		local health_ratio = nil
+
+		if alive(player_unit) and player_unit:character_damage() then
+			health_ratio = player_unit:character_damage():health_ratio() * 100
+		else
+			health_ratio = 0
+		end
 		local melee_pass, melee_weapons_pass, type_pass, enemy_pass, enemy_weapon_pass, diff_pass, health_pass, level_pass, job_pass, jobs_pass, enemy_count_pass, tags_all_pass, tags_any_pass, all_pass, cop_pass, gangster_pass, civilian_pass, stealth_pass, on_fire_pass, behind_pass, result_pass, mutators_pass, critical_pass, action_pass, is_dropin_pass, style_pass = nil
 
 		local function count_enemy_type_kills(enemy_type)

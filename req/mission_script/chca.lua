4cc865b900fe699423135363ac766cb9c1d0287a
Doshyy <haritharsyadcp@gmail.com>
2026-05-29 11:36:33 +0800

    Update chca.lua

--- req/mission_script/chca.lua
+++ req/mission_script/chca.lua
@@ -141,15 +141,6 @@ return {
 			GS_defend_shields = false,
 		},
 	},
-	values = {
-			elements = {
-				102515,
-				102516,
-				102517,
-				102518,
-				102519
-			},
-		},
 		-- Add LoS blockers
 	[143003] = {
 		spawn = los_blockers

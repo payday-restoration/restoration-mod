b6c45d65cea09ffb7808180458cc4d1ccf93bf5f
moon <108492539+erushinj@users.noreply.github.com>
2026-06-15 22:49:54 -0600

    Unimaginable implications for solo players

--- lua/sc/units/weapons/newnpcraycastweaponbase.lua
+++ lua/sc/units/weapons/newnpcraycastweaponbase.lua
@@ -261,3 +261,18 @@ function NewNPCRaycastWeaponBase:_sound_singleshot()
 		sound = self._sound_fire:post_event(sound_name)
 	end
 end
+
+if Network:is_client() then
+	return
+end
+
+-- Make team AI weapons alert enemies (oversight from when bots got the ability to use player weapons)
+Hooks:PostHook(NewNPCRaycastWeaponBase, "set_user_is_team_ai", "sh_set_user_is_team_ai", function (self)
+	if not self._setup or not alive(self._setup.user_unit) then
+		return
+	end
+
+	self._setup.alert_AI = true
+	self._setup.alert_filter = self._setup.user_unit:brain():SO_access()
+	self._alert_events = {}
+end)

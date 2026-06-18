403c01b843acbff17b2ba49acecf7dd00071ba1f
SC <gametwisterdotnet@yahoo.com>
2026-06-17 22:58:01 -0700

    Update blackmarketgui.lua

--- lua/sc/managers/blackmarketgui.lua
+++ lua/sc/managers/blackmarketgui.lua
@@ -5735,7 +5735,8 @@ function BlackMarketGui:update_info_text()
 		local proj_b_tweak = tweak_data.blackmarket.projectiles[slot_data.name]
 		local pickup_low = proj_b_tweak and proj_b_tweak.base_pickup_chance and proj_b_tweak.base_pickup_chance[1] or 0.01
 		local pickup_high = proj_b_tweak and proj_b_tweak.base_pickup_chance and proj_b_tweak.base_pickup_chance[2] or 0.02
-		local skill_pickup_chance = managers.player:upgrade_value("player", "regain_throwable_from_ammo", 1)
+		-- Set this to 1 for now, since this line straight up isn't needed in gold yet
+		local skill_pickup_chance = 1
 		local desc_text = managers.localization:text(tweak_data.blackmarket.projectiles[slot_data.name].desc_id, {
 			damage = ((proj_tweak and proj_tweak.damage) or 0) * 10, --I LOVE that damage is defined elsewhere
 			pickup_1 = math.floor(1 / (pickup_high * skill_pickup_chance)),

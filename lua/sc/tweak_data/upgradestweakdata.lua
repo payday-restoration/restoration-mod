c682ee22b2e5b4600f40d9e34b4d724a85a85866
Cerapter <cerap@protonmail.com>
2026-07-14 09:44:24 +0200

    Stoic giganerfs
    Healing from negated damage changed from 200% of DoT to 175% of DoT.

--- lua/sc/tweak_data/upgradestweakdata.lua
+++ lua/sc/tweak_data/upgradestweakdata.lua
@@ -2966,7 +2966,7 @@ Hooks:PostHook(UpgradesTweakData, "_init_pd2_values", "ResSkillsInit", function(
 		4
 	}
 	self.values.player.damage_control_healing = {
-		200
+		175
 	}
 
 	self.values.player.damage_control_cooldown_drain = {

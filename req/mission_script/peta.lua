57f5dcfdabcb48b9cfb36f0da8d72c343675ae5a
Doshyy <haritharsyadcp@gmail.com>
2026-07-14 16:35:41 +0800

    missions scrriptpttt updates

--- req/mission_script/peta.lua
+++ req/mission_script/peta.lua
@@ -1,6 +1,21 @@
 local pro_job = Global.game_settings and Global.game_settings.one_down
 local bravos_inbound = (pro_job and true) or false
-
+local rapple_spawn = {
+	values = {
+		interval = 35,
+	},
+}
+local standard_spawn = {
+	values = {
+		interval = 35,
+	},
+}
+local burning_apartment_spawn = {
+	values = {
+		interval = 20,
+	},
+}
+local 
 return {
 	--Highlight all remaining goats when Boris arrives
 	--Call Bravos when on PJs
@@ -8,6 +23,25 @@ return {
 	spawn_bravos = bravos_inbound,
 	on_executed = {
 			{id = 100672, delay = 0}
-		}
-	}
+		},
+	},
+	-- Disable one of the  misreable truck drop offs
+	[101784] = {
+		values = {
+			enabled = false,
+		},
+	},
+	-- No Matter what fucking overhaul this heist is utterly shit, increased intervals all around!
+	[100128] = standard_spawn,
+	[100130] = standard_spawn,
+	[100132] = standard_spawn,
+	[100133] = standard_spawn,
+	[100694] = rapple_spawn,
+	[100131] = rapple_spawn,
+	[106015] = standard_spawn,
+	[106016] = standard_spawn,
+	[106017] = standard_spawn,
+	[106019] = standard_spawn,
+	[106021] = standard_spawn,
+	[106076] = burning_apartment_spawn,
 }

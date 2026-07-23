cb623505dea5ef2d4055fd9b84019fddb33e812f
Doshyy <haritharsyadcp@gmail.com>
2026-07-23 20:12:31 +0800

    try fix asu russian not stacking UMP For Me, UMP for you cheevo

--- lua/sc/tweak_data/achievementstweakdata.lua
+++ lua/sc/tweak_data/achievementstweakdata.lua
@@ -66,7 +66,16 @@ function AchievementsTweakData:init(tweak_data)
 			}
 		}
 	}
-	
+	-- Fix for UMP For Me, UMP For You not stacking on ASU 
+	self.enemy_kill_achievements.ump_for_me_ump_for_you = {
+		weapon_type = "smg",
+		stat = "smg_any_specials",
+		difficulty = overkill_and_above,
+		enemy_tags_any = {
+			"hrt_titan"
+		}
+
+	}
 	-- Fix for Sneaky Beaky Like to be possible
 	self.complete_heist_achievements.grv_4 = {
 		stealth = true,
@@ -240,7 +249,6 @@ function AchievementsTweakData:init(tweak_data)
 			"spooc_gangster",
 			"autumn", --why not?
 			"spooc_titan"
-			
 		}
 	}
 	

6dc60c8a2acef58cc8a26a4bf03a8063b90e4afe
Doshyy <haritharsyadcp@gmail.com>
2026-07-29 13:01:25 +0800

    fix normal swat nypd not having sound helmet pop sound
    this is my first time touching these so i assume this is how its done

--- lua/sc/units/enemies/copdamage.lua
+++ lua/sc/units/enemies/copdamage.lua
@@ -78,6 +78,13 @@ local enemies_plink = {
 	ids_func("units/pd2_mod_lapd/characters/ene_city_heavy_r870_sc/ene_city_heavy_r870_sc"),
 	ids_func("units/pd2_mod_lapd/characters/ene_city_heavy_r870_sc/ene_city_heavy_r870_sc_husk"),
 		
+	ids_func("units/pd2_mod_nypd/characters/ene_nypd_swat_1/ene_nypd_swat_1"),
+	ids_func("units/pd2_mod_nypd/characters/ene_nypd_swat_1/ene_nypd_swat_1_husk"),
+	ids_func("units/pd2_mod_nypd/characters/ene_nypd_swat_2/ene_nypd_swat_2"),
+	ids_func("units/pd2_mod_nypd/characters/ene_nypd_swat_2/ene_nypd_swat_2_husk"),
+	ids_func("units/pd2_mod_nypd/characters/ene_nypd_swat_3/ene_nypd_swat_3"),
+	ids_func("units/pd2_mod_nypd/characters/ene_nypd_swat_32/ene_nypd_swat_3_husk"),
+
 	ids_func("units/pd2_mod_reapers/characters/ene_city_heavy_r870/ene_city_heavy_r870"),
 	ids_func("units/pd2_mod_reapers/characters/ene_city_heavy_r870/ene_city_heavy_r870_husk"),   
 	

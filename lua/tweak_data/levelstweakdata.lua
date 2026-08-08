b5666bec9f2db08bb4899af597c09d92e0f159ab
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2026-08-08 09:21:55 +0100

    Attempt at fixing music shuffle option (Needs testing)

--- lua/tweak_data/levelstweakdata.lua
+++ lua/tweak_data/levelstweakdata.lua
@@ -7,12 +7,14 @@ end
 local get_music_event_original_res = LevelsTweakData.get_music_event
 function LevelsTweakData:get_music_event(stage)
     local result = get_music_event_original_res(self, stage)
-    if result and stage == "control" and restoration.Options:GetValue("OTHER/MusicShuffle") and not restoration.Options:GetValue("OTHER/PONRTrack") then
-        if self.can_change_music then
-            managers.music:check_music_switch()
-        else
-            self.can_change_music = true
+    if result and restoration.Options:GetValue("OTHER/MusicShuffle") and not restoration.Options:GetValue("OTHER/PONRTrack") then
+        if stage == 'control' then
+            if self.can_change_music then
+                managers.music:check_music_switch()
+            else
+                self.can_change_music = 1
+            end
         end
     end
     return result
-end 
+end

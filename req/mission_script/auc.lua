35ad63ae08fbbd1cc066a2da65c184b8bcf0b40c
Doshyy <haritharsyadcp@gmail.com>
2026-09-20 14:18:10 +0800

    auc mission script so i can shove my own shit later
    with perms from eclipse i just take this so i can set up the other shit later

---
+++ req/mission_script/auc.lua
@@ -0,0 +1,53 @@
+return {
+	-- Combine some navigation areas
+	[101204] = { -- link_startup
+		ai_area = {
+			{ 97, 130, 131 },
+			{ 95, 35 },
+			{ 150, 162 },
+		},
+	},
+	-- New reinforce
+	[103416] = { -- difficulty_0.5
+		reinforce = {
+			{
+				name = "what_a_nice_truck",
+				force = 2,
+				position = Vector3(1400, 5375, -100),
+			},
+			{
+				name = "street01",
+				force = 2,
+				position = Vector3(1200, 1000, -100),
+			},
+			{
+				name = "street02",
+				force = 2,
+				position = Vector3(1225, 3425, -100),
+			},
+		},
+	},
+	[103629] = { -- difficulty_1
+		reinforce = {
+			{
+				name = "such_nice_cars",
+				force = 2,
+				position = Vector3(-3225, 4200, 0),
+			},
+			{
+				name = "reception",
+				force = 2,
+				position = Vector3(-900, 2300, 0),
+			},
+		},
+	},
+	[106784] = { -- enemy_trigger_auctioneer_dead
+		reinforce = {
+			{
+				name = "auction",
+				force = 2,
+				position = Vector3(-3600, 1400, 400),
+			},
+		},
+	},
+}
